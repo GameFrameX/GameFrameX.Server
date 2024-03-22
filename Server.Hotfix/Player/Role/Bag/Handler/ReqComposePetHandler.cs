8d5ba00807ae8e035ac52613b4e5b1beb00b65c8
AlianBlank <wangfj11@foxmail.com>
2024-03-22 15:04:22 +0800

    从主项目拆分；版本记录时间为2024-3-22 14:10:12
    https://github.com/AlianBlank/GameFrameX/commit/51eb1f141a4b0395835b2fc6acbfee7876ca798f

---
+++ Server.Hotfix/Player/Role/Bag/Handler/ReqComposePetHandler.cs
@@ -0,0 +1,15 @@
+﻿using Server.Core.Net.BaseHandler;
+using Server.Hotfix.Player.Role.Bag.Agent;
+using Server.NetWork.Messages;
+
+namespace Server.Hotfix.Player.Role.Bag.Handler
+{
+    [MessageMapping(typeof(ReqComposePet))]
+    public class ReqComposePetHandler : RoleComponentHandler<BagComponentAgent>
+    {
+        protected override async Task ActionAsync()
+        {
+            await Comp.ComposePet(Message as ReqComposePet);
+        }
+    }
+}
