038dcaeb2e2803264bfae97674478821c4b093b4
AlianBlank <wangfj11@foxmail.com>
2024-04-07 11:50:10 +0800

    [修改]1. 修改外部消息解码器

--- Server.Launcher/Message/MessageRouterDecoderHandler.cs
+++ Server.Launcher/Message/MessageRouterDecoderHandler.cs
@@ -1,8 +1,4 @@
-using System.Buffers;
 using Server.Extension;
-using Server.NetWork;
-using Server.NetWork.Messages;
-using Server.Proto;
 using Server.Serialize.Serialize;
 using SuperSocket.ProtoBase;
 
@@ -12,29 +8,39 @@ class MessageRouterDecoderHandler : IMessageDecoderHandler, IPackageDecoder<IMes
 {
     public IMessage Handler(Span<byte> data)
     {
-        int readOffset = 0;
-        var length = data.ReadInt(ref readOffset);
-        var timestamp = data.ReadLong(ref readOffset);
-        var messageId = data.ReadInt(ref readOffset);
-        // var messageUniqueData = data.ReadBytes(ref readOffset);
-        var messageData = data.ReadBytes(ref readOffset);
-        var messageType = ProtoMessageIdHandler.GetReqTypeById(messageId);
-        if (messageType != null)
+        try
         {
-            var messageObject = (MessageObject)SerializerHelper.Deserialize(messageData, messageType);
-            messageObject.MessageId = messageId;
-            return messageObject;
+            int readOffset = 0;
+            var length = data.ReadInt(ref readOffset);
+            var timestamp = data.ReadLong(ref readOffset);
+            var messageId = data.ReadInt(ref readOffset);
+            // var messageUniqueData = data.ReadBytes(ref readOffset);
+            var messageData = data.ReadBytes(ref readOffset);
+            var messageType = ProtoMessageIdHandler.GetReqTypeById(messageId);
+            if (messageType != null)
+            {
+                var messageObject = (MessageObject)SerializerHelper.Deserialize(messageData, messageType);
+                messageObject.MessageId = messageId;
+                return messageObject;
+            }
+            else
+            {
+                LogHelper.Fatal("未知消息类型");
+                return null;
+            }
         }
-        else
+        catch (Exception e)
         {
-            LogHelper.Fatal("未知消息类型");
+            LogHelper.Fatal(e);
             return null;
         }
     }
 
+
     public IMessage Decode(ref ReadOnlySequence<byte> buffer, object context)
     {
         var data = buffer.ToArray();
+        LogHelper.Info(data.ToArrayString());
         var messageObject = Handler(data);
         return messageObject;
     }
