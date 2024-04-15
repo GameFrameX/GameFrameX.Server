898c27f4d11024871a1195df5e1de2911b134b12
AlianBlank <wangfj11@foxmail.com>
2024-04-15 16:18:29 +0800

    [增加]1. 增加外部消息对象的传入解析

---
+++ GameFrameX.Launcher/PipelineFilter/BaseRouterSocketMessagePlugin.cs
@@ -0,0 +1,59 @@
+﻿using GameFrameX.Launcher.Extension.DependencyProperty;
+using GameFrameX.NetWork;
+using GameFrameX.NetWork.Messages;
+using GameFrameX.Utility;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+
+namespace GameFrameX.Launcher.PipelineFilter;
+
+public class BaseRouterSocketMessagePlugin : PluginBase, ITcpConnectedPlugin<ISocketClient>, ITcpDisconnectedPlugin<ISocketClient>
+{
+    static readonly MessageRouterEncoderHandler messageEncoderHandler = new MessageRouterEncoderHandler();
+    private readonly MessageRouterDecoderHandler messageRouterDecoderHandler = new MessageRouterDecoderHandler();
+
+    protected async Task<bool> MessageHandler(string socketClientIp, byte[] buffer)
+    {
+        var message = (MessageObject)messageRouterDecoderHandler.Handler(buffer);
+        if (message != null)
+        {
+            LogHelper.Debug($"---收到消息ID:[{message}] ==>消息类型:{message.GetType()} 消息内容:{message}");
+            var handler = HotfixMgr.GetTcpHandler(message.MessageId);
+            if (handler == null)
+            {
+                LogHelper.Error($"找不到[{message.MessageId}][{message.GetType()}]对应的handler");
+                return false;
+            }
+
+            handler.Message = message;
+            handler.Channel = GameClientSessionManager.GetSession(socketClientIp); // (INetChannel)socketClient.GetPropertyValue(nameof(INetChannel));
+            await handler.Init();
+            await handler.InnerAction();
+            RespHeartBeat resp = new RespHeartBeat
+            {
+                Timestamp = TimeHelper.UnixTimeSeconds()
+            };
+            await handler.Channel.WriteAsync(resp);
+            return true;
+        }
+
+        return false;
+    }
+
+    public Task OnTcpConnected(ISocketClient socketClient, ConnectedEventArgs e)
+    {
+        LogHelper.Info("有外部客户端网络连接成功！。链接信息：SessionID:" + socketClient.Id + " RemoteEndPoint:" + socketClient.Id);
+        var gameSession = new GameSession(socketClient.IP, socketClient);
+        socketClient.SetGameSession(gameSession);
+        var netChannel = new DefaultNetChannel(gameSession, messageEncoderHandler);
+        GameClientSessionManager.SetSession(socketClient.IP, netChannel); //移除
+        return Task.CompletedTask;
+    }
+
+    public Task OnTcpDisconnected(ISocketClient socketClient, DisconnectEventArgs e)
+    {
+        LogHelper.Info("有外部客户端网络断开连接成功！。断开信息：" + socketClient.Id + "  " + e.Message);
+        GameClientSessionManager.RemoveSession(socketClient.IP); //移除
+        return Task.CompletedTask;
+    }
+}
---
+++ GameFrameX.Launcher/PipelineFilter/RouterWebSocketMessagePlugin.cs
@@ -0,0 +1,72 @@
+﻿using GameFrameX.NetWork.Messages;
+using TouchSocket.Http.WebSockets;
+
+namespace GameFrameX.Launcher.PipelineFilter;
+
+public sealed class RouterWebSocketMessagePlugin : BaseRouterSocketMessagePlugin, IWebSocketReceivedPlugin<IWebSocket>
+{
+    static readonly MessageRouterEncoderHandler messageEncoderHandler = new MessageRouterEncoderHandler();
+    private readonly MessageRouterDecoderHandler messageRouterDecoderHandler = new MessageRouterDecoderHandler();
+
+    public async Task OnWebSocketReceived(IWebSocket socketClient, WSDataFrameEventArgs e)
+    {
+        switch (e.DataFrame.Opcode)
+        {
+            case WSDataType.Cont:
+                LogHelper.Info($"收到中间数据，长度为：{e.DataFrame.PayloadLength}");
+                return;
+
+            case WSDataType.Text:
+                LogHelper.Info(e.DataFrame.ToText());
+
+                if (!socketClient.Client.IsClient)
+                {
+                    socketClient.Send("我已收到");
+                }
+
+                return;
+
+            case WSDataType.Binary:
+                if (e.DataFrame.FIN)
+                {
+                    var result = await MessageHandler(socketClient.Client.IP, e.DataFrame.PayloadData.Buffer);
+                    if (result)
+                    {
+                        LogHelper.Info($"收到二进制数据，长度为：{e.DataFrame.PayloadLength}");
+                    }
+                    else
+                    {
+                        var outBuffer = new byte[e.DataFrame.PayloadLength];
+                        e.DataFrame.PayloadData.Pos = 0;
+                        _ = e.DataFrame.PayloadData.Read(outBuffer);
+
+                        await socketClient.SendAsync(outBuffer);
+                    }
+                }
+                else
+                {
+                    LogHelper.Info($"收到未结束的二进制数据，长度为：{e.DataFrame.PayloadLength}");
+                }
+
+                return;
+
+            case WSDataType.Close:
+            {
+                LogHelper.Info("远程请求断开");
+                socketClient.Close("断开");
+            }
+                return;
+
+            case WSDataType.Ping:
+                break;
+
+            case WSDataType.Pong:
+                break;
+
+            default:
+                break;
+        }
+
+        await e.InvokeNext();
+    }
+}
