ff650fa2947418257b61f0e105145a6042b19ef8
AlianBlank <wangfj11@foxmail.com>
2024-04-15 16:14:23 +0800

    [增加]1. 增加扩展设置函数

---
+++ GameFrameX.Launcher/Extension/DependencyProperty/GameSessionDependencyPropertyExtension.cs
@@ -0,0 +1,21 @@
+﻿using GameFrameX.NetWork;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+
+namespace GameFrameX.Launcher.Extension.DependencyProperty;
+
+public static class GameSessionDependencyPropertyExtension
+{
+    public static readonly DependencyProperty<IGameSession> GameSessionProperty = DependencyProperty<IGameSession>.Register("GameSession", null);
+
+    public static ISocketClient SetGameSession(this ISocketClient config, IGameSession value)
+    {
+        config.SetValue(GameSessionProperty, value);
+        return config;
+    }
+
+    public static IGameSession GetGameSession(this ISocketClient config)
+    {
+        return config.GetValue(GameSessionProperty);
+    }
+}
