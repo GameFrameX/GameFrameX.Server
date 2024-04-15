e5677af3dab7ce8bf5f3c822ec3e0c6e82a252ff
AlianBlank <wangfj11@foxmail.com>
2024-04-15 16:17:50 +0800

    [增加]1. 增加解码的返回值为空的处理

--- GameFrameX.NetWork/IMessageDecoderHandler.cs
+++ GameFrameX.NetWork/IMessageDecoderHandler.cs
@@ -4,5 +4,10 @@ namespace GameFrameX.NetWork;
 
 public interface IMessageDecoderHandler
 {
-    IMessage Handler(Span<byte> data);
+    /// <summary>
+    /// 处理消息
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    IMessage? Handler(byte[] data);
 }
