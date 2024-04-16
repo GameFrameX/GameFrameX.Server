92ae7280ccb50af34176458c25cad6d261c20ee0
Blank <wangfj11@foxmail.com>
2024-04-17 00:02:13 +0800

    [修改]1. 临时修改传送代码

--- GameFrameX.Launcher/Transfer/TransferHandler.cs
+++ GameFrameX.Launcher/Transfer/TransferHandler.cs
@@ -1,8 +1,4 @@
-﻿using GameFrameX.NetWork;
-using GameFrameX.NetWork.Messages;
-using TouchSocket.Sockets;
-
-namespace GameFrameX.Launcher.Transfer;
+﻿namespace GameFrameX.Launcher.Transfer;
 
 public interface ITransferHandler
 {
@@ -12,5 +8,5 @@ public interface ITransferHandler
     /// <param name="sender"></param>
     /// <param name="messageEncoderHandler"></param>
     /// <param name="message"></param>
-    void Transfer(ITcpClientBase sender, IMessageEncoderHandler messageEncoderHandler, IMessage message);
+    // void Transfer(ITcpClientBase sender, IMessageEncoderHandler messageEncoderHandler, IMessage message);
 }
--- GameFrameX.Launcher/Transfer/TransferRouterToGatewayHandler.cs
+++ GameFrameX.Launcher/Transfer/TransferRouterToGatewayHandler.cs
@@ -1,15 +1,11 @@
-using GameFrameX.NetWork;
-using GameFrameX.NetWork.Messages;
-using TouchSocket.Sockets;
-
 namespace GameFrameX.Launcher.Transfer;
 
 [Transfer(ServerType.Router, ServerType.Gateway)]
 public class TransferRouterToGatewayHandler : ITransferHandler
 {
-    public async void Transfer(ITcpClientBase sender, IMessageEncoderHandler messageEncoderHandler, IMessage message)
+    /*public async void Transfer(ITcpClientBase sender, IMessageEncoderHandler messageEncoderHandler, IMessage message)
     {
         var messageBytes = messageEncoderHandler.Handler(message);
         await sender.SendAsync(messageBytes);
-    }
+    }*/
 }
