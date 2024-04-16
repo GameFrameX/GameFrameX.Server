d06b20538247dc82a47153cbf2e0429c2b4dec6d
AlianBlank <wangfj11@foxmail.com>
2024-04-16 19:00:42 +0800

    [增加]1. 增加客户端的连接管理

---
+++ GameFrameX.Launcher/GameClientSessionManager.cs
@@ -0,0 +1,24 @@
+﻿using GameFrameX.NetWork;
+
+namespace GameFrameX.Launcher;
+
+public static class GameClientSessionManager
+{
+    static readonly ConcurrentDictionary<string, INetChannel> Sessions = new ConcurrentDictionary<string, INetChannel>();
+
+    public static INetChannel GetSession(string id)
+    {
+        Sessions.TryGetValue(id, out var session);
+        return session;
+    }
+
+    public static void SetSession(string id, INetChannel session)
+    {
+        Sessions.TryAdd(id, session);
+    }
+
+    public static void RemoveSession(string id)
+    {
+        Sessions.TryRemove(id, out _);
+    }
+}
