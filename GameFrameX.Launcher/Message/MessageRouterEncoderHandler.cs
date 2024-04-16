d1be990a241b5b5e5f5d1a084c04ac602c96dc0e
AlianBlank <wangfj11@foxmail.com>
2024-04-16 18:59:28 +0800

    [修改]1. 修改下发给客户端的协议头

--- GameFrameX.Launcher/Message/MessageRouterEncoderHandler.cs
+++ GameFrameX.Launcher/Message/MessageRouterEncoderHandler.cs
@@ -4,18 +4,17 @@ using GameFrameX.NetWork;
 using GameFrameX.NetWork.Messages;
 using GameFrameX.Serialize.Serialize;
 using GameFrameX.Utility;
-using SuperSocket.ProtoBase;
 
 namespace GameFrameX.Launcher.Message;
 
-class MessageRouterEncoderHandler : IMessageEncoderHandler, IPackageEncoder<IMessage>
+class MessageRouterEncoderHandler : IMessageEncoderHandler
 {
     public byte[] Handler(IMessage message)
     {
         var bytes = SerializerHelper.Serialize(message);
 
         // len +timestamp + msgId + bytes.length
-        int len = 4 + 8 + 4 + 4 + bytes.Length;
+        int len = 4 + 8 + 4 + bytes.Length;
         var span = ArrayPool<byte>.Shared.Rent(len);
         int offset = 0;
         span.WriteInt(len, ref offset);
