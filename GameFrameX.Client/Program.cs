bed474899566cbddfc562ed942c91ed959c21c92
AlianBlank <wangfj11@foxmail.com>
2024-04-17 18:23:20 +0800

    [增加]1. 增加测试客户端的收发协议

--- GameFrameX.Client/Program.cs
+++ GameFrameX.Client/Program.cs
@@ -2,6 +2,8 @@
 
 
 using GameFrameX.Extension;
+using GameFrameX.NetWork.Messages;
+using GameFrameX.Proto;
 using GameFrameX.Proto.Proto;
 using GameFrameX.Serialize.Serialize;
 using GameFrameX.Utility;
@@ -9,6 +11,7 @@ using TouchSocket.Core;
 using TouchSocket.Sockets;
 using ResultCode = TouchSocket.Core.ResultCode;
 
+ProtoMessageIdHandler.Init();
 var tcpClient = new TcpClient();
 tcpClient.Connecting = (client, e) => { return EasyTask.CompletedTask; }; //即将连接到服务器，此时已经创建socket，但是还未建立tcp
 tcpClient.Connected = (client, e) => { return EasyTask.CompletedTask; }; //成功连接到服务器
@@ -18,7 +21,19 @@ tcpClient.Received = (client, e) =>
 {
     //从服务器收到信息。但是一般byteBlock和requestInfo会根据适配器呈现不同的值。
     // var mes = Encoding.UTF8.GetString(e.ByteBlock.Buffer, 0, e.ByteBlock.Len);
-    tcpClient.Logger.Info($"客户端接收到信息：");
+    int length = e.ByteBlock.ReadInt32(EndianType.Big);
+    long timestamp = e.ByteBlock.ReadInt64(EndianType.Big);
+    int messageId = e.ByteBlock.ReadInt32(EndianType.Big);
+    int offset = e.ByteBlock.Pos;
+    var messageData = e.ByteBlock.Buffer.ReadBytes(ref offset);
+    var messageType = ProtoMessageIdHandler.GetRespTypeById(messageId);
+    if (messageType != null)
+    {
+        var messageObject = (MessageObject)SerializerHelper.Deserialize(messageData, messageType);
+        messageObject.MessageId = messageId;
+        tcpClient.Logger.Info($"客户端接收到信息：{messageObject}");
+    }
+
     return EasyTask.CompletedTask;
 };
 
