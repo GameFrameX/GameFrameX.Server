6a1e6092fc9ee0c6b5a1fe834e9b0a24e421c9ce
Blank <wangfj11@foxmail.com>
2024-04-17 23:22:32 +0800

    [修改]1. 修改DB接口启用

--- GameFrameX.Apps/Account/Login/Component/LoginComponent.cs
+++ GameFrameX.Apps/Account/Login/Component/LoginComponent.cs
@@ -8,14 +8,13 @@ namespace GameFrameX.Apps.Account.Login.Component
     {
         public async Task<LoginState> OnLogin(ReqLogin reqLogin)
         {
-            return null;
-            // return await GameDb.FindAsync<LoginState>(m => m.UserName == reqLogin.UserName && m.Password == reqLogin.Password);
+            return await GameDb.FindAsync<LoginState>(m => m.UserName == reqLogin.UserName && m.Password == reqLogin.Password);
         }
 
         public async Task<LoginState> Register(long accountId, ReqLogin reqLogin)
         {
             LoginState loginState = new LoginState() { Id = accountId, UserName = reqLogin.UserName, Password = reqLogin.Password };
-            // await GameDb.SaveOneAsync<LoginState>(loginState);
+            await GameDb.SaveOneAsync<LoginState>(loginState);
             return loginState;
         }
     }
