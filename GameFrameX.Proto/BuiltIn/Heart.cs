175cb8a1a1a59076f49c56b62ad8c3b1c164dc2e
AlianBlank <wangfj11@foxmail.com>
2024-04-23 11:27:45 +0800

    [修改]1. 修改内置的协议接口

--- GameFrameX.Proto/BuiltIn/Heart.cs
+++ GameFrameX.Proto/BuiltIn/Heart.cs
@@ -1,7 +1,44 @@
 using GameFrameX.NetWork.Messages;
+using GameFrameX.Proto.Proto;
 using ProtoBuf;
 
 namespace GameFrameX.Proto.BuiltIn;
+
+public enum ErrorCode
+{
+    /// <summary>
+    /// 成功
+    /// </summary>
+    Success = 0,
+
+    /// <summary>
+    /// 失败
+    /// </summary>
+    Fail = 1,
+
+    /// <summary>
+    /// 未知
+    /// </summary>
+    Unknown = 2,
+
+    /// <summary>
+    /// 连接达到上限
+    /// </summary>
+    ConnectionMax = 3
+}
+
+/// <summary>
+/// 连接到达上限
+/// </summary>
+[MessageTypeHandler(100000)]
+public partial class RespConnected : MessageObject, IResponseMessage
+{
+    /// <summary>
+    /// 错误
+    /// </summary>
+    [ProtoMember(1)]
+    public ErrorCode ErrorCode { get; set; }
+}
 
 /// <summary>
 /// 请求Cache连接 目标
