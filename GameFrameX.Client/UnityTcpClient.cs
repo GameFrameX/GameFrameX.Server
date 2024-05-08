419dc10d04ea88e1e9c07014ebe85cba4aef4037
Blank <wangfj11@foxmail.com>
2024-05-09 00:31:30 +0800

    [修改]1. 修改测试客户端的连接方式

--- GameFrameX.Client/UnityTcpClient.cs
+++ GameFrameX.Client/UnityTcpClient.cs
@@ -1,80 +1,65 @@
+using System.Net;
 using GameFrameX.Extension;
 using GameFrameX.NetWork.Messages;
 using GameFrameX.Proto;
 using GameFrameX.Proto.Proto;
 using GameFrameX.Serialize.Serialize;
 using GameFrameX.Utility;
-using TouchSocket.Core;
-using TouchSocket.Sockets;
+using SuperSocket.ClientEngine;
 
 namespace GameFrameX.Client;
-
-using ResultCode = TouchSocket.Core.ResultCode;
 
 public static class UnityTcpClient
 {
     public static async void Entry(string[] args)
     {
-        var tcpClient = new TcpClient();
-        tcpClient.Connected = (client, e) =>
-        {
-            Console.WriteLine("客户端成功连接到服务器");
-            return EasyTask.CompletedTask;
-        }; //成功连接到服务器
-        tcpClient.Disconnected = (client, e) =>
-        {
-            Console.WriteLine("客户端断开连接");
-            return EasyTask.CompletedTask;
-        }; //从服务器断开连接，当连接不成功时不会触发。
-        tcpClient.Received = (client, e) =>
+        var tcpClient = new AsyncTcpSession();
+        tcpClient.Connected += (client, e) => { Console.WriteLine("客户端成功连接到服务器"); }; //成功连接到服务器
+        tcpClient.Closed += (client, e) => { Console.WriteLine("客户端断开连接"); }; //从服务器断开连接，当连接不成功时不会触发。
+        tcpClient.DataReceived += (client, e) =>
         {
             //从服务器收到信息。但是一般byteBlock和requestInfo会根据适配器呈现不同的值。
             // var mes = Encoding.UTF8.GetString(e.ByteBlock.Buffer, 0, e.ByteBlock.Len);
-            int length = e.ByteBlock.ReadInt32(EndianType.Big);
-            long timestamp = e.ByteBlock.ReadInt64(EndianType.Big);
-            int messageId = e.ByteBlock.ReadInt32(EndianType.Big);
-            int offset = e.ByteBlock.Pos;
-            var messageData = e.ByteBlock.Buffer.ReadBytes(ref offset);
+            int offset = 0;
+            int length = e.Data.ReadInt(ref offset);
+            int uniqueId = e.Data.ReadInt(ref offset);
+            int messageId = e.Data.ReadInt(ref offset);
+            var messageData = e.Data.ReadBytes(ref offset);
             var messageType = ProtoMessageIdHandler.GetRespTypeById(messageId);
             if (messageType != null)
             {
                 var messageObject = (MessageObject)SerializerHelper.Deserialize(messageData, messageType);
                 messageObject.MessageId = messageId;
-                tcpClient.Logger.Info($"客户端接收到信息：{messageObject}");
+                Console.WriteLine($"客户端接收到信息：{messageObject}");
             }
-
-            return EasyTask.CompletedTask;
         };
-
-//载入配置
-        await tcpClient.SetupAsync(new TouchSocketConfig()
-            .SetRemoteIPHost("127.0.0.1:21000")
-            .ConfigureContainer(a =>
-            {
-                a.AddConsoleLogger(); //添加一个日志注入
-            }));
+        tcpClient.Error += (client, e) => { Console.WriteLine("客户端发生错误:" + e.Exception.Message); };
 
         while (true)
         {
             Thread.Sleep(5000);
 
-            if (!tcpClient.Online)
+            if (!tcpClient.IsConnected)
             {
                 Console.WriteLine("未链接到服务器,开启重连");
-                var result = tcpClient.TryConnect();
-                Console.WriteLine("链接到服务器结果：" + result);
-                if (result.ResultCode != ResultCode.Success)
-                {
-                    continue;
-                }
+                tcpClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 21000));
+                continue;
+                // Console.WriteLine("链接到服务器结果：" + result);
+                // if (result.ResultCode != ResultCode.Success)
+                // {
+                //     continue;
+                // }
             }
 
-            var buffer = GetBuffer();
-            tcpClient.Send(buffer);
+            for (int i = 0; i < 10; i++)
+            {
+                var buffer = GetBuffer();
+                tcpClient.Send(buffer);
+            }
         }
     }
 
-    public static byte[] GetBuffer()
+    private static byte[] GetBuffer()
     {
         ReqLogin req = new ReqLogin()
         {
