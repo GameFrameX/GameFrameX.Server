7f0943f63c4d6b6d83dff50a72c8759e0cb16ea2
Blank <wangfj11@foxmail.com>
2024-05-12 01:50:43 +0800

    [增加]1. 增加游戏的编码和解码器

---
+++ GameFrameX.Hotfix/Launcher/MessageGameDecoderHandler.cs
@@ -0,0 +1,51 @@
+using System.Buffers;
+using GameFrameX.NetWork;
+using GameFrameX.NetWork.Messages;
+using GameFrameX.Serialize.Serialize;
+using SuperSocket.ProtoBase;
+
+namespace GameFrameX.Hotfix.Launcher;
+
+/// <summary>
+/// 消息结构 4 + 4 + 4 + bytes.length
+/// </summary>
+public class MessageGameDecoderHandler : IMessageDecoderHandler, IPackageDecoder<IMessage>
+{
+    /// <summary>
+    /// 和客户端之间的消息 数据长度(4)+消息唯一ID(4)+消息ID(4)+消息内容
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public IMessage Handler(byte[] data)
+    {
+        try
+        {
+            int readOffset = 0;
+            var length = data.ReadInt(ref readOffset);
+            var uniqueId = data.ReadLong(ref readOffset);
+            var messageId = data.ReadInt(ref readOffset);
+            var messageData = data.ReadBytes(ref readOffset);
+            var messageType = ProtoMessageIdHandler.GetReqTypeById(messageId);
+            if (messageType != null)
+            {
+                var messageObject = (MessageObject)SerializerHelper.Deserialize(messageData, messageType);
+                messageObject.MessageId = messageId;
+                messageObject.UniqueId = uniqueId;
+                return messageObject;
+            }
+
+            LogHelper.Fatal("未知消息类型");
+            return null;
+        }
+        catch (Exception e)
+        {
+            LogHelper.Fatal(e);
+            return null;
+        }
+    }
+
+    public IMessage Decode(ref ReadOnlySequence<byte> buffer, object context)
+    {
+        return Handler(buffer.ToArray());
+    }
+}
