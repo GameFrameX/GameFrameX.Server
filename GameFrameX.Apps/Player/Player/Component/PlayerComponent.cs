bc8baecbcad8f4c21eeb0370cfc28df1c890e0bb
AlianBlank <wangfj11@foxmail.com>
2024-05-12 16:56:40 +0800

    [增加]1. 增加角色的创建获取和登录功能

--- GameFrameX.Apps/Player/Player/Component/PlayerComponent.cs
+++ GameFrameX.Apps/Player/Player/Component/PlayerComponent.cs
@@ -1,7 +1,35 @@
-﻿namespace GameFrameX.Apps.Player.Player.Component
+﻿using GameFrameX.Apps.Player.Player.Entity;
+using GameFrameX.DBServer;
+using GameFrameX.Setting;
+using GameFrameX.Utility;
+
+namespace GameFrameX.Apps.Player.Player.Component
 {
     [ComponentType(ActorType.Player)]
     public sealed class PlayerComponent : StateComponent<PlayerState>
     {
+        public async Task<List<PlayerState>?> GetPlayerList(ReqPlayerList reqPlayerList)
+        {
+            return await GameDb.FindListAsync<PlayerState>(m => m.AccountId == reqPlayerList.Id);
+        }
+
+        public async Task<PlayerState> OnPlayerCreate(ReqPlayerCreate reqPlayerCreate)
+        {
+            PlayerState playerState = new PlayerState
+            {
+                Id = IdGenerator.GetActorID(ActorType.Player),
+                AccountId = reqPlayerCreate.Id,
+                Name = reqPlayerCreate.Name,
+                Level = 0,
+                State = 0
+            };
+            await GameDb.SaveOneAsync<PlayerState>(playerState);
+            return playerState;
+        }
+
+        public async Task<PlayerState?> OnLogin(ReqPlayerLogin reqLogin)
+        {
+            return await GameDb.FindAsync<PlayerState>(m => m.Id == reqLogin.Id);
+        }
     }
 }
---
+++ GameFrameX.Apps/Player/Player/Entity/PlayerState.cs
@@ -0,0 +1,43 @@
+using GameFrameX.DBServer.State;
+
+namespace GameFrameX.Apps.Player.Player.Entity;
+
+public class PlayerState : CacheState
+{
+    public long RoleId => Id;
+
+    /// <summary>
+    /// 账号ID
+    /// </summary>
+    public long AccountId { get; set; }
+
+    /// <summary>
+    /// 玩家名
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 玩家等级
+    /// </summary>
+    public uint Level { get; set; }
+
+    /// <summary>
+    /// 性别
+    /// </summary>
+    public int Gender { get; set; }
+
+    /// <summary>
+    /// 玩家状态
+    /// </summary>
+    public int State { get; set; }
+
+    /// <summary>
+    /// 登录时间
+    /// </summary>
+    public DateTime LoginTime { get; set; }
+
+    /// <summary>
+    /// 离线时间
+    /// </summary>
+    public DateTime OfflineTime { get; set; }
+}
