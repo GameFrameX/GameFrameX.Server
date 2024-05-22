9e8d471e97e624793fdba9c7149af637dd7c14b1
AlianBlank <wangfj11@foxmail.com>
2024-05-22 11:55:18 +0800

    [修改]1. 修改发现中心的消息返回

--- GameFrameX.Launcher/StartUp/Discovery/MessageActorDiscoveryEncoderHandler.cs
+++ GameFrameX.Launcher/StartUp/Discovery/MessageActorDiscoveryEncoderHandler.cs
@@ -8,18 +8,32 @@ class MessageActorDiscoveryEncoderHandler : IPackageEncoder<IMessage>
     public byte[] Handler(IMessage message)
     {
         var bytes = SerializerHelper.Serialize(message);
+        var messageType = message.GetType();
+        long uniqueId = 0;
+        int msgId = 0;
+        if (message is MessageObject messageObject)
+        {
+            msgId = ProtoMessageIdHandler.GetRespMessageIdByType(messageType);
+            messageObject.MessageId = msgId;
+            uniqueId = messageObject.UniqueId;
+            bytes = SerializerHelper.Serialize(messageObject);
+        }
+        else if (message is MessageActorObject messageActorObject)
+        {
+            msgId = ProtoMessageIdHandler.GetResponseActorMessageIdByType(messageType);
+            messageActorObject.MessageId = msgId;
+            uniqueId = messageActorObject.UniqueId;
+            bytes = SerializerHelper.Serialize(messageActorObject);
+        }
 
         // len +timestamp + msgId + bytes.length
-        int len = 4 + 8 + 4 + 4 + 4 + bytes.Length;
+        int len = 4 + 8 + 4 + 4 + bytes.Length;
         var span = new byte[len];
         int offset = 0;
         span.WriteInt(len, ref offset);
-        span.WriteLong(TimeHelper.UnixTimeSeconds(), ref offset);
-        var messageType = message.GetType();
-        var msgId = ProtoMessageIdHandler.GetRespMessageIdByType(messageType);
+        span.WriteLong(uniqueId, ref offset);
         span.WriteInt(msgId, ref offset);
-        span.WriteInt(bytes.Length, ref offset);
-        span.WriteBytesWithoutLength(bytes, ref offset);
+        span.WriteBytes(bytes, ref offset);
         return span;
     }
 
