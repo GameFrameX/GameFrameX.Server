37c02499c23502e54894d5b210ed07a27158b64d
AlianBlank <wangfj11@foxmail.com>
2024-05-24 18:04:24 +0800

    [增加]1. 增加消息对象的主消息和子消息的字段

--- GameFrameX.NetWork/Messages/MessageTypeHandlerAttribute.cs
+++ GameFrameX.NetWork/Messages/MessageTypeHandlerAttribute.cs
@@ -12,6 +12,16 @@ public class MessageTypeHandlerAttribute : Attribute
     public int MessageId { get; }
 
     /// <summary>
+    /// 主消息ID
+    /// </summary>
+    public int MainId { get; }
+
+    /// <summary>
+    /// 子消息ID
+    /// </summary>
+    public int SubId { get; }
+
+    /// <summary>
     /// 构造消息类型处理器
     /// </summary>
     /// <param name="messageId">消息ID</param>
