b7e570db5725d7869d991be911fe04b7d189a6ea
AlianBlank <wangfj11@foxmail.com>
2024-05-27 11:24:50 +0800

    [优化]1. 抽象和发现中心的通讯基类

---
+++ GameFrameX.Launcher/StartUp/AppStartUpService.cs
@@ -0,0 +1,200 @@
+﻿using GameFrameX.Proto.BuiltIn;
+
+namespace GameFrameX.Launcher.StartUp;
+
+public abstract class AppStartUpService : AppStartUpBase
+{
+    /// <summary>
+    /// 链接到发现中心的客户端
+    /// </summary>
+    AsyncTcpSession _discoveryCenterClient;
+
+    protected RpcSession RpcSession { get; private set; }
+
+    /// <summary>
+    /// 从发现中心请求的目标服务器类型
+    /// </summary>
+    protected abstract ServerType GetServerType { get; }
+
+    /// <summary>
+    /// 连接的目标信息
+    /// </summary>
+    protected RespConnectServer ConnectTargetServer { get; private set; }
+
+    private readonly IMessageEncoderHandler _messageEncoderHandler;
+    private readonly IMessageDecoderHandler _messageDecoderHandler;
+
+    protected AppStartUpService(IMessageEncoderHandler messageEncoderHandler, IMessageDecoderHandler messageDecoderHandler)
+    {
+        RpcSession = new RpcSession();
+        _messageEncoderHandler = messageEncoderHandler;
+        _messageDecoderHandler = messageDecoderHandler;
+    }
+
+    public override Task EnterAsync()
+    {
+        StartDiscoveryCenterClient();
+        _ = Task.Run(RpcHandler);
+        return Task.CompletedTask;
+    }
+
+    private void RpcHandler()
+    {
+        while (true)
+        {
+            var message = RpcSession.Handler();
+            if (message == null || _discoveryCenterClient.IsConnected == false)
+            {
+                Thread.Sleep(1);
+                continue;
+            }
+
+            SendToDiscoveryCenterMessage(message.UniqueId, message.RequestMessage);
+        }
+    }
+
+    /// <summary>
+    /// 给发现中心发送消息
+    /// </summary>
+    /// <param name="messageUniqueId"></param>
+    /// <param name="message"></param>
+    protected void SendToDiscoveryCenterMessage(long messageUniqueId, IMessage message)
+    {
+        var span = _messageEncoderHandler.RpcHandler(messageUniqueId, message);
+        if (Setting.IsDebug && Setting.IsDebugSend)
+        {
+            LogHelper.Debug($"---发送[{ServerType} To {ServerType.DiscoveryCenter}] {message.ToMessageString()}");
+        }
+
+        _discoveryCenterClient.TrySend(span);
+    }
+
+    /// <summary>
+    /// 重连定时器
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected override void ReconnectionTimerOnElapsed(object sender, ElapsedEventArgs e)
+    {
+        // 重连到发现中心服务器
+        ConnectToDiscoveryCenter();
+    }
+
+    private void ConnectToDiscoveryCenter()
+    {
+        var endPoint = new IPEndPoint(IPAddress.Parse(Setting.DiscoveryCenterIp), Setting.DiscoveryCenterPort);
+        _discoveryCenterClient.Connect(endPoint);
+    }
+
+    private void StartDiscoveryCenterClient()
+    {
+        _discoveryCenterClient = new AsyncTcpSession();
+        _discoveryCenterClient.Closed += DiscoveryCenterClientOnClosed;
+        _discoveryCenterClient.DataReceived += DiscoveryCenterClientOnDataReceived;
+        _discoveryCenterClient.Connected += DiscoveryCenterClientOnConnected;
+        _discoveryCenterClient.Error += DiscoveryCenterClientOnError;
+
+        LogHelper.Info("开始链接到发现中心服务器 ...");
+        ConnectToDiscoveryCenter();
+    }
+
+
+    private void DiscoveryCenterClientOnError(object sender, ErrorEventArgs e)
+    {
+        LogHelper.Info("和发现中心服务器链接链接发生错误!" + e);
+        DiscoveryCenterClientOnClosed(_discoveryCenterClient, e);
+    }
+
+    private void DiscoveryCenterClientOnConnected(object sender, EventArgs e)
+    {
+        LogHelper.Info("和发现中心服务器链接链接成功!");
+        // 和网关服务器链接成功，关闭重连
+        ReconnectionTimer.Stop();
+        // 开启和网关服务器的心跳
+        HeartBeatTimer.Start();
+
+        if (ConnectTargetServer == null)
+        {
+            ReqConnectServer reqConnectServer = new ReqConnectServer
+            {
+                ServerType = GetServerType
+            };
+            SendToDiscoveryCenterMessage(reqConnectServer.UniqueId, reqConnectServer);
+        }
+    }
+
+    /// <summary>
+    /// 获取到连接的目标
+    /// </summary>
+    protected abstract void ConnectServerHandler();
+
+    /// <summary>
+    /// 连接的目标下线
+    /// </summary>
+    protected abstract void DisconnectServerHandler();
+
+    /// <summary>
+    /// 收到发现中心推送的非特殊消息
+    /// </summary>
+    /// <param name="message"></param>
+    protected abstract void DiscoveryCenterDataReceived(IMessage message);
+
+    private void DiscoveryCenterClientOnDataReceived(object o, DataEventArgs dataEventArgs)
+    {
+        var messageData = dataEventArgs.Data.ReadBytes(dataEventArgs.Offset, dataEventArgs.Length);
+        var message = _messageDecoderHandler.RpcHandler(messageData);
+        if (message == null)
+        {
+            LogHelper.Error("数据解析失败！");
+            return;
+        }
+
+        if (message is IActorResponseMessage actorResponseMessage)
+        {
+            RpcSession.Reply(actorResponseMessage);
+        }
+
+        if (message is RespConnectServer respConnectServer && ConnectTargetServer == null)
+        {
+            ConnectTargetServer = respConnectServer;
+            ConnectServerHandler();
+            return;
+        }
+
+        if (message is RespServerOfflineServer respServerOfflineServer)
+        {
+            if (respServerOfflineServer.ServerType == ConnectTargetServer?.ServerType && respServerOfflineServer.ServerID == ConnectTargetServer?.ServerID)
+            {
+                ConnectTargetServer = null;
+                DisconnectServerHandler();
+            }
+
+            return;
+        }
+
+        if (Setting.IsDebug && Setting.IsDebugReceive)
+        {
+            LogHelper.Debug($"---接收[{ServerType.DiscoveryCenter} To {ServerType}] {message.ToMessageString()}");
+        }
+
+        DiscoveryCenterDataReceived(message);
+    }
+
+    private void DiscoveryCenterClientOnClosed(object sender, EventArgs eventArgs)
+    {
+        LogHelper.Info("和网关服务器链接链接断开!");
+        // 和网关服务器链接断开，开启重连
+        ReconnectionTimer.Start();
+    }
+
+    protected void StopDiscoveryCenter()
+    {
+        HeartBeatTimer?.Close();
+        ReconnectionTimer?.Close();
+        if (_discoveryCenterClient != null)
+        {
+            _discoveryCenterClient.Close();
+            _discoveryCenterClient = null;
+        }
+    }
+}
