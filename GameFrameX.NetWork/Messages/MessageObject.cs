58d6bd7fd4dd9b4fb59f024c8fdfa2d85c3b32bd
AlianBlank <wangfj11@foxmail.com>
2024-05-28 17:44:52 +0800

    [修改]1. 修改消息的基类

--- GameFrameX.NetWork/Messages/MessageObject.cs
+++ GameFrameX.NetWork/Messages/MessageObject.cs
@@ -1,17 +1,41 @@
 ﻿using GameFrameX.Setting;
+using GameFrameX.Utility;
 using Newtonsoft.Json;
 using ProtoBuf;
 
 namespace GameFrameX.NetWork.Messages
 {
     [ProtoContract]
-    public abstract class MessageObject : BaseMessageObject, IMessage
+    public abstract class MessageObject : IMessage
     {
         /// <summary>
         /// 消息唯一id
         /// </summary>
         [JsonIgnore]
         public int UniId { get; set; }
+
+        /// <summary>
+        /// 消息ID
+        /// </summary>
+        [JsonIgnore]
+        [ProtoMember(998)]
+        public int MessageId { get; set; }
+
+        /// <summary>
+        /// 消息的唯一ID
+        /// </summary>
+        [ProtoMember(999)]
+        public long UniqueId { get; set; }
+
+        public MessageObject()
+        {
+            UpdateUniqueId();
+        }
+
+        public void UpdateUniqueId()
+        {
+            UniqueId = UtilityIdGenerator.GetNextUniqueId();
+        }
 
         public string ToSendMessageString(ServerType srcServerType, ServerType destServerType)
         {
@@ -21,6 +45,16 @@ namespace GameFrameX.NetWork.Messages
         public string ToReceiveMessageString(ServerType srcServerType, ServerType destServerType)
         {
             return $"---收到[{srcServerType} To {destServerType}] {ToMessageString()}";
+        }
+
+        public string ToMessageString()
+        {
+            return $"消息ID:[{MessageId},{GetType().Name}] 消息内容:{JsonHelper.Serialize(this)}";
+        }
+
+        public override string ToString()
+        {
+            return JsonHelper.Serialize(this);
         }
     }
 }
