919da7d5cf9421bcc3d07701fbbaeb8009613409
AlianBlank <wangfj11@foxmail.com>
2024-05-28 17:44:11 +0800

    [修改]1. 修改心跳的基础接口。

---
+++ GameFrameX.NetWork/Messages/IReqHeartBeatMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace GameFrameX.NetWork.Messages;
+
+/// <summary>
+/// 发送的心跳消息
+/// </summary>
+public interface IReqHeartBeatMessage;
--- GameFrameX.NetWork/Messages/IRespHeartBeatMessage.cs
+++ GameFrameX.NetWork/Messages/IRespHeartBeatMessage.cs
@@ -1,11 +1,6 @@
 ﻿namespace GameFrameX.NetWork.Messages;
 
 /// <summary>
-/// 发送的心跳消息
-/// </summary>
-public interface IReqHeartBeatMessage;
-
-/// <summary>
 /// 接收的心跳消息
 /// </summary>
 public interface IRespHeartBeatMessage;
