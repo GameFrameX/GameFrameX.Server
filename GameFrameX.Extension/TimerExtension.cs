d849dfb47dcaca8c261906359b8e7c57b9e00c49
AlianBlank <wangfj11@foxmail.com>
2024-05-28 10:35:41 +0800

    [增加]1. 增加计时器重置的接口

---
+++ GameFrameX.Extension/TimerExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace GameFrameX.Extension;
+
+public static class TimerExtension
+{
+    /// <summary>
+    /// 重置计时器
+    /// </summary>
+    /// <param name="timer"></param>
+    public static void Reset(this System.Timers.Timer timer)
+    {
+        timer.Stop();
+        timer.Start();
+    }
+}
