e5fa8a08c07cda999d709cd38cdd2237edaea34c
AlianBlank <wangfj11@foxmail.com>
2024-05-28 18:30:36 +0800

    [增加]1. 增加int 的UniqueID的生成

--- GameFrameX.Utility/UtilityIdGenerator.cs
+++ GameFrameX.Utility/UtilityIdGenerator.cs
@@ -5,10 +5,17 @@
 /// </summary>
 public static class UtilityIdGenerator
 {
-    private static readonly DateTime utcTimeStart = new(2022, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    private static readonly DateTime UtcTimeStart = new(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
     // 共享计数器
-    private static long _counter = (long)(DateTime.UtcNow - utcTimeStart).TotalSeconds;
+    private static long _longCounter = (long)(DateTime.UtcNow - UtcTimeStart).TotalSeconds;
+    private static int _intCounter = (int)(DateTime.UtcNow - UtcTimeStart).TotalSeconds;
+
+    public static int GetNextUniqueIntId()
+    {
+        // 原子性地递增值
+        return Interlocked.Increment(ref _intCounter);
+    }
 
     /// <summary>
     /// 使用Interlocked.Increment生成唯一ID的方法
@@ -17,7 +24,7 @@ public static class UtilityIdGenerator
     public static long GetNextUniqueId()
     {
         // 原子性地递增值
-        return Interlocked.Increment(ref _counter);
+        return Interlocked.Increment(ref _longCounter);
     }
 
     public static string GetUniqueIdString()
