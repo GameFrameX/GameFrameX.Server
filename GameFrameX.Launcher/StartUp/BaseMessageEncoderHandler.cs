374032cb80fcfff475eeb7c0a1cb495ed5a9f5e2
AlianBlank <wangfj11@foxmail.com>
2024-05-28 18:36:11 +0800

    [修改]1. 修改消息的编码规则

--- GameFrameX.Launcher/StartUp/BaseMessageEncoderHandler.cs
+++ GameFrameX.Launcher/StartUp/BaseMessageEncoderHandler.cs
@@ -6,25 +6,11 @@ namespace GameFrameX.Launcher.StartUp;
 public abstract class BaseMessageEncoderHandler : IMessageEncoderHandler, IPackageEncoder<IMessage>
 {
     /// <summary>
-    /// 获取Actor消息ID
-    /// </summary>
-    /// <param name="messageType"></param>
-    /// <returns></returns>
-    protected abstract int GetActorMessageId(Type messageType);
-
-    /// <summary>
     /// 获取消息ID
     /// </summary>
     /// <param name="messageType"></param>
     /// <returns></returns>
     protected abstract int GetMessageId(Type messageType);
-
-    /// <summary>
-    /// 获取Rpc消息ID
-    /// </summary>
-    /// <param name="messageType"></param>
-    /// <returns></returns>
-    protected abstract int GetRpcMessageId(Type messageType);
 
     /// <summary>
     /// 和客户端之间的消息 数据长度(4)+消息唯一ID(4)+消息ID(4)+消息内容
@@ -33,41 +19,27 @@ public abstract class BaseMessageEncoderHandler : IMessageEncoderHandler, IPacka
     /// <returns></returns>
     public byte[] Handler(IMessage message)
     {
-        byte[] bytes = null;
-        var messageType = message.GetType();
-        long uniqueId = 0;
-        int msgId = 0;
         if (message is MessageObject messageObject)
         {
-            msgId = GetMessageId(messageType);
+            var messageType = message.GetType();
+
+            var msgId = GetMessageId(messageType);
             messageObject.MessageId = msgId;
-            uniqueId = messageObject.UniqueId;
-            bytes = SerializerHelper.Serialize(messageObject);
-        }
-        else if (message is MessageActorObject messageActorObject)
-        {
-            msgId = GetActorMessageId(messageType);
-            messageActorObject.MessageId = msgId;
-            uniqueId = messageActorObject.UniqueId;
-            bytes = SerializerHelper.Serialize(messageActorObject);
-        }
-
-        if (bytes == null)
-        {
-            LogHelper.Error("消息对象为空，编码异常");
-            return null;
+            var uniqueId = messageObject.UniqueId;
+            var bytes = SerializerHelper.Serialize(messageObject);
+            // len +uniqueId + msgId + bytes.length
+            ushort len = (ushort)(2 + 4 + 4 + bytes.Length);
+            var span = new byte[len];
+            int offset = 0;
+            span.WriteUShort(len, ref offset);
+            span.WriteInt(uniqueId, ref offset);
+            span.WriteInt(msgId, ref offset);
+            span.WriteBytesWithoutLength(bytes, ref offset);
+            return span;
         }
 
-        // len +timestamp + msgId + bytes.length
-        int len = 4 + 8 + 4 + 4 + bytes.Length;
-        var span = new byte[len];
-        int offset = 0;
-        span.WriteInt(len, ref offset);
-        span.WriteLong(uniqueId, ref offset);
-
-        span.WriteInt(msgId, ref offset);
-        span.WriteBytes(bytes, ref offset);
-        return span;
+        LogHelper.Error("消息对象为空，编码异常");
+        return null;
     }
 
     /// <summary>
@@ -76,27 +48,22 @@ public abstract class BaseMessageEncoderHandler : IMessageEncoderHandler, IPacka
     /// <param name="messageUniqueId"></param>
     /// <param name="message"></param>
     /// <returns></returns>
-    public byte[] RpcHandler(long messageUniqueId, IMessage message)
+    /*public byte[] RpcHandler(int messageUniqueId, IMessage message)
     {
         var bytes = SerializerHelper.Serialize(message);
         // len + UniqueId + msgId + bytes.length
-        int len = 4 + 8 + 4 + 4 + bytes.Length;
+        ushort len = (ushort)(2 + 4 + 4 + bytes.Length);
         var span = new byte[len];
         int offset = 0;
-        span.WriteInt(len, ref offset);
-        span.WriteLong(messageUniqueId, ref offset);
+        span.WriteUShort(len, ref offset);
+        span.WriteInt(messageUniqueId, ref offset);
         var messageType = message.GetType();
         var msgId = GetRpcMessageId(messageType);
-        if (message is BaseMessageObject baseMessageObject)
-        {
-            baseMessageObject.MessageId = msgId;
-        }
-
+        message.MessageId = msgId;
         span.WriteInt(msgId, ref offset);
-        span.WriteBytes(bytes, ref offset);
+        span.WriteBytesWithoutLength(bytes, ref offset);
         return span;
-    }
-
+    }*/
     public int Encode(IBufferWriter<byte> writer, IMessage pack)
     {
         var bytes = Handler(pack);
