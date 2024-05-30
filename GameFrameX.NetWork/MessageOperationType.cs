1d5a13a9a33ad12506a17035d2672075ecbc424d
AlianBlank <wangfj11@foxmail.com>
2024-05-30 18:42:34 +0800

    [增加]1. 增加消息操作类型定义

---
+++ GameFrameX.NetWork/MessageOperationType.cs
@@ -0,0 +1,17 @@
+﻿namespace GameFrameX.NetWork;
+
+/// <summary>
+/// 消息操作业务类型
+/// </summary>
+public enum MessageOperationType : byte
+{
+    /// <summary>
+    /// 心跳
+    /// </summary>
+    HeartBeat = 1,
+
+    /// <summary>
+    /// 游戏
+    /// </summary>
+    Game = 2
+}
