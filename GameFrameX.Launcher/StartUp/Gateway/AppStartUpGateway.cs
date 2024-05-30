f797998ea48e9e8e8ae5623bb7a1d8859ae0a85a
AlianBlank <wangfj11@foxmail.com>
2024-05-30 09:46:14 +0800

    [删除]1. 删除网关的无效代码

--- GameFrameX.Launcher/StartUp/Gateway/AppStartUpGateway.cs
+++ GameFrameX.Launcher/StartUp/Gateway/AppStartUpGateway.cs
@@ -29,7 +29,6 @@ internal sealed partial class AppStartUpGateway : AppStartUpService
             // _namingServiceManager.OnServerRemove = OnServerRemove;
             _namingServiceManager.AddSelf(Setting);
             await base.EnterAsync();
-            StartGameClient();
             await AppExitToken;
         }
         catch (Exception e)
@@ -89,7 +88,6 @@ internal sealed partial class AppStartUpGateway : AppStartUpService
     private ValueTask OnConnected(IAppSession appSession)
     {
         LogHelper.Info("有客户端网络连接成功！。链接信息：SessionID:" + appSession.SessionID + " RemoteEndPoint:" + appSession.RemoteEndPoint);
-        // var gameSession = new GameSession(socketClient.IP, socketClient);
         var netChannel = new DefaultNetWorkChannel(appSession, messageEncoderHandler, RpcSession);
         GameClientSessionManager.SetSession(appSession.SessionID, netChannel); //移除
         return ValueTask.CompletedTask;
@@ -149,15 +147,6 @@ internal sealed partial class AppStartUpGateway : AppStartUpService
                     }
                 }
             }
-            /*if (message is MessageObject messageActorObject)
-            {
-                // 发送
-                var response = new RespActorHeartBeat()
-                {
-                    Timestamp = TimeHelper.UnixTimeSeconds()
-                };
-                SendMessage(session, response);
-            }*/
         }
 
         return ValueTask.CompletedTask;
@@ -183,69 +172,6 @@ internal sealed partial class AppStartUpGateway : AppStartUpService
 
             await session.SendAsync(result);
         }
-    }
-
-    private void StartGameClient()
-    {
-        // _gameClientList = new AsyncTcpSession();
-        // _gameClientList.Connected += GameClientOnConnected;
-        // _gameClientList.Closed += GameClientOnClosed;
-        // _gameClientList.DataReceived += GameClientOnDataReceived;
-        // _gameClientList.Error += GameClientOnError;
-    }
-
-    private void ConnectToGame()
-    {
-        // _gameClientList.Connect(new DnsEndPoint(Setting.DiscoveryCenterIp, Setting.DiscoveryCenterPort));
-    }
-
-    private void GameClientOnDataReceived(object sender, DataEventArgs e)
-    {
-        var messageObject = messageDecoderHandler.Handler(e.Data.ReadBytes(e.Offset, e.Length));
-        if (messageObject == null)
-        {
-            LogHelper.Error("数据解析失败：" + e.Data.ReadBytes(e.Offset, e.Length));
-            return;
-        }
-
-        if (Setting.IsDebug && Setting.IsDebugReceive)
-        {
-            LogHelper.Debug($"---收到[{ServerType}] {messageObject.ToMessageString()}");
-        }
-    }
-
-    private void GameClientOnConnected(object sender, EventArgs e)
-    {
-        LogHelper.Info("和中心服务器链接成功, 开始心跳");
-        HeartBeatTimer.Start();
-        ReconnectionTimer.Stop();
-        // 这里要注册到发现中心
-        ReqRegisterServer reqRegisterServer = new ReqRegisterServer
-        {
-            ServerID = Setting.ServerId,
-            ServerType = Setting.ServerType,
-            ServerName = Setting.ServerName,
-            InnerIP = Setting.InnerIp,
-            InnerPort = Setting.InnerPort,
-            OuterIP = Setting.OuterIp,
-            OuterPort = Setting.OuterPort
-        };
-        SendToDiscoveryCenterMessage(reqRegisterServer);
-    }
-
-    private void GameClientOnClosed(object sender, EventArgs eventArgs)
-    {
-        LogHelper.Info("和游戏服务器网络连接断开, 开始重连：断开信息:" + eventArgs);
-        HeartBeatTimer.Stop();
-        ReconnectionTimer.Start();
-    }
-
-    private void GameClientOnError(object sender, SuperSocket.ClientEngine.ErrorEventArgs errorEventArgs)
-    {
-        LogHelper.Info("和游戏服务器连接错误, 开始重连:错误信息：" + errorEventArgs.Exception);
-        // 开启重连
-        HeartBeatTimer.Stop();
-        ReconnectionTimer.Start();
     }
 
     protected override void Init()
