07c7ecee967da635386aca64437ad0b6cb26e82f
AlianBlank <wangfj11@foxmail.com>
2024-05-30 18:43:32 +0800

    [增加]1. 增加内部消息基础接口

---
+++ GameFrameX.NetWork/Messages/IInnerMessage.cs
@@ -0,0 +1,25 @@
+﻿namespace GameFrameX.NetWork.Messages;
+
+public interface IInnerMessage : IMessage
+{
+    /// <summary>
+    /// 消息操作业务类型
+    /// </summary>
+    MessageOperationType OperationType { get; }
+
+    /// <summary>
+    /// 消息数据
+    /// </summary>
+    byte[] MessageData { get; }
+
+    /// <summary>
+    /// 消息类型
+    /// </summary>
+    Type MessageType { get; }
+
+    /// <summary>
+    /// 转换消息数据为消息对象
+    /// </summary>
+    /// <returns></returns>
+    MessageObject DeserializeMessageObject();
+}
