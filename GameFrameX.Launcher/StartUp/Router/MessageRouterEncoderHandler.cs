62176afe00835ab471a7b591edd3a6f8e7a1137a
AlianBlank <wangfj11@foxmail.com>
2024-05-31 18:03:55 +0800

    [增加]1. 增加路由的内部消息处理

--- GameFrameX.Launcher/StartUp/Router/MessageRouterEncoderHandler.cs
+++ GameFrameX.Launcher/StartUp/Router/MessageRouterEncoderHandler.cs
@@ -9,6 +9,25 @@ class MessageRouterEncoderHandler : BaseMessageEncoderHandler
     /// </summary>
     /// <param name="message"></param>
     /// <returns></returns>
+    public byte[] InnerHandler(IInnerMessage message)
+    {
+        var msgId = MessageProtoHelper.GetMessageIdByType(message.MessageType);
+        ushort len = (ushort)(PackageLength + message.MessageDataLength);
+        var buffer = new byte[len];
+        int offset = 0;
+        buffer.WriteUShort(len, ref offset);
+        buffer.WriteByte((byte)message.OperationType, ref offset);
+        buffer.WriteInt(message.UniqueId, ref offset);
+        buffer.WriteInt(msgId, ref offset);
+        buffer.WriteBytesWithoutLength(message.MessageData, ref offset);
+        return buffer;
+    }
+
+    /// <summary>
+    /// 和客户端之间的消息 数据长度(4)+消息唯一ID(4)+消息ID(4)+消息内容
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
     public override byte[] Handler(IMessage message)
     {
         if (message is MessageObject messageObject)
