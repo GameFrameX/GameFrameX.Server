71202291f0444424b6bf76420cc18c4ce7a1dcd3
AlianBlank <wangfj11@foxmail.com>
2024-06-04 16:12:01 +0800

    [增加]1. 增加消息指令的接口和实现

---
+++ GameFrameX.NetWork/Interface/ICommandInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace GameFrameX.NetWork.Interface;
+
+/// <summary>
+/// 消息指令信息
+/// </summary>
+public interface ICommandInfo
+{
+    /// <summary>
+    /// 合并后的消息ID
+    /// </summary>
+    int MessageId { get; }
+
+    /// <summary>
+    /// 主消息ID
+    /// </summary>
+    int MainId { get; }
+
+    /// <summary>
+    /// 次消息ID
+    /// </summary>
+    int SubId { get; }
+}
