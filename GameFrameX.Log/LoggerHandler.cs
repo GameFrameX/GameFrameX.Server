d713296c4f7edf6bb3eff0a61b506945bdb91a97
AlianBlank <wangfj11@foxmail.com>
2024-07-03 21:19:55 +0800

    [增加]1. 增加日志程序集的日志打印规则

--- GameFrameX.Log/LoggerHandler.cs
+++ GameFrameX.Log/LoggerHandler.cs
@@ -4,24 +4,38 @@ namespace GameFrameX.Log;
 
 public static class LoggerHandler
 {
-    public static bool Start(string serverType = null)
+    public static bool Start(string serverType = null, string logSavePath = null, bool isDebug = false, RollingInterval rollingInterval = RollingInterval.Hour)
     {
         try
         {
-            var logPath = $"./logs/{serverType}_log_.log"; // 日志文件存储的路径
+            // 日志文件存储的路径
+            string logPath = "./logs/";
+            string logFileName = $"{serverType ?? "Server"}_log.log";
+            if (logSavePath != null)
+            {
+                logPath = Path.Combine(logSavePath, logFileName);
+            }
+            else
+            {
+                logPath = Path.Combine(logPath, logFileName);
+            }
+
             Console.WriteLine("初始化日志系统配置 开始...");
-            Serilog.Log.Logger = new LoggerConfiguration()
+
+            var logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
                 .MinimumLevel.Debug()
-#if DEBUG
-                .WriteTo.Console()
-#endif
                 .WriteTo.File(logPath,
-                    rollingInterval: RollingInterval.Hour,
+                    rollingInterval: rollingInterval,
                     rollOnFileSizeLimit: true,
-                    fileSizeLimitBytes: 10 * 1024 * 1024)
-                .CreateLogger();
-            Console.WriteLine("初始化日志系统配置 结束...");
+                    fileSizeLimitBytes: 10 * 1024 * 1024);
+            if (isDebug)
+            {
+                logger.WriteTo.Console();
+            }
+
+            Serilog.Log.Logger = logger.CreateLogger();
+            Console.WriteLine($"初始化日志系统配置 结束...,日志文件路径：{logPath} 存档周期：{rollingInterval}");
             return true;
         }
         catch (Exception e)
