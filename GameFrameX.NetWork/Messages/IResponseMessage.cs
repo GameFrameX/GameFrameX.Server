4b5795876f84a54541418c0785388b42f4ba5b5f
AlianBlank <wangfj11@foxmail.com>
2024-07-04 20:45:38 +0800

    [修改]1. 修改错误的设置权限

--- GameFrameX.NetWork/Messages/IResponseMessage.cs
+++ GameFrameX.NetWork/Messages/IResponseMessage.cs
@@ -1,9 +1,12 @@
 namespace GameFrameX.NetWork.Messages;
 
+/// <summary>
+/// 响应消息
+/// </summary>
 public interface IResponseMessage : IMessage
 {
     /// <summary>
     /// 错误码，非 0 表示错误
     /// </summary>
-    int ErrorCode { get; }
+    int ErrorCode { get; set; }
 }
