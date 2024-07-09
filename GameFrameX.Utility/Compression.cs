fef29310ce5eca12200d2a83a4d5a259263807f4
AlianBlank <wangfj11@foxmail.com>
2024-07-09 17:30:16 +0800

    [增加]1. 增加压缩工具库

---
+++ GameFrameX.Utility/Compression.cs
@@ -0,0 +1,41 @@
+﻿using System.IO.Compression;
+
+namespace GameFrameX.Utility
+{
+    /// <summary>
+    /// 压缩解压缩辅助器。
+    /// </summary>
+    public static class Compression
+    {
+        /// <summary>
+        /// 压缩数据。
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static byte[] Compress(byte[] bytes)
+        {
+            var uncompressed = new MemoryStream(bytes);
+            var compressed = new MemoryStream();
+            var deflateStream = new DeflateStream(compressed, CompressionMode.Compress);
+            uncompressed.CopyTo(deflateStream);
+            deflateStream.Close();
+            return compressed.ToArray();
+        }
+
+
+        /// <summary>
+        /// 解压数据。
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static byte[] Decompress(byte[] bytes)
+        {
+            var compressed = new MemoryStream(bytes);
+            var decompressed = new MemoryStream();
+            var deflateStream = new DeflateStream(compressed, CompressionMode.Decompress); // 注意： 这里第一个参数同样是填写压缩的数据，但是这次是作为输入的数据
+            deflateStream.CopyTo(decompressed);
+            byte[] result = decompressed.ToArray();
+            return result;
+        }
+    }
+}
---
+++ Tests/GameFrameX.Tests/CompressTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace GameFrameX.Tests;
+
+public class CompressTest
+{
+    private byte[] bytes;
+    private byte[] comData;
+
+    [SetUp]
+    public void Setup()
+    {
+        bytes = Encoding.UTF8.GetBytes("qwertyuiopasdfghjklzxcvbnmuiopasddfghjklzdfghjklzfuiopasdfdfghjklzdfghjklzuidfghjklzdfghjklzopasdfuidfghjklzdfghjklzodfghjklzdfghjklzpasdfuiopasdfuidfghjklzdfghjklzopasdfuiopasdfuiopasdfuiopasdf");
+    }
+
+    [Test]
+    public void compress()
+    {
+        var comData = Utility.Compression.Compress(bytes);
+        Console.WriteLine(comData);
+    }
+
+    [Test]
+    public void decompress()
+    {
+        comData = Utility.Compression.Compress(bytes);
+        var data = Utility.Compression.Decompress(comData);
+        var x = Encoding.UTF8.GetString(data);
+        Console.WriteLine(x);
+    }
+}
