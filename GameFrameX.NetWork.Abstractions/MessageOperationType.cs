072219c6a55abf6583c4f24ad17a75ac8c04de01
AlianBlank <wangfj11@foxmail.com>
2024-07-11 19:48:33 +0800

    [修改]1. 修改消息操作类型的所属位置

---
+++ GameFrameX.NetWork.Abstractions/MessageOperationType.cs
@@ -0,0 +1,77 @@
+﻿namespace GameFrameX.NetWork.Abstractions;
+
+/// <summary>
+/// 消息操作业务类型
+/// </summary>
+public enum MessageOperationType : byte
+{
+    /// <summary>
+    /// 空类型
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// 心跳
+    /// </summary>
+    HeartBeat,
+
+    /// <summary>
+    /// 缓存查询
+    /// </summary>
+    Cache,
+
+    /// <summary>
+    /// 数据库查询
+    /// </summary>
+    Database,
+
+    /// <summary>
+    /// 游戏
+    /// </summary>
+    Game,
+
+    /// <summary>
+    /// 游戏管理
+    /// </summary>
+    GameManager,
+
+    /// <summary>
+    /// 禁止
+    /// </summary>
+    Forbid,
+
+    /// <summary>
+    /// 重启
+    /// </summary>
+    Reboot,
+
+    /// <summary>
+    /// 重连
+    /// </summary>
+    Reconnect,
+
+    /// <summary>
+    /// 重载
+    /// </summary>
+    Reload,
+
+    /// <summary>
+    /// 退出
+    /// </summary>
+    Exit,
+
+    /// <summary>
+    /// 踢人
+    /// </summary>
+    Kick,
+
+    /// <summary>
+    /// 通知
+    /// </summary>
+    Notify,
+
+    /// <summary>
+    /// 转发
+    /// </summary>
+    Forward,
+}
