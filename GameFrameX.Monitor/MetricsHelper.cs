af8f472d5eb8ede2f95b4da330d221ddeae10b7e
AlianBlank <wangfj11@foxmail.com>
2024-07-18 20:12:14 +0800

    [修改]1. 修改监控帮助类的注释

--- GameFrameX.Monitor/MetricsHelper.cs
+++ GameFrameX.Monitor/MetricsHelper.cs
@@ -3,10 +3,16 @@ using Prometheus;
 
 namespace GameFrameX.Monitor;
 
+/// <summary>
+/// 监控帮助类
+/// </summary>
 public static class MetricsHelper
 {
     private static KestrelMetricServer _server;
 
+    /// <summary>
+    /// 停止监控
+    /// </summary>
     public static void Stop()
     {
         _server.Stop();
@@ -19,32 +25,32 @@ public static class MetricsHelper
     public static void Start(int port = 29090)
     {
         Metrics.SuppressDefaultMetrics(new SuppressDefaultMetricOptions
-        {
-            SuppressEventCounters = true,
-            SuppressMeters = true,
-            SuppressProcessMetrics = true
-        });
+                                       {
+                                           SuppressEventCounters  = true,
+                                           SuppressMeters         = true,
+                                           SuppressProcessMetrics = true
+                                       });
         _server = new KestrelMetricServer(port);
         _server.Start();
 
         var totalSleepTime = Prometheus.Metrics.CreateCounter("sample_sleep_seconds_total", "Total amount of time spent sleeping.");
 
         _ = Task.Run(async delegate
-        {
-            while (true)
-            {
-                // DEFAULT EXEMPLAR: We expose the trace_id and span_id for distributed tracing, based on Activity.Current.
-                // Activity.Current is often automatically inherited from incoming HTTP requests if using OpenTelemetry tracing with ASP.NET Core.
-                // Here, we manually create and start an activity for sample purposes, without relying on the platform managing the activity context.
-                // See https://learn.microsoft.com/en-us/dotnet/core/diagnostics/distributed-tracing-concepts
-                using var activity = new Activity("Pausing before record processing").Start();
-                var sleepStopwatch = Stopwatch.StartNew();
-                await Task.Delay(TimeSpan.FromSeconds(1));
+                     {
+                         while (true)
+                         {
+                             // DEFAULT EXEMPLAR: We expose the trace_id and span_id for distributed tracing, based on Activity.Current.
+                             // Activity.Current is often automatically inherited from incoming HTTP requests if using OpenTelemetry tracing with ASP.NET Core.
+                             // Here, we manually create and start an activity for sample purposes, without relying on the platform managing the activity context.
+                             // See https://learn.microsoft.com/en-us/dotnet/core/diagnostics/distributed-tracing-concepts
+                             using var activity       = new Activity("Pausing before record processing").Start();
+                             var       sleepStopwatch = Stopwatch.StartNew();
+                             await Task.Delay(TimeSpan.FromSeconds(1));
 
-                // The trace_id and span_id from the current Activity are exposed as the exemplar by default.
-                totalSleepTime.Inc(sleepStopwatch.Elapsed.TotalSeconds);
-            }
-        });
+                             // The trace_id and span_id from the current Activity are exposed as the exemplar by default.
+                             totalSleepTime.Inc(sleepStopwatch.Elapsed.TotalSeconds);
+                         }
+                     });
         Console.WriteLine($"Open http://localhost:{port}/metrics?accept=application/openmetrics-text in a web browser.");
     }
 }
