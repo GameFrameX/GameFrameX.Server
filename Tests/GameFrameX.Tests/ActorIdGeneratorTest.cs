da2917e97e828a7890f360214a90c1788963d905
AlianBlank <wangfj11@foxmail.com>
2024-07-28 19:11:24 +0800

    [增加]1. 增加ActorID的单元测试

---
+++ Tests/GameFrameX.Tests/ActorIdGeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using GameFrameX.Core.Abstractions;
+using GameFrameX.Core.Utility;
+
+namespace GameFrameX.Tests;
+
+public class ActorIdGeneratorTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void compress()
+    {
+        var actorId = ActorIdGenerator.GetActorId(ActorType.World, 1000);
+
+        var type     = ActorIdGenerator.GetActorType(actorId);
+        var serverId = ActorIdGenerator.GetServerId(actorId);
+
+        Assert.That(type == ActorType.World, Is.EqualTo(true));
+        Assert.That(serverId == 1000, Is.EqualTo(true));
+    }
+}
