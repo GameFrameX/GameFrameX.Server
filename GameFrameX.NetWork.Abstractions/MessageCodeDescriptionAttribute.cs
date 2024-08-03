59eb5d0dd8fa36b3437eb481f73e18d308fe8499
Blank <wangfj11@foxmail.com>
2024-08-03 23:19:34 +0800

    [增加]1. 增加消息码描述属性

---
+++ GameFrameX.NetWork.Abstractions/MessageCodeDescriptionAttribute.cs
@@ -0,0 +1,22 @@
+namespace GameFrameX.NetWork.Abstractions;
+
+/// <summary>
+/// 消息错误码的描述
+/// </summary>
+[AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field | AttributeTargets.Property)]
+public sealed class MessageCodeDescriptionAttribute : Attribute
+{
+    /// <summary>
+    /// 消息错误码的描述
+    /// </summary>
+    public string Description { get; }
+
+    /// <summary>
+    /// 构造消息错误码的描述
+    /// </summary>
+    /// <param name="description">错误码描述</param>
+    public MessageCodeDescriptionAttribute(string description)
+    {
+        Description = description;
+    }
+}
