8812f0c89500a7c9fc6791d31ad02a556fd1beb5
Blank <wangfj11@foxmail.com>
2024-08-21 19:35:31 +0800

    [增加]1. 增加玩家组件处理器

---
+++ GameFrameX.Core/BaseHandler/PlayerComponentHandler.cs
@@ -0,0 +1,47 @@
+using GameFrameX.Core.Abstractions.Agent;
+using GameFrameX.Setting;
+
+namespace GameFrameX.Core.BaseHandler;
+
+/// <summary>
+/// 玩家组件处理器
+/// </summary>
+public abstract class PlayerComponentHandler : BaseComponentHandler
+{
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InitActor()
+    {
+        if (ActorId <= 0)
+        {
+            ActorId = NetWorkChannel.GetData<long>(GlobalConst.ActorIdKey);
+        }
+
+        return Task.CompletedTask;
+    }
+}
+
+/// <summary>
+/// 玩家组件处理器
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class PlayerComponentHandler<T> : PlayerComponentHandler where T : IComponentAgent
+{
+    /// <summary>
+    /// 组件代理类型
+    /// </summary>
+    protected override Type ComponentAgentType
+    {
+        get { return typeof(T); }
+    }
+
+    /// <summary>
+    /// 缓存组件代理对象
+    /// </summary>
+    protected T ComponentAgent
+    {
+        get { return (T)CacheComponent; }
+    }
+}
