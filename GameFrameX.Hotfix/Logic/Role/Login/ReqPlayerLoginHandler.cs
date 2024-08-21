0f203f79f9ca468a45c11a8733989aeb8d49a5ca
Blank <wangfj11@foxmail.com>
2024-08-21 19:39:34 +0800

    [修改]1. 修改Player 对象引用对象错误的问题

--- GameFrameX.Hotfix/Logic/Role/Login/ReqPlayerLoginHandler.cs
+++ GameFrameX.Hotfix/Logic/Role/Login/ReqPlayerLoginHandler.cs
@@ -4,7 +4,7 @@ using GameFrameX.NetWork.Messages;
 namespace GameFrameX.Hotfix.Logic.Role.Login
 {
     [MessageMapping(typeof(ReqPlayerLogin))]
-    internal class ReqPlayerLoginHandler : GlobalComponentHandler<PlayerComponentAgent>
+    internal class ReqPlayerLoginHandler : PlayerComponentHandler<PlayerComponentAgent>
     {
         protected override async Task ActionAsync()
         {
