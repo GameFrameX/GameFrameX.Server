0d02c334508116f8f682a3ba7854f233659d8c93
Blank <wangfj11@foxmail.com>
2024-08-23 01:00:17 +0800

    [增加]1. 增加序列化JSON的时候格式化函数接口

--- GameFrameX.Utility/JsonHelper.cs
+++ GameFrameX.Utility/JsonHelper.cs
@@ -20,6 +20,18 @@ public static class JsonHelper
     }
 
     /// <summary>
+    /// 序列化对象且格式化JSON
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public static string SerializeFormat(object obj)
+    {
+        obj.CheckNotNull(nameof(obj));
+        string json = Newtonsoft.Json.JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented);
+        return json;
+    }
+
+    /// <summary>
     /// 反序列化对象
     /// </summary>
     /// <param name="json"></param>
