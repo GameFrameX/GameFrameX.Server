bda28632a05746a949269813d57167b7f7699794
Blank <wangfj11@foxmail.com>
2024-08-23 01:13:00 +0800

    [增加]1. 增加服务状态

--- GameFrameX.ServerManager/ServiceStatus.cs
+++ GameFrameX.ServerManager/ServiceStatus.cs
@@ -6,17 +6,42 @@ namespace GameFrameX.ServerManager;
 public enum ServiceStatus
 {
     /// <summary>
+    /// 未知  
+    /// </summary>
+    Unknown,
+
+    /// <summary>
     /// 在线
     /// </summary>
     Online,
 
     /// <summary>
+    /// 重连中
+    /// </summary>
+    Reconnecting,
+
+    /// <summary>
+    /// 维护
+    /// </summary>
+    Maintenance,
+
+    /// <summary>
+    /// 待命
+    /// </summary>
+    Standby,
+
+    /// <summary>
+    /// 重启
+    /// </summary>
+    Reboot,
+
+    /// <summary>
+    /// 备用
+    /// </summary>
+    Backup,
+
+    /// <summary>
     /// 离线
     /// </summary>
     Offline,
-
-    /// <summary>
-    /// 未知  
-    /// </summary>
-    Unknown
 }
