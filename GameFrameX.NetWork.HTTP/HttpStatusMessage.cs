52865aa75981cc23de7c0714ec4d74731ed05d47
Blank <wangfj11@foxmail.com>
2024-08-23 10:34:15 +0800

    [增加]1. 增加新的消息状态定义

--- GameFrameX.NetWork.HTTP/HttpStatusMessage.cs
+++ GameFrameX.NetWork.HTTP/HttpStatusMessage.cs
@@ -9,4 +9,24 @@ public static class HttpStatusMessage
     /// 成功
     /// </summary>
     public const string Success = "ok";
+
+    /// <summary>
+    /// 未定义的命令
+    /// </summary>
+    public const string UndefinedCommand = "undefined command";
+
+    /// <summary>
+    /// 未找到的命令
+    /// </summary>
+    public const string NotFoundCommand = "not found command";
+
+    /// <summary>
+    /// 验证失败的命令
+    /// </summary>
+    public const string CheckFailedCommand = "check failed command";
+
+    /// <summary>
+    /// 服务器错误
+    /// </summary>
+    public const string ServerError = "server error";
 }
