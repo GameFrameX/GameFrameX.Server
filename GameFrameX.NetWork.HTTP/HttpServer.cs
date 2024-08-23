d907afcd86aa23946a41233a303224e5aabfb0d3
Blank <wangfj11@foxmail.com>
2024-08-23 10:44:00 +0800

    [修改]1. 修改根路径可以从外部传入
    2. 增加HTTP端口的有效值判断.非法则强制为28080.3. 增加日志记录等级的外部传入

--- GameFrameX.NetWork.HTTP/HttpServer.cs
+++ GameFrameX.NetWork.HTTP/HttpServer.cs
@@ -1,3 +1,4 @@
+using GameFrameX.Extension;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
@@ -18,48 +19,82 @@ namespace GameFrameX.NetWork.HTTP
         /// <summary>
         /// 游戏API根地址
         /// </summary>
-        public const string GameApiPath = "/game/api/";
+        private const string GameApiPath = "/game/api/";
+
+        /// <summary>
+        /// API根地址
+        /// </summary>
+        public static string ApiRootPath { get; private set; }
 
         /// <summary>
         /// 启动
         /// </summary>
-        /// <param name="httpPort">HTTP端口</param>
-        /// <param name="httpsPort">HTTPS端口</param>
+        /// <param name="httpPort">HTTP端口,如果没有指定端口，则默认为28080</param>
+        /// <param name="httpsPort">HTTPS端口,如果没有指定端口，则不监听HTTPS</param>
         /// <param name="baseHandler">根据命令Id获得处理器</param>
-        public static Task Start(int httpPort, int httpsPort, Func<string, BaseHttpHandler> baseHandler)
+        /// <param name="apiRootPath">接口根路径,必须以 / 开头和以 / 结尾,默认为 [/game/api]</param>
+        /// <param name="minimumLevelLogLevel">日志记录最小级别</param>
+        public static Task Start(int httpPort, int httpsPort, Func<string, BaseHttpHandler> baseHandler, string apiRootPath = GameApiPath, LogLevel minimumLevelLogLevel = LogLevel.Debug)
         {
+            baseHandler.CheckNotNull(nameof(baseHandler));
+
+            // 如果没有指定端口，则默认为28080
+            if (httpPort <= 0)
+            {
+                httpPort = 28080;
+            }
+
+            // 如果没有指定根路径，则默认为/game/api/
+            if (apiRootPath.IsNullOrEmpty())
+            {
+                apiRootPath = GameApiPath;
+            }
+
+            // 根路径必须以/开头和以/结尾
+            if (!apiRootPath.StartsWith("/"))
+            {
+                apiRootPath = "/" + apiRootPath;
+            }
+
+            if (!apiRootPath.EndsWith("/"))
+            {
+                apiRootPath += "/";
+            }
+
+            ApiRootPath = apiRootPath;
             var builder = WebApplication.CreateBuilder();
             builder.WebHost.UseKestrel(options =>
-                                       {
-                                           // HTTP 
-                                           if (httpPort > 0)
-                                           {
-                                               options.ListenAnyIP(httpPort);
-                                           }
+                   {
+                       // HTTP 
+                       if (httpPort > 0)
+                       {
+                           options.ListenAnyIP(httpPort);
+                       }
 
-                                           // HTTPS
-                                           if (httpsPort > 0)
-                                           {
-                                               options.ListenAnyIP(httpsPort, listenOptions => { listenOptions.UseHttps(); });
-                                           }
-                                       })
-                   .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug); })
+                       // HTTPS
+                       if (httpsPort > 0)
+                       {
+                           options.ListenAnyIP(httpsPort, listenOptions => { listenOptions.UseHttps(); });
+                       }
+                   })
+                   .ConfigureLogging(logging => { logging.SetMinimumLevel(minimumLevelLogLevel); })
                    .UseSerilog();
 
             App = builder.Build();
             App.UseExceptionHandler((errorContext) =>
-                                    {
-                                        errorContext.Run(async (context) =>
-                                                         {
-                                                             // 获取异常信息
-                                                             var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+            {
+                errorContext.Run(async (context) =>
+                {
+                    // 获取异常信息
+                    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
 
-                                                             // 自定义返回Json信息；
-                                                             await context.Response.WriteAsync(exceptionHandlerPathFeature.Error.Message);
-                                                         });
-                                    });
-            App.MapGet(GameApiPath + "{text}", context => HttpHandler.HandleRequest(context, baseHandler));
-            App.MapPost(GameApiPath + "{text}", context => HttpHandler.HandleRequest(context, baseHandler));
+                    // 自定义返回Json信息；
+                    await context.Response.WriteAsync(exceptionHandlerPathFeature.Error.Message);
+                });
+            });
+            string routePath = $"{ApiRootPath}{{text}}";
+            App.MapGet(routePath, context => HttpHandler.HandleRequest(context, baseHandler));
+            App.MapPost(routePath, context => HttpHandler.HandleRequest(context, baseHandler));
             return App.StartAsync();
         }
 
