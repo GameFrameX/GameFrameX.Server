a2180ab1d67cc8e36dead2a95b561a642fab4569
chenxin16 <chenxin16@hisense.com>
2024-08-24 14:52:51 +0800

    [删除]1. 删除手写的Wrapper代理类

--- GameFrameX.Hotfix/Logic/Role/Role/RoleComponentAgentWrapper.cs
+++
@@ -1,7 +0,0 @@
-// using GameFrameX.Hotfix.Player.Role.Role.Agent;
-//
-// namespace GameFrameX.Hotfix.Player.Role.Role.Warpper;
-//
-// public class RoleComponentAgentWrapper : RoleComponentAgent
-// {
-// }
