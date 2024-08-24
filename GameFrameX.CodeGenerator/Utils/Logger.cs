8e067f0be2311467a5e823c6034e4cabf11aa741
chenxin16 <chenxin16@hisense.com>
2024-08-24 14:53:50 +0800

    [增加]1. 增加启Wrapper代理类自动生成模块

---
+++ GameFrameX.CodeGenerator/Utils/Logger.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace GameFrameX.CodeGenerator.Utils
+{
+    public static class Logger
+    {
+        public static void LogError(this GeneratorExecutionContext context, string msg)
+        {
+            DiagnosticDescriptor invalidXmlWarning = new DiagnosticDescriptor(id: "Error",
+                                                                                               title: "Code Generator Error",
+                                                                                               messageFormat: "{0}",
+                                                                                               category: "CodeGenerator",
+                                                                                               DiagnosticSeverity.Error,
+                                                                                               isEnabledByDefault: true);
+            context.ReportDiagnostic(Diagnostic.Create(invalidXmlWarning, Location.None, msg));
+        }
+    }
+}
