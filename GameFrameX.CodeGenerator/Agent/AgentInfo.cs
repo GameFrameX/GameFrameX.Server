790977bb416f5f928f56d3db1e1e4c9197e376ce
Blank <wangfj11@foxmail.com>
2024-08-26 11:52:13 +0800

    [修改]1. 修改SourceGenerator 生成方式为代码拼接

--- GameFrameX.CodeGenerator/Agent/AgentInfo.cs
+++ GameFrameX.CodeGenerator/Agent/AgentInfo.cs
@@ -5,10 +5,20 @@ namespace GameFrameX.CodeGenerator.Agent;
 
 public class MthInfo
 {
+    /// <summary>
+    ///     方法名
+    /// </summary>
     public string Name { get; set; }
 
+    /// <summary>
+    ///     返回类型
+    /// </summary>
     public string ReturnType { get; set; }
 
+
+    /// <summary>
+    /// 函数签名
+    /// </summary>
     public string Declare
     {
         get
@@ -26,28 +36,59 @@ public class MthInfo
         }
     }
 
+    /// <summary>
+    ///     是否是Api
+    /// </summary>
     public bool IsApi { get; set; }
 
+    /// <summary>
+    ///     是否修改
+    /// </summary>
     public string Modify { get; set; }
 
+    /// <summary>
+    ///     是否公开
+    /// </summary>
     public bool IsPublic { get; set; }
 
+    /// <summary>
+    ///     是否静态
+    /// </summary>
     public bool IsStatic { get; set; }
 
+    /// <summary>
+    ///     是否虚方法
+    /// </summary>
     public bool IsVirtual { get; set; }
 
-    public bool Isasync { get; set; }
+    /// <summary>
+    ///     是否异步
+    /// </summary>
+    public bool IsAsync { get; set; }
 
     public List<string> Params { get; } = new();
 
+    /// <summary>
+    /// 属性标记列表
+    /// </summary>
     public List<string> AttributeList { get; private set; } = new();
 
     public bool Discard { get; set; }
 
+    /// <summary>
+    ///     是否有超时
+    /// </summary>
     public bool HasTimeout { get; set; }
-    public int Timeout { get; set; } = int.MaxValue;
 
-    public bool Threadsafe { get; set; }
+    /// <summary>
+    ///     超时时间
+    /// </summary>
+    public int TimeOut { get; set; } = int.MaxValue;
+
+    /// <summary>
+    ///     是否线程安全
+    /// </summary>
+    public bool IsThreadSafe { get; set; }
 
     /// <summary>
     ///     约束
@@ -64,7 +105,10 @@ public class MthInfo
     /// </summary>
     public string ParamDeclare { get; set; }
 
-    public string Paramstr
+    /// <summary>
+    ///     参数字符串
+    /// </summary>
+    public string ParamString
     {
         get
         {
@@ -90,10 +134,33 @@ public class MthInfo
 
 public class AgentInfo
 {
+    /// <summary>
+    ///     命名空间
+    /// </summary>
     public string Space { get; set; }
+
+    /// <summary>
+    /// 分部类
+    /// </summary>
     public string Partial { get; set; } = "";
+
+    /// <summary>
+    ///     类名
+    /// </summary>
     public string Name { get; set; }
+
+    /// <summary>
+    ///     父类
+    /// </summary>
     public string Super { get; set; }
+
+    /// <summary>
+    ///     方法列表
+    /// </summary>
     public List<MthInfo> Methods { get; set; } = new();
-    public List<string> Usingspaces { get; set; } = new();
+
+    /// <summary>
+    ///     用到的命名空间
+    /// </summary>
+    public List<string> UsingSpaces { get; set; } = new();
 }
