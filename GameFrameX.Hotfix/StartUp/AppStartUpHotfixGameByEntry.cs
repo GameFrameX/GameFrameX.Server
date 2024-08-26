c79b3160c02c1c5d39dfdc80d9e1921795b34409
Blank <wangfj11@foxmail.com>
2024-08-26 18:56:13 +0800

    [修改]1. 修改逻辑的启动方式
    # Conflicts:
    #	GameFrameX.Hotfix/Common/HotfixBridge.cs

---
+++ GameFrameX.Hotfix/StartUp/AppStartUpHotfixGameByEntry.cs
@@ -0,0 +1,29 @@
+﻿using GameFrameX.Core.Components;
+using GameFrameX.Launcher.StartUp;
+
+namespace GameFrameX.Hotfix.StartUp
+{
+    internal partial class AppStartUpHotfixGame : AppStartUpService, IHotfixBridge
+    {
+        public async Task<bool> OnLoadSuccess(AppSetting setting, bool reload)
+        {
+            if (reload)
+            {
+                ActorManager.ClearAgent();
+                return true;
+            }
+
+            Init(setting.ServerType, setting);
+            RunServer();
+            // 启动定时器
+            GlobalTimer.Start();
+            await ComponentRegister.ActiveGlobalComponents();
+            return true;
+        }
+
+        public async Task Stop()
+        {
+            await StopAsync();
+        }
+    }
+}
