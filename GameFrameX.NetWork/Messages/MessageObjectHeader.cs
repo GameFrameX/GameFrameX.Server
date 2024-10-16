8523637579e2dc9cd791a11076e812fa63302ce1
Blank <wangfj11@foxmail.com>
2024-10-16 15:44:55 +0800

    [增加]1. 增加内部消息头的处理

--- GameFrameX.NetWork/Messages/MessageObjectHeader.cs
+++ GameFrameX.NetWork/Messages/MessageObjectHeader.cs
@@ -13,4 +13,16 @@ public sealed class MessageObjectHeader
     /// </summary>
     [ProtoMember(1)]
     public int ServerId { get; set; }
+
+    /// <summary>
+    /// 消息ID
+    /// </summary>
+    [ProtoMember(2)]
+    public int MessageId { get; set; }
+
+    /// <summary>
+    /// 操作类型
+    /// </summary>
+    [ProtoMember(3)]
+    public byte OperationType { get; set; }
 }
