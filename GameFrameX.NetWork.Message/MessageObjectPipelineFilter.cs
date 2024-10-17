602be28d174f1ede6883bd3d8699a23766cca940
Blank <wangfj11@foxmail.com>
2024-10-17 10:06:15 +0800

    [修改]1. 修改消息解析读取器

--- GameFrameX.NetWork.Message/MessageObjectPipelineFilter.cs
+++ GameFrameX.NetWork.Message/MessageObjectPipelineFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers;
+using GameFrameX.Extension;
 using GameFrameX.NetWork.Abstractions;
 using GameFrameX.SuperSocket.ProtoBase;
 
@@ -7,17 +8,17 @@ namespace GameFrameX.NetWork.Message;
 /// <summary>
 /// 消息对象流水线过滤处理器
 /// </summary>
-public class MessageObjectPipelineFilter : PipelineFilterBase<INetworkMessage>
+public class MessageObjectPipelineFilter : PipelineFilterBase<IMessage>
 {
     /// <summary>
     /// 解析函数
     /// </summary>
     /// <param name="reader"></param>
     /// <returns></returns>
-    public override INetworkMessage Filter(ref SequenceReader<byte> reader)
+    public override IMessage Filter(ref SequenceReader<byte> reader)
     {
         var pack = reader.Sequence;
-        reader.TryReadBigEndian(out ushort totalLength);
+        reader.TryPeekBigEndian(out ushort totalLength);
         if (totalLength <= 0)
         {
             reader.AdvanceToEnd();
@@ -29,12 +30,7 @@ public class MessageObjectPipelineFilter : PipelineFilterBase<INetworkMessage>
         {
             reader.AdvanceToEnd();
         }
-        else
-        {
-            // 前面读了一个长度，所以要减回去
-            reader.Advance(totalLength - 2);
-        }
 
-        return this.Decoder.Decode(ref readBuffer, this.Context);
+        return Decoder.Decode(ref readBuffer, Context);
     }
 }
