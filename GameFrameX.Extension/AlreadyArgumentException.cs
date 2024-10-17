5e57a2e44acb82f73fc5a51fb1cb80e5eaf83229
Blank <wangfj11@foxmail.com>
2024-10-17 09:36:39 +0800

    [增加]1. 增加重复值的异常类型

---
+++ GameFrameX.Extension/AlreadyArgumentException.cs
@@ -0,0 +1,15 @@
+﻿namespace GameFrameX.Extension;
+
+/// <summary>
+/// 已经存在参数重复异常
+/// </summary>
+public class AlreadyArgumentException : Exception
+{
+    /// <summary>
+    /// 已经存在参数重复
+    /// </summary>
+    /// <param name="message">异常消息</param>
+    public AlreadyArgumentException(string message) : base(message)
+    {
+    }
+}
