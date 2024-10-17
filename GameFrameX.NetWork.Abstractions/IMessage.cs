5883c737ad70f0fb4d682cf04293f51a5add192d
Blank <wangfj11@foxmail.com>
2024-10-17 10:09:10 +0800

    [增加]1. 增加消息基类

---
+++ GameFrameX.NetWork.Abstractions/IMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace GameFrameX.NetWork.Abstractions;
+
+/// <summary>
+/// 网络消息
+/// </summary>
+public interface IMessage
+{
+    /// <summary>
+    /// 获取格式化后的消息字符串
+    /// </summary>
+    /// <returns></returns>
+    string ToFormatMessageString();
+}
