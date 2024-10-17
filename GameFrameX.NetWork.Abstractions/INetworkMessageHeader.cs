1a3554c709a0bdcc20df11c3c9c814d24c621e59
Blank <wangfj11@foxmail.com>
2024-10-17 15:34:26 +0800

    [增加]1. 增加消息头的接口定义

---
+++ GameFrameX.NetWork.Abstractions/INetworkMessageHeader.cs
@@ -0,0 +1,27 @@
+﻿namespace GameFrameX.NetWork.Abstractions;
+
+/// <summary>
+/// 网络消息头
+/// </summary>
+public interface INetworkMessageHeader
+{
+    /// <summary>
+    /// 消息ID
+    /// </summary>
+    int MessageId { get; set; }
+
+    /// <summary>
+    /// 唯一消息序列ID
+    /// </summary>
+    int UniqueId { get; set; }
+
+    /// <summary>
+    /// 操作类型
+    /// </summary>
+    byte OperationType { get; set; }
+
+    /// <summary>
+    /// 压缩标记
+    /// </summary>
+    byte ZipFlag { get; set; }
+}
