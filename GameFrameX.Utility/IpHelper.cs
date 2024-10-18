510cae88394eb244536f6b7974877283128eca2a
Blank <wangfj11@foxmail.com>
2024-10-18 14:28:34 +0800

    [增加]1. 增加IP工具函数

---
+++ GameFrameX.Utility/IpHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace GameFrameX.Utility;
+
+/// <summary>
+/// IP帮助类
+/// </summary>
+public static class IpHelper
+{
+    /// <summary>
+    /// 判断IP是否合法
+    /// </summary>
+    /// <param name="ipAddress">Ip地址</param>
+    /// <param name="value">解析成功的值</param>
+    /// <returns></returns>
+    public static bool IsValidIpAddress(string ipAddress, out IPAddress value)
+    {
+        return IPAddress.TryParse(ipAddress, out value);
+    }
+}
