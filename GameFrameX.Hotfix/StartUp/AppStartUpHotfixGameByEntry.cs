7d9c1f6985ba4c5546d9c608b8dba4671c89c60d
Blank <wangfj11@foxmail.com>
2024-11-08 10:27:38 +0800

    [修改]1. 修改游戏统计的初始化

--- GameFrameX.Hotfix/StartUp/AppStartUpHotfixGameByEntry.cs
+++ GameFrameX.Hotfix/StartUp/AppStartUpHotfixGameByEntry.cs
@@ -13,7 +13,7 @@ namespace GameFrameX.Hotfix.StartUp
                 return true;
             }
 
-            GameAnalyticsHelper.Init("ec33486a135ae80984dbfc66eede1e41", "379e705038d46691c5c567db947af1828f702861", Assembly.GetCallingAssembly().ImageRuntimeVersion);
+            
             Init(setting.ServerType, setting);
             RunServer();
             // 启动定时器
