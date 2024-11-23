8aeea2ed225f62ff9482ea947aec42f3600e2c61
AlianBlank <wangfj11@foxmail.com>
2024-11-23 10:42:53 +0800

    [增加]1. 增加AES 注释

--- GameFrameX.Utility/Utility.Encryption.Aes.cs
+++ GameFrameX.Utility/Utility.Encryption.Aes.cs
@@ -23,9 +23,9 @@ public static partial class Encryption
         /// </summary>
         /// <param name="EncryptString">待加密密文</param>
         /// <param name="EncryptKey">加密密钥</param>
-        public static string AESEncrypt(string EncryptString, string EncryptKey)
+        public static string Encrypt(string EncryptString, string EncryptKey)
         {
-            return Convert.ToBase64String(AESEncrypt(Encoding.UTF8.GetBytes(EncryptString), EncryptKey));
+            return Convert.ToBase64String(Encrypt(Encoding.UTF8.GetBytes(EncryptString), EncryptKey));
         }
 
         #endregion
@@ -35,60 +35,48 @@ public static partial class Encryption
         /// <summary>
         /// AES 加密(高级加密标准，是下一代的加密算法标准，速度快，安全级别高，目前 AES 标准的一个实现是 Rijndael 算法)
         /// </summary>
-        /// <param name="EncryptString">待加密密文</param>
-        /// <param name="EncryptKey">加密密钥</param>
-        public static byte[] AESEncrypt(byte[] EncryptByte, string EncryptKey)
+        /// <param name="encryptByte">待加密密文</param>
+        /// <param name="encryptKey">加密密钥</param>
+        public static byte[] Encrypt(byte[] encryptByte, string encryptKey)
         {
-            if (EncryptByte.Length == 0)
+            if (encryptByte.Length == 0)
             {
                 throw (new Exception("明文不得为空"));
             }
 
-            if (string.IsNullOrEmpty(EncryptKey))
+            if (string.IsNullOrEmpty(encryptKey))
             {
                 throw (new Exception("密钥不得为空"));
             }
 
-            byte[] m_strEncrypt;
+            byte[] mStrEncrypt = null;
             byte[] m_btIV = new byte[16] { 224, 131, 122, 101, 37, 254, 33, 17, 19, 28, 212, 130, 45, 65, 43, 32 };
             byte[] m_salt = new byte[16] { 234, 231, 123, 100, 87, 254, 123, 17, 89, 18, 230, 13, 45, 65, 43, 32 };
             Rijndael m_AESProvider = Rijndael.Create();
             try
             {
                 MemoryStream m_stream = new MemoryStream();
-                PasswordDeriveBytes pdb = new PasswordDeriveBytes(EncryptKey, m_salt);
+                PasswordDeriveBytes pdb = new PasswordDeriveBytes(encryptKey, m_salt);
                 ICryptoTransform transform = m_AESProvider.CreateEncryptor(pdb.GetBytes(32), m_btIV);
                 CryptoStream m_csstream = new CryptoStream(m_stream, transform, CryptoStreamMode.Write);
-                m_csstream.Write(EncryptByte, 0, EncryptByte.Length);
+                m_csstream.Write(encryptByte, 0, encryptByte.Length);
                 m_csstream.FlushFinalBlock();
-                m_strEncrypt = m_stream.ToArray();
+                mStrEncrypt = m_stream.ToArray();
                 m_stream.Close();
                 m_stream.Dispose();
                 m_csstream.Close();
                 m_csstream.Dispose();
             }
-            catch (IOException ex)
-            {
-                throw ex;
-            }
-            catch (CryptographicException ex)
-            {
-                throw ex;
-            }
-            catch (ArgumentException ex)
-            {
-                throw ex;
-            }
             catch (Exception ex)
             {
-                throw ex;
+                LogHelper.Error(ex);
             }
             finally
             {
                 m_AESProvider.Clear();
             }
 
-            return m_strEncrypt;
+            return mStrEncrypt;
         }
 
         #endregion
