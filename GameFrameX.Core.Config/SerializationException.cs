f8a48f2839bc09e3676aa89878b509dcbc9314c8
AlianBlank <wangfj11@foxmail.com>
2024-11-23 10:42:27 +0800

    [删除]1. 删除无效的注释

--- GameFrameX.Core.Config/SerializationException.cs
+++ GameFrameX.Core.Config/SerializationException.cs
@@ -1,11 +1,6 @@
 ﻿#define CPU_SUPPORT_MEMORY_NOT_ALIGN //CPU 是否支持读取非对齐内存
 
 
-/// <summary>
-/// TODO  
-/// 1. 整理代码
-/// 2. 优化序列化 (像这样 data[endPos + 1] = (byte)(x >> 8) 挨个字节赋值总感觉很低效，能优化吗)
-/// </summary>
 namespace GameFrameX.Core.Config
 {
     /// <summary>
