11508375ad9d5a67c7e4d7a9420538b0ddcb80dc
Blank <wangfj11@foxmail.com>
2024-12-05 14:41:03 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.NetWork.HTTP/HttpHandler.cs
+++ GameFrameX.NetWork.HTTP/HttpHandler.cs
@@ -5,153 +5,159 @@ using GameFrameX.Extension;
 using GameFrameX.Log;
 using GameFrameX.Setting;
 
-namespace GameFrameX.NetWork.HTTP
+namespace GameFrameX.NetWork.HTTP;
+
+/// <summary>
+/// HTTP处理器，用于处理HTTP请求
+/// </summary>
+public static class HttpHandler
 {
+    private const string ContentType = "application/json; charset=utf-8";
+
     /// <summary>
-    /// HTTP处理器
+    /// 处理HTTP请求
     /// </summary>
-    public static class HttpHandler
+    /// <param name="context">HTTP上下文</param>
+    /// <param name="baseHandler">基础HTTP处理器工厂方法</param>
+    /// <param name="aopHandlerTypes">AOP处理器列表，可选</param>
+    public static async Task HandleRequest(HttpContext context, Func<string, BaseHttpHandler> baseHandler, List<IHttpAopHandler> aopHandlerTypes = null)
     {
-        private const string ContentType = "application/json; charset=utf-8";
+        string ip = context.Connection.RemoteIpAddress?.ToString();
+        string url = context.Request.PathBase + context.Request.Path;
+        string command = context.Request.Path.ToString().Substring(HttpServer.ApiRootPath.Length);
+        string logHeader = $"[HTTPServer] TraceIdentifier:[{context.TraceIdentifier}], 来源[{ip}], url:[{url}]";
+        LogHelper.Info($"{logHeader}，请求方式:[{context.Request.Method}]");
 
-        /// <summary>
-        /// 处理HTTP请求
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="baseHandler"></param>
-        /// <param name="aopHandlerTypes"></param>
-        public static async Task HandleRequest(HttpContext context, Func<string, BaseHttpHandler> baseHandler, List<IHttpAopHandler> aopHandlerTypes = null)
+        try
         {
-            string ip = context.Connection.RemoteIpAddress?.ToString();
-            string url = context.Request.PathBase + context.Request.Path;
-            string command = context.Request.Path.ToString().Substring(HttpServer.ApiRootPath.Length);
-            string logHeader = $"[HTTPServer] TraceIdentifier:[{context.TraceIdentifier}], 来源[{ip}], url:[{url}]";
-            LogHelper.Info($"{logHeader}，请求方式:[{context.Request.Method}]");
-            try
+            Dictionary<string, object> paramMap = new Dictionary<string, object>();
+
+            // 从查询字符串中提取参数
+            foreach (var keyValuePair in context.Request.Query)
             {
-                Dictionary<string, object> paramMap = new Dictionary<string, object>();
+                paramMap.Add(keyValuePair.Key, keyValuePair.Value.ToString());
+            }
 
-                foreach (var keyValuePair in context.Request.Query)
+            context.Response.Headers.ContentType = ContentType;
+
+            // 处理POST请求
+            if (string.Equals(context.Request.Method, HttpMethod.Post.Method, StringComparison.OrdinalIgnoreCase))
+            {
+                var headContentType = context.Request.ContentType;
+                if (headContentType.IsNullOrWhiteSpace())
                 {
-                    paramMap.Add(keyValuePair.Key, keyValuePair.Value.ToString());
-                }
-
-                context.Response.Headers.ContentType = ContentType;
-                if (string.Equals(context.Request.Method, HttpMethod.Post.Method, StringComparison.OrdinalIgnoreCase))
-                {
-                    var headContentType = context.Request.ContentType;
-                    if (headContentType.IsNullOrWhiteSpace())
-                    {
-                        await context.Response.WriteAsync("http header content type is null");
-                        return;
-                    }
-
-                    var isJson = context.Request.HasJsonContentType();
-                    var isForm = context.Request.HasFormContentType;
-                    // LogHelper.Info("isJson:" + isJson);
-                    if (isJson)
-                    {
-                        JsonElement json = await context.Request.ReadFromJsonAsync<JsonElement>();
-                        foreach (var keyValuePair in json.EnumerateObject())
-                        {
-                            if (!paramMap.TryAdd(keyValuePair.Name, keyValuePair.Value))
-                            {
-                                // 参数Key发生重复
-                                await context.Response.WriteAsync(HttpResult.CreateErrorParam("参数重复了:" + keyValuePair.Name));
-                                return;
-                            }
-                        }
-                    }
-                    else if (isForm)
-                    {
-                        foreach (var keyValuePair in context.Request.Form)
-                        {
-                            if (!paramMap.TryAdd(keyValuePair.Key, keyValuePair.Value.ToString()))
-                            {
-                                // 参数Key发生重复
-                                await context.Response.WriteAsync(HttpResult.CreateErrorParam("参数重复了:" + keyValuePair.Key));
-                                return;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        await context.Response.WriteAsync(HttpResult.CreateErrorParam("不支持的Content Type: " + headContentType));
-                        return;
-                    }
-                }
-
-                if (paramMap.Count > 0)
-                {
-                    var str = new StringBuilder();
-                    str.Append("请求参数:");
-                    foreach (var parameter in paramMap)
-                    {
-                        if (parameter.Key.IsNullOrEmptyOrWhiteSpace())
-                        {
-                            continue;
-                        }
-
-                        str.Append('\'').Append(parameter.Key).Append("'='").Append(parameter.Value).Append("'  ");
-                    }
-
-                    LogHelper.Info(str.ToString());
-                }
-
-                // 检查指令是否有效
-                if (command.IsNullOrEmptyOrWhiteSpace())
-                {
-                    await context.Response.WriteAsync(HttpResult.Undefined);
+                    await context.Response.WriteAsync("http header content type is null");
                     return;
                 }
 
-                if (!GlobalSettings.IsAppRunning)
-                {
-                    await context.Response.WriteAsync(HttpResult.CreateActionFailed("服务器状态错误[正在起/关服]"));
-                    return;
-                }
+                var isJson = context.Request.HasJsonContentType();
+                var isForm = context.Request.HasFormContentType;
 
-                #region AOP
-
-                if (aopHandlerTypes is { Count: > 0 })
+                if (isJson)
                 {
-                    foreach (var httpAopHandler in aopHandlerTypes)
+                    JsonElement json = await context.Request.ReadFromJsonAsync<JsonElement>();
+                    foreach (var keyValuePair in json.EnumerateObject())
                     {
-                        if (!httpAopHandler.Run(context, ip, url, paramMap))
+                        if (!paramMap.TryAdd(keyValuePair.Name, keyValuePair.Value))
                         {
+                            // 参数Key发生重复
+                            await context.Response.WriteAsync(HttpResult.CreateErrorParam("参数重复了:" + keyValuePair.Name));
                             return;
                         }
                     }
                 }
-
-                #endregion
-
-
-                var handler = baseHandler(command);
-                if (handler == null)
+                else if (isForm)
                 {
-                    LogHelper.Warn($"http cmd handler 不存在：{command}");
-                    await context.Response.WriteAsync(HttpResult.NotFound);
+                    foreach (var keyValuePair in context.Request.Form)
+                    {
+                        if (!paramMap.TryAdd(keyValuePair.Key, keyValuePair.Value.ToString()))
+                        {
+                            // 参数Key发生重复
+                            await context.Response.WriteAsync(HttpResult.CreateErrorParam("参数重复了:" + keyValuePair.Key));
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    await context.Response.WriteAsync(HttpResult.CreateErrorParam("不支持的Content Type: " + headContentType));
                     return;
                 }
-
-                //验证
-                var isChecked = handler.CheckSign(paramMap, out var error);
-                if (isChecked == false)
-                {
-                    await context.Response.WriteAsync(HttpResult.CheckFailed);
-                    return;
-                }
-
-                var result = await Task.Run(() => { return handler.Action(ip, url, paramMap); });
-                LogHelper.Warn($"{logHeader}, 结果: {result}");
-                await context.Response.WriteAsync(result);
             }
-            catch (Exception e)
+
+            // 记录请求参数
+            if (paramMap.Count > 0)
             {
-                LogHelper.Error($"{logHeader}, 发生异常. {{0}} {{1}}", e.Message, e.StackTrace);
-                await context.Response.WriteAsync(HttpResult.Create(HttpStatusCode.ServerError, e.Message));
+                var str = new StringBuilder();
+                str.Append("请求参数:");
+                foreach (var parameter in paramMap)
+                {
+                    if (parameter.Key.IsNullOrEmptyOrWhiteSpace())
+                    {
+                        continue;
+                    }
+
+                    str.Append('\'').Append(parameter.Key).Append("'='").Append(parameter.Value).Append("'  ");
+                }
+
+                LogHelper.Info(str.ToString());
             }
+
+            // 检查指令是否有效
+            if (command.IsNullOrEmptyOrWhiteSpace())
+            {
+                await context.Response.WriteAsync(HttpResult.Undefined);
+                return;
+            }
+
+            if (!GlobalSettings.IsAppRunning)
+            {
+                await context.Response.WriteAsync(HttpResult.CreateActionFailed("服务器状态错误[正在起/关服]"));
+                return;
+            }
+
+            #region AOP
+
+            // 执行AOP处理器
+            if (aopHandlerTypes is { Count: > 0 })
+            {
+                foreach (var httpAopHandler in aopHandlerTypes)
+                {
+                    if (!httpAopHandler.Run(context, ip, url, paramMap))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            #endregion
+
+            // 获取并执行对应的HTTP处理器
+            var handler = baseHandler(command);
+            if (handler == null)
+            {
+                LogHelper.Warn($"http cmd handler 不存在：{command}");
+                await context.Response.WriteAsync(HttpResult.NotFound);
+                return;
+            }
+
+            // 验证签名
+            var isChecked = handler.CheckSign(paramMap, out var error);
+            if (isChecked == false)
+            {
+                await context.Response.WriteAsync(HttpResult.CheckFailed);
+                return;
+            }
+
+            // 执行处理器逻辑
+            var result = await Task.Run(() => { return handler.Action(ip, url, paramMap); });
+            LogHelper.Warn($"{logHeader}, 结果: {result}");
+            await context.Response.WriteAsync(result);
+        }
+        catch (Exception e)
+        {
+            LogHelper.Error($"{logHeader}, 发生异常. {{0}} {{1}}", e.Message, e.StackTrace);
+            await context.Response.WriteAsync(HttpResult.Create(HttpStatusCode.ServerError, e.Message));
         }
     }
 }
