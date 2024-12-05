2bf98fab941ff4f4175ea30fd105ee6b1882c636
Blank <wangfj11@foxmail.com>
2024-12-05 19:37:29 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core.Abstractions/Events/IEventListener.cs
+++ GameFrameX.Core.Abstractions/Events/IEventListener.cs
@@ -1,24 +1,23 @@
 ﻿using GameFrameX.Core.Abstractions.Agent;
 
-namespace GameFrameX.Core.Abstractions.Events
+namespace GameFrameX.Core.Abstractions.Events;
+
+/// <summary>
+/// 每个实例其实都是单例的
+/// </summary>
+public interface IEventListener
 {
     /// <summary>
-    /// 每个实例其实都是单例的
+    /// 事件处理函数
     /// </summary>
-    public interface IEventListener
-    {
-        /// <summary>
-        /// 事件处理函数
-        /// </summary>
-        /// <param name="agent"> 组件代理 </param>
-        /// <param name="evt"> 事件 </param>
-        /// <returns></returns>
-        Task HandleEvent(IComponentAgent agent, Event evt);
+    /// <param name="agent">组件代理</param>
+    /// <param name="evt">事件</param>
+    /// <returns>一个表示异步操作的任务</returns>
+    Task HandleEvent(IComponentAgent agent, Event evt);
 
-        /// <summary>
-        /// 事件代理类型
-        /// </summary>
-        /// <returns></returns>
-        Type AgentType { get; }
-    }
+    /// <summary>
+    /// 获取事件代理类型
+    /// </summary>
+    /// <returns>事件代理的类型</returns>
+    Type AgentType { get; }
 }
--- GameFrameX.Core.Abstractions/Timer/ICrossDay.cs
+++ GameFrameX.Core.Abstractions/Timer/ICrossDay.cs
@@ -1,15 +1,14 @@
-﻿namespace GameFrameX.Core.Abstractions.Timer
+﻿namespace GameFrameX.Core.Abstractions.Timer;
+
+/// <summary>
+/// 跨天接口
+/// </summary>
+public interface ICrossDay
 {
     /// <summary>
-    /// 跨天接口
+    /// 在跨天时触发的方法
     /// </summary>
-    public interface ICrossDay
-    {
-        /// <summary>
-        /// 在跨天时触发的方法
-        /// </summary>
-        /// <param name="openServerDay">开服天数</param>
-        /// <returns>表示异步操作的任务</returns>
-        public Task OnCrossDay(int openServerDay);
-    }
+    /// <param name="openServerDay">开服天数</param>
+    /// <returns>表示异步操作的任务</returns>
+    public Task OnCrossDay(int openServerDay);
 }
