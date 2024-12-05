ea9b6d32ddc76fc03c3bd9bdc4951ee1f9b9cb1a
Blank <wangfj11@foxmail.com>
2024-12-05 19:37:40 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core.Abstractions/Attribute/FuncAttribute.cs
+++ GameFrameX.Core.Abstractions/Attribute/FuncAttribute.cs
@@ -1,23 +1,22 @@
-﻿namespace GameFrameX.Core.Abstractions.Attribute
+﻿namespace GameFrameX.Core.Abstractions.Attribute;
+
+/// <summary>
+/// 有关组件的功能属性
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class FuncAttribute : System.Attribute
 {
     /// <summary>
-    /// 有关组件的功能
+    /// 功能标识
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
-    public class FuncAttribute : System.Attribute
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public readonly short Func;
+    public readonly short Func;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="func"></param>
-        public FuncAttribute(short func)
-        {
-            Func = func;
-        }
+    /// <summary>
+    /// 初始化功能属性
+    /// </summary>
+    /// <param name="func">功能标识</param>
+    public FuncAttribute(short func)
+    {
+        Func = func;
     }
 }
--- GameFrameX.Core.Abstractions/Attribute/ServiceAttribute.cs
+++ GameFrameX.Core.Abstractions/Attribute/ServiceAttribute.cs
@@ -1,10 +1,9 @@
-﻿namespace GameFrameX.Core.Abstractions.Attribute
+﻿namespace GameFrameX.Core.Abstractions.Attribute;
+
+/// <summary>
+/// 此方法会提供给其他Actor访问(对外提供服务)
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class ServiceAttribute : System.Attribute
 {
-    /// <summary>
-    /// 此方法会提供给其他Actor访问(对外提供服务)
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method)]
-    public sealed class ServiceAttribute : System.Attribute
-    {
-    }
 }
