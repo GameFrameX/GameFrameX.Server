04b6217a3625a84b2db19e1e604d126aa70022c6
Blank <wangfj11@foxmail.com>
2024-12-05 19:42:26 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core.Abstractions/Agent/IComponentAgent.cs
+++ GameFrameX.Core.Abstractions/Agent/IComponentAgent.cs
@@ -1,48 +1,47 @@
-﻿namespace GameFrameX.Core.Abstractions.Agent
+﻿namespace GameFrameX.Core.Abstractions.Agent;
+
+/// <summary>
+/// 组件代理接口
+/// </summary>
+public interface IComponentAgent : IWorker
 {
     /// <summary>
-    /// 组件代理接口
+    /// 获取Actor的唯一标识
     /// </summary>
-    public interface IComponentAgent : IWorker
-    {
-        /// <summary>
-        /// ActorId
-        /// </summary>
-        long ActorId { get; }
+    long ActorId { get; }
 
-        /// <summary>
-        /// 拥有者
-        /// </summary>
-        IComponent Owner { get; set; }
+    /// <summary>
+    /// 获取或设置组件的所有者
+    /// </summary>
+    IComponent Owner { get; set; }
 
-        /// <summary>
-        /// 激活
-        /// </summary>
-        void Active();
+    /// <summary>
+    /// 激活组件代理
+    /// </summary>
+    void Active();
 
-        /// <summary>
-        /// 反激活
-        /// </summary>
-        /// <returns></returns>
-        Task Inactive();
+    /// <summary>
+    /// 反激活组件代理
+    /// </summary>
+    /// <returns>一个表示异步操作的任务</returns>
+    Task Inactive();
 
-        /// <summary>
-        /// 拥有者类型
-        /// </summary>
-        ActorType OwnerType { get; }
+    /// <summary>
+    /// 获取所有者的类型
+    /// </summary>
+    ActorType OwnerType { get; }
 
-        /// <summary>
-        /// 根据代理类型获取代理组件
-        /// </summary>
-        /// <param name="agentType">代理类型</param>
-        /// <returns></returns>
-        public Task<IComponentAgent> GetComponentAgent(Type agentType);
+    /// <summary>
+    /// 根据代理类型获取代理组件
+    /// </summary>
+    /// <param name="agentType">代理类型</param>
+    /// <returns>代理组件</returns>
+    public Task<IComponentAgent> GetComponentAgent(Type agentType);
 
-        /// <summary>
-        /// 根据代理类型获取代理组件
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public Task<T> GetComponentAgent<T>() where T : IComponentAgent;
-    }
+    /// <summary>
+    /// 根据泛型代理类型获取代理组件
+    /// </summary>
+    /// <typeparam name="T">代理组件的类型</typeparam>
+    /// <returns>代理组件</returns>
+    public Task<T> GetComponentAgent<T>() where T : IComponentAgent;
 }
--- GameFrameX.Core.Abstractions/Attribute/ComponentTypeAttribute.cs
+++ GameFrameX.Core.Abstractions/Attribute/ComponentTypeAttribute.cs
@@ -1,23 +1,22 @@
-﻿namespace GameFrameX.Core.Abstractions.Attribute
+﻿namespace GameFrameX.Core.Abstractions.Attribute;
+
+/// <summary>
+/// 组件类型标记
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class ComponentTypeAttribute : System.Attribute
 {
     /// <summary>
-    /// 组件类型标记
+    /// 组件类型
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
-    public class ComponentTypeAttribute : System.Attribute
+    /// <param name="type">组件类型</param>
+    public ComponentTypeAttribute(ActorType type)
     {
-        /// <summary>
-        /// 组件类型
-        /// </summary>
-        /// <param name="type">组件类型</param>
-        public ComponentTypeAttribute(ActorType type)
-        {
-            ActorType = type;
-        }
-
-        /// <summary>
-        /// ActorType
-        /// </summary>
-        public ActorType ActorType { get; }
+        ActorType = type;
     }
+
+    /// <summary>
+    /// ActorType
+    /// </summary>
+    public ActorType ActorType { get; }
 }
