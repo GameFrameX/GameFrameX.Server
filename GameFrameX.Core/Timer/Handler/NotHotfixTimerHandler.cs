984aecafa783aa7b9c93182d706b0b8f75b54309
Blank <wangfj11@foxmail.com>
2024-12-05 19:53:01 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core/Timer/Handler/NotHotfixTimerHandler.cs
+++ GameFrameX.Core/Timer/Handler/NotHotfixTimerHandler.cs
@@ -1,29 +1,28 @@
 ﻿using GameFrameX.Utility;
 using Quartz;
 
-namespace GameFrameX.Core.Timer.Handler
+namespace GameFrameX.Core.Timer.Handler;
+
+/// <summary>
+/// 非热更程序集的计时器处理器，不需要热更时间更新
+/// </summary>
+public abstract class NotHotfixTimerHandler : IJob
 {
     /// <summary>
-    /// 非热更程序集的计时器处理器，不需要热更时间更新
+    /// 内部计时器处理器调用函数
     /// </summary>
-    public abstract class NotHotfixTimerHandler : IJob
+    /// <param name="context">Quartz 作业执行上下文，包含作业执行所需的信息</param>
+    /// <returns>一个任务，表示异步操作的结果</returns>
+    public Task Execute(IJobExecutionContext context)
     {
-        /// <summary>
-        /// 内部计时器处理器调用函数
-        /// </summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public Task Execute(IJobExecutionContext context)
-        {
-            var param = context.JobDetail.JobDataMap.Get(QuartzTimer.ParamKey) as Param;
-            return HandleTimer(param);
-        }
-
-        /// <summary>
-        /// 计时器处理函数
-        /// </summary>
-        /// <param name="param">参数</param>
-        /// <returns></returns>
-        protected abstract Task HandleTimer(Param param);
+        var param = context.JobDetail.JobDataMap.Get(QuartzTimer.ParamKey) as Param;
+        return HandleTimer(param);
     }
+
+    /// <summary>
+    /// 计时器处理函数
+    /// </summary>
+    /// <param name="param">传递给处理器的参数</param>
+    /// <returns>一个任务，表示异步操作的结果</returns>
+    protected abstract Task HandleTimer(Param param);
 }
