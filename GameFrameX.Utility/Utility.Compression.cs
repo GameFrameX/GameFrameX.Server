5999d9785e8e63b530bd77749677808c0ccb31d2
Blank <wangfj11@foxmail.com>
2024-12-05 11:45:13 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Utility/Utility.Compression.cs
+++ GameFrameX.Utility/Utility.Compression.cs
@@ -1,41 +1,39 @@
 ﻿using System.IO.Compression;
 
-namespace GameFrameX.Utility
+namespace GameFrameX.Utility;
+
+/// <summary>
+/// 压缩解压缩辅助器。
+/// </summary>
+public static class Compression
 {
     /// <summary>
-    /// 压缩解压缩辅助器。
+    /// 压缩数据。
     /// </summary>
-    public static class Compression
+    /// <param name="bytes">要压缩的原始字节数组。</param>
+    /// <returns>压缩后的字节数组。</returns>
+    public static byte[] Compress(byte[] bytes)
     {
-        /// <summary>
-        /// 压缩数据。
-        /// </summary>
-        /// <param name="bytes"></param>
-        /// <returns></returns>
-        public static byte[] Compress(byte[] bytes)
-        {
-            var uncompressed = new MemoryStream(bytes);
-            var compressed = new MemoryStream();
-            var deflateStream = new DeflateStream(compressed, CompressionMode.Compress);
-            uncompressed.CopyTo(deflateStream);
-            deflateStream.Close();
-            return compressed.ToArray();
-        }
+        var uncompressed = new MemoryStream(bytes);
+        var compressed = new MemoryStream();
+        var deflateStream = new DeflateStream(compressed, CompressionMode.Compress);
+        uncompressed.CopyTo(deflateStream);
+        deflateStream.Close();
+        return compressed.ToArray();
+    }
 
-
-        /// <summary>
-        /// 解压数据。
-        /// </summary>
-        /// <param name="bytes"></param>
-        /// <returns></returns>
-        public static byte[] Decompress(byte[] bytes)
-        {
-            var compressed = new MemoryStream(bytes);
-            var decompressed = new MemoryStream();
-            var deflateStream = new DeflateStream(compressed, CompressionMode.Decompress); // 注意： 这里第一个参数同样是填写压缩的数据，但是这次是作为输入的数据
-            deflateStream.CopyTo(decompressed);
-            byte[] result = decompressed.ToArray();
-            return result;
-        }
+    /// <summary>
+    /// 解压数据。
+    /// </summary>
+    /// <param name="bytes">要解压的压缩字节数组。</param>
+    /// <returns>解压后的字节数组。</returns>
+    public static byte[] Decompress(byte[] bytes)
+    {
+        var compressed = new MemoryStream(bytes);
+        var decompressed = new MemoryStream();
+        var deflateStream = new DeflateStream(compressed, CompressionMode.Decompress); // 注意：这里第一个参数同样是填写压缩的数据，但是这次是作为输入的数据
+        deflateStream.CopyTo(decompressed);
+        byte[] result = decompressed.ToArray();
+        return result;
     }
 }
--- GameFrameX.Utility/Utility.Encryption.cs
+++ GameFrameX.Utility/Utility.Encryption.cs
@@ -1,11 +1,4 @@
-﻿//------------------------------------------------------------
-// Game Framework
-// Copyright © 2013-2021 Jiang Yin. All rights reserved.
-// Homepage: https://gameframework.cn/
-// Feedback: mailto:ellan@gameframework.cn
-//------------------------------------------------------------
-
-namespace GameFrameX.Utility;
+﻿namespace GameFrameX.Utility;
 
 /// <summary>
 /// 加密解密相关的实用函数。
