d4fb003f86f0ac89f56ce9b378910122b85968e2
Blank <wangfj11@foxmail.com>
2024-12-05 19:32:21 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core.Abstractions/IWorker.cs
+++ GameFrameX.Core.Abstractions/IWorker.cs
@@ -9,46 +9,48 @@ public interface IWorker
     /// 发送无返回值的工作指令
     /// </summary>
     /// <param name="work">工作内容</param>
-    /// <param name="timeOut">超时,默认为int.MaxValue</param>
+    /// <param name="timeOut">超时时间，默认为int.MaxValue</param>
     void Tell(Action work, int timeOut = int.MaxValue);
 
     /// <summary>
     /// 发送有返回值的工作指令
     /// </summary>
     /// <param name="work">工作内容</param>
-    /// <param name="timeOut">超时,默认为int.MaxValue</param>
+    /// <param name="timeOut">超时时间，默认为int.MaxValue</param>
     void Tell(Func<Task> work, int timeOut = int.MaxValue);
 
     /// <summary>
-    /// 发送无返回值工作指令
+    /// 异步发送无返回值的工作指令
     /// </summary>
     /// <param name="work">工作内容</param>
-    /// <param name="timeOut">超时,默认为int.MaxValue</param>
-    /// <returns></returns>
+    /// <param name="timeOut">超时时间，默认为int.MaxValue</param>
+    /// <returns>一个表示异步操作的任务</returns>
     Task SendAsync(Action work, int timeOut = int.MaxValue);
 
     /// <summary>
-    /// 发送有返回值工作指令
+    /// 异步发送有返回值的工作指令
     /// </summary>
     /// <param name="work">工作内容</param>
-    /// <param name="timeOut">超时,默认为int.MaxValue</param>
-    /// <returns></returns>
+    /// <param name="timeOut">超时时间，默认为int.MaxValue</param>
+    /// <typeparam name="T">泛型参数</typeparam>
+    /// <returns>一个表示异步操作的任务，任务结果为委托的返回值</returns>
     Task<T> SendAsync<T>(Func<T> work, int timeOut = int.MaxValue);
 
     /// <summary>
-    /// 发送有返回值工作指令
+    /// 异步发送有返回值的工作指令
     /// </summary>
     /// <param name="work">工作内容</param>
-    /// <param name="timeOut">超时,默认为int.MaxValue</param>
+    /// <param name="timeOut">超时时间，默认为int.MaxValue</param>
     /// <param name="checkLock">是否检查锁</param>
-    /// <returns></returns>
+    /// <returns>一个表示异步操作的任务</returns>
     Task SendAsync(Func<Task> work, int timeOut = int.MaxValue, bool checkLock = true);
 
     /// <summary>
-    /// 发送有返回值工作指令
+    /// 异步发送有返回值的工作指令
     /// </summary>
     /// <param name="work">工作内容</param>
-    /// <param name="timeOut">超时,默认为int.MaxValue</param>
-    /// <returns></returns>
+    /// <param name="timeOut">超时时间，默认为int.MaxValue</param>
+    /// <typeparam name="T">泛型参数</typeparam>
+    /// <returns>一个表示异步操作的任务，任务结果为委托的返回值</returns>
     Task<T> SendAsync<T>(Func<Task<T>> work, int timeOut = int.MaxValue);
 }
