8c9b8f1288c3869199bef3d26f1e442881d535b5
Blank <wangfj11@foxmail.com>
2024-12-05 11:28:05 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Utility/Utility.Net.cs
+++ GameFrameX.Utility/Utility.Net.cs
@@ -4,16 +4,16 @@ using System.Net.NetworkInformation;
 namespace GameFrameX.Utility;
 
 /// <summary>
-/// 端口帮助类
+/// 网络端口帮助类
 /// </summary>
 public static class Net
 {
     /// <summary>
-    /// 判断IP是否合法
+    /// 判断IP地址是否合法
     /// </summary>
-    /// <param name="ipAddress">Ip地址</param>
-    /// <param name="value">解析成功的值</param>
-    /// <returns></returns>
+    /// <param name="ipAddress">IP地址字符串</param>
+    /// <param name="value">解析成功的IPAddress对象</param>
+    /// <returns>如果IP地址合法，返回true；否则返回false</returns>
     public static bool IsValidIpAddress(string ipAddress, out IPAddress value)
     {
         return IPAddress.TryParse(ipAddress, out value);
@@ -22,9 +22,9 @@ public static class Net
     /// <summary>
     /// 获取第一个可用的端口号
     /// </summary>
-    /// <param name="startPort">起始端口号</param>
-    /// <param name="maxPort">结束端口号</param>
-    /// <returns></returns>
+    /// <param name="startPort">起始端口号，默认为667</param>
+    /// <param name="maxPort">结束端口号，默认为65535</param>
+    /// <returns>第一个可用的端口号，如果没有可用端口号则返回-1</returns>
     public static int GetFirstAvailablePort(int startPort = 667, int maxPort = 65535)
     {
         for (int i = startPort; i < maxPort; i++)
@@ -41,19 +41,19 @@ public static class Net
     /// <summary>
     /// 获取操作系统已用的端口号
     /// </summary>
-    /// <returns></returns>
+    /// <returns>包含已用端口号的列表</returns>
     public static List<int> PortIsUsed()
     {
-        //获取本地计算机的网络连接和通信统计数据的信息
+        // 获取本地计算机的网络连接和通信统计数据的信息
         var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
 
-        //返回本地计算机上的所有Tcp监听程序
+        // 返回本地计算机上的所有TCP监听程序
         var ipsTcp = ipGlobalProperties.GetActiveTcpListeners();
 
-        //返回本地计算机上的所有UDP监听程序
+        // 返回本地计算机上的所有UDP监听程序
         var ipsUdp = ipGlobalProperties.GetActiveUdpListeners();
 
-        //返回本地计算机上的Internet协议版本4(IPV4 传输控制协议(TCP)连接的信息。
+        // 返回本地计算机上的Internet协议版本4 (IPV4) 传输控制协议 (TCP) 连接的信息
         var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
 
         var allPorts = new List<int>();
@@ -78,8 +78,8 @@ public static class Net
     /// <summary>
     /// 检查指定端口是否已用
     /// </summary>
-    /// <param name="port"></param>
-    /// <returns></returns>
+    /// <param name="port">要检查的端口号</param>
+    /// <returns>如果端口未被使用，返回true；否则返回false</returns>
     public static bool PortIsAvailable(int port)
     {
         var isAvailable = true;
