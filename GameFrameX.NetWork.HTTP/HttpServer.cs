20032b6fa8c8a0a2e746b60432d71a675149e5ff
Blank <wangfj11@foxmail.com>
2024-12-05 12:13:06 +0800

    [优化]1. 优化HTTP服务启动类的代码结构

--- GameFrameX.NetWork.HTTP/HttpServer.cs
+++ GameFrameX.NetWork.HTTP/HttpServer.cs
@@ -8,117 +8,118 @@ using Serilog;
 using GameFrameX.Log;
 using GameFrameX.Utility;
 
-namespace GameFrameX.NetWork.HTTP
+namespace GameFrameX.NetWork.HTTP;
+
+/// <summary>
+/// HTTP服务器
+/// </summary>
+public static class HttpServer
 {
+    private static WebApplication App { get; set; }
+
     /// <summary>
-    /// HTTP服务器
+    /// 游戏API根地址
     /// </summary>
-    public static class HttpServer
+    private const string GameApiPath = "/game/api/";
+
+    /// <summary>
+    /// API根地址
+    /// </summary>
+    public static string ApiRootPath { get; private set; }
+
+    /// <summary>
+    /// 启动
+    /// </summary>
+    /// <param name="httpPort">HTTP端口,如果没有指定端口，则默认为28080</param>
+    /// <param name="httpsPort">HTTPS端口,如果没有指定端口，则不监听HTTPS</param>
+    /// <param name="baseHandler">根据命令Id获得处理器</param>
+    /// <param name="aopHandlerTypes">Aop处理器列表</param>
+    /// <param name="apiRootPath">接口根路径,必须以 / 开头和以 / 结尾,默认为 [/game/api]</param>
+    /// <param name="minimumLevelLogLevel">日志记录最小级别</param>
+    public static Task Start(int httpPort, int httpsPort, Func<string, BaseHttpHandler> baseHandler, List<IHttpAopHandler> aopHandlerTypes = null, string apiRootPath = GameApiPath, LogLevel minimumLevelLogLevel = LogLevel.Debug)
     {
-        private static WebApplication App { get; set; }
+        LogHelper.Info("开始启动 HTTP 服务器...");
+        baseHandler.CheckNotNull(nameof(baseHandler));
 
-        /// <summary>
-        /// 游戏API根地址
-        /// </summary>
-        private const string GameApiPath = "/game/api/";
-
-        /// <summary>
-        /// API根地址
-        /// </summary>
-        public static string ApiRootPath { get; private set; }
-
-        /// <summary>
-        /// 启动
-        /// </summary>
-        /// <param name="httpPort">HTTP端口,如果没有指定端口，则默认为28080</param>
-        /// <param name="httpsPort">HTTPS端口,如果没有指定端口，则不监听HTTPS</param>
-        /// <param name="baseHandler">根据命令Id获得处理器</param>
-        /// <param name="aopHandlerTypes">Aop处理器列表</param>
-        /// <param name="apiRootPath">接口根路径,必须以 / 开头和以 / 结尾,默认为 [/game/api]</param>
-        /// <param name="minimumLevelLogLevel">日志记录最小级别</param>
-        public static Task Start(int httpPort, int httpsPort, Func<string, BaseHttpHandler> baseHandler, List<IHttpAopHandler> aopHandlerTypes = null, string apiRootPath = GameApiPath, LogLevel minimumLevelLogLevel = LogLevel.Debug)
+        // 如果没有指定端口，则默认为28080
+        if (httpPort <= 0)
         {
-            LogHelper.Info("开始启动 HTTP 服务器...");
-            baseHandler.CheckNotNull(nameof(baseHandler));
-
-            // 如果没有指定端口，则默认为28080
-            if (httpPort <= 0)
-            {
-                httpPort = 28080;
-            }
-
-            // 如果没有指定根路径，则默认为/game/api/
-            if (apiRootPath.IsNullOrEmpty())
-            {
-                apiRootPath = GameApiPath;
-            }
-
-            // 根路径必须以/开头和以/结尾
-            if (!apiRootPath.StartsWith("/"))
-            {
-                apiRootPath = "/" + apiRootPath;
-            }
-
-            if (!apiRootPath.EndsWith("/"))
-            {
-                apiRootPath += "/";
-            }
-
-            ApiRootPath = apiRootPath;
-            var builder = WebApplication.CreateBuilder();
-            builder.WebHost.UseKestrel(options =>
-            {
-                // HTTP
-                if (httpPort > 0 && Net.PortIsAvailable(httpPort))
-                {
-                    options.ListenAnyIP(httpPort);
-                }
-
-                // HTTPS
-                if (httpsPort > 0 && Net.PortIsAvailable(httpPort))
-                {
-                    options.ListenAnyIP(httpsPort, listenOptions => { listenOptions.UseHttps(); });
-                }
-            }).ConfigureLogging(logging =>
-            {
-                logging.ClearProviders();
-                logging.AddSerilog(Serilog.Log.Logger);
-                logging.SetMinimumLevel(minimumLevelLogLevel);
-            });
-            App = builder.Build();
-            App.UseExceptionHandler((errorContext) =>
-            {
-                errorContext.Run(async (context) =>
-                {
-                    // 获取异常信息
-                    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
-
-                    // 自定义返回Json信息；
-                    await context.Response.WriteAsync(exceptionHandlerPathFeature.Error.Message);
-                });
-            });
-            string routePath = $"{ApiRootPath}{{text}}";
-            App.MapGet(routePath, context => HttpHandler.HandleRequest(context, baseHandler, aopHandlerTypes));
-            App.MapPost(routePath, context => HttpHandler.HandleRequest(context, baseHandler, aopHandlerTypes));
-            var task = App.StartAsync();
-            LogHelper.Info("启动 HTTP 服务器完成...");
-            return task;
+            httpPort = 28080;
         }
 
-        /// <summary>
-        /// 停止
-        /// </summary>
-        public static Task Stop()
+        // 如果没有指定根路径，则默认为/game/api/
+        if (apiRootPath.IsNullOrEmpty())
         {
-            if (App != null)
+            apiRootPath = GameApiPath;
+        }
+
+        // 根路径必须以/开头和以/结尾
+        if (!apiRootPath.StartsWith("/"))
+        {
+            apiRootPath = "/" + apiRootPath;
+        }
+
+        if (!apiRootPath.EndsWith("/"))
+        {
+            apiRootPath += "/";
+        }
+
+        ApiRootPath = apiRootPath;
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.UseKestrel(options =>
+        {
+            // HTTP
+            if (httpPort > 0 && Net.PortIsAvailable(httpPort))
             {
-                LogHelper.Info("停止http服务...");
-                var task = App.StopAsync();
-                App = null;
-                return task;
+                options.ListenAnyIP(httpPort);
             }
 
+            // HTTPS
+            if (httpsPort > 0 && Net.PortIsAvailable(httpPort))
+            {
+                options.ListenAnyIP(httpsPort, listenOptions => { listenOptions.UseHttps(); });
+            }
+        }).ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddSerilog(Serilog.Log.Logger);
+            logging.SetMinimumLevel(minimumLevelLogLevel);
+        });
+        App = builder.Build();
+        App.UseExceptionHandler(ExceptionHandler);
+        string routePath = $"{ApiRootPath}{{text}}";
+        App.MapGet(routePath, context => HttpHandler.HandleRequest(context, baseHandler, aopHandlerTypes));
+        App.MapPost(routePath, context => HttpHandler.HandleRequest(context, baseHandler, aopHandlerTypes));
+        var task = App.StartAsync();
+        LogHelper.Info("启动 HTTP 服务器完成...");
+        return task;
+    }
+
+    private static void ExceptionHandler(IApplicationBuilder errorContext)
+    {
+        errorContext.Run(async (context) =>
+        {
+            // 获取异常信息
+            var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+
+            // 自定义返回Json信息；
+            await context.Response.WriteAsync(exceptionHandlerPathFeature.Error.Message);
+        });
+    }
+
+    /// <summary>
+    /// 停止
+    /// </summary>
+    public static Task Stop()
+    {
+        LogHelper.Info("停止http服务...");
+        if (App == null)
+        {
             return Task.CompletedTask;
         }
+
+        var task = App.StopAsync();
+        App = null;
+        return task;
     }
 }
