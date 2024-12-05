a51b28363ca3d81c531a0c8a97928fe3b2889b95
Blank <wangfj11@foxmail.com>
2024-12-05 19:31:15 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core.Abstractions/IWorkerActor.cs
+++ GameFrameX.Core.Abstractions/IWorkerActor.cs
@@ -6,49 +6,50 @@
 public interface IWorkerActor : IWorker
 {
     /// <summary>
-    /// 是否需要入队
+    /// 判断是否需要入队
     /// </summary>
-    /// <returns></returns>
+    /// <returns>一个元组，包含是否需要入队的布尔值和调用链ID</returns>
     (bool needEnqueue, long chainId) IsNeedEnqueue();
-    
+
     /// <summary>
-    /// 入队
+    /// 将无返回值的委托入队
     /// </summary>
     /// <param name="work">无返回值的委托</param>
     /// <param name="callChainId">调用链ID</param>
     /// <param name="discard">是否强制入队</param>
     /// <param name="timeOut">超时时间</param>
-    /// <returns></returns>
-    Task Enqueue(Action work, long callChainId, bool discard = false, int timeOut =  int.MaxValue);
-    
+    /// <returns>一个表示异步操作的任务</returns>
+    Task Enqueue(Action work, long callChainId, bool discard = false, int timeOut = int.MaxValue);
+
     /// <summary>
-    /// 入队
+    /// 将有返回值的委托入队
     /// </summary>
     /// <param name="work">有返回值的委托</param>
     /// <param name="callChainId">调用链ID</param>
     /// <param name="discard">是否强制入队</param>
     /// <param name="timeOut">超时时间</param>
     /// <typeparam name="T">泛型参数</typeparam>
-    /// <returns></returns>
+    /// <returns>一个表示异步操作的任务，任务结果为委托的返回值</returns>
     Task<T> Enqueue<T>(Func<T> work, long callChainId, bool discard = false, int timeOut = int.MaxValue);
-    
+
     /// <summary>
-    /// 入队
+    /// 将返回Task的委托入队
     /// </summary>
-    /// <param name="work">有返回值的委托</param>
+    /// <param name="work">返回Task的委托</param>
     /// <param name="callChainId">调用链ID</param>
     /// <param name="discard">是否强制入队</param>
     /// <param name="timeOut">超时时间</param>
+    /// <returns>一个表示异步操作的任务</returns>
     Task Enqueue(Func<Task> work, long callChainId, bool discard = false, int timeOut = int.MaxValue);
-    
+
     /// <summary>
-    /// 入队
+    /// 将返回Task<T>的委托入队
     /// </summary>
-    /// <param name="work"></param>
+    /// <param name="work">返回Task<T>的委托</param>
     /// <param name="callChainId">调用链ID</param>
     /// <param name="discard">是否强制入队</param>
     /// <param name="timeOut">超时时间</param>
     /// <typeparam name="T">泛型参数</typeparam>
-    /// <returns></returns>
+    /// <returns>一个表示异步操作的任务，任务结果为委托的返回值</returns>
     Task<T> Enqueue<T>(Func<Task<T>> work, long callChainId, bool discard = false, int timeOut = int.MaxValue);
 }
