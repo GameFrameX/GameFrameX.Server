74e7ba5f6521332b03dea19424b8c1f16cbb82b7
Blank <wangfj11@foxmail.com>
2024-12-05 11:26:02 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Utility/JsonHelper.cs
+++ GameFrameX.Utility/JsonHelper.cs
@@ -10,8 +10,8 @@ public static class JsonHelper
     /// <summary>
     /// 序列化对象
     /// </summary>
-    /// <param name="obj"></param>
-    /// <returns></returns>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <returns>序列化后的 JSON 字符串</returns>
     public static string Serialize(object obj)
     {
         obj.CheckNotNull(nameof(obj));
@@ -22,8 +22,8 @@ public static class JsonHelper
     /// <summary>
     /// 序列化对象且格式化JSON
     /// </summary>
-    /// <param name="obj"></param>
-    /// <returns></returns>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <returns>序列化后的 JSON 字符串</returns>
     public static string SerializeFormat(object obj)
     {
         obj.CheckNotNull(nameof(obj));
@@ -34,9 +34,9 @@ public static class JsonHelper
     /// <summary>
     /// 反序列化对象
     /// </summary>
-    /// <param name="json"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="json">需要反序列化的 JSON 字符串</param>
+    /// <typeparam name="T">反序列化后的对象类型</typeparam>
+    /// <returns>反序列化后的对象</returns>
     public static T Deserialize<T>(string json) where T : class, new()
     {
         json.CheckNotNullOrEmpty(nameof(json));
@@ -46,9 +46,9 @@ public static class JsonHelper
     /// <summary>
     /// 反序列化对象
     /// </summary>
-    /// <param name="json"></param>
-    /// <param name="type"></param>
-    /// <returns></returns>
+    /// <param name="json">需要反序列化的 JSON 字符串</param>
+    /// <param name="type">反序列化后的对象类型</param>
+    /// <returns>反序列化后的对象</returns>
     public static object Deserialize(string json, Type type)
     {
         json.CheckNotNullOrEmpty(nameof(json));
