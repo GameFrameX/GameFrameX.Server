6febe7a29d25a57f75cdc304d1958101513b0202
Blank <wangfj11@foxmail.com>
2024-12-05 20:14:48 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core/Hotfix/Agent/FuncComponentAgent.cs
+++ GameFrameX.Core/Hotfix/Agent/FuncComponentAgent.cs
@@ -1,12 +1,11 @@
 ﻿using GameFrameX.Core.Components;
 
-namespace GameFrameX.Core.Hotfix.Agent
+namespace GameFrameX.Core.Hotfix.Agent;
+
+/// <summary>
+/// 无返回值组件代理
+/// </summary>
+/// <typeparam name="TComponent">具体的组件类型</typeparam>
+public abstract class FuncComponentAgent<TComponent> : BaseComponentAgent<TComponent> where TComponent : BaseComponent
 {
-    /// <summary>
-    /// 无返回值组件代理
-    /// </summary>
-    /// <typeparam name="TComponent"></typeparam>
-    public abstract class FuncComponentAgent<TComponent> : BaseComponentAgent<TComponent> where TComponent : BaseComponent
-    {
-    }
 }
--- GameFrameX.Core/Hotfix/Agent/StateComponentAgent.cs
+++ GameFrameX.Core/Hotfix/Agent/StateComponentAgent.cs
@@ -1,21 +1,22 @@
 ﻿using GameFrameX.Core.Components;
 using GameFrameX.DataBase.Abstractions;
 
-namespace GameFrameX.Core.Hotfix.Agent
+namespace GameFrameX.Core.Hotfix.Agent;
+
+/// <summary>
+/// 状态组件代理类
+/// </summary>
+/// <typeparam name="TComponent">组件对象类型</typeparam>
+/// <typeparam name="TState">数据对象类型</typeparam>
+public abstract class StateComponentAgent<TComponent, TState> : BaseComponentAgent<TComponent>
+    where TComponent : StateComponent<TState>
+    where TState : class, ICacheState, new()
 {
     /// <summary>
-    /// 状态组件代理
+    /// 获取数据对象
     /// </summary>
-    /// <typeparam name="TComponent">组件对象</typeparam>
-    /// <typeparam name="TState">数据对象</typeparam>
-    public abstract class StateComponentAgent<TComponent, TState> : BaseComponentAgent<TComponent> where TComponent : StateComponent<TState> where TState : class, ICacheState, new()
+    public TState State
     {
-        /// <summary>
-        /// 数据对象
-        /// </summary>
-        public TState State
-        {
-            get { return OwnerComponent.State; }
-        }
+        get { return OwnerComponent.State; }
     }
 }
