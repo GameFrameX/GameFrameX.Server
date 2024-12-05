6b43e92d2c7a6a2f592f28e94635b4b109d3a0f1
Blank <wangfj11@foxmail.com>
2024-12-05 19:26:34 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core.Config/IDataTable.cs
+++ GameFrameX.Core.Config/IDataTable.cs
@@ -6,86 +6,87 @@
 public interface IDataTable
 {
     /// <summary>
-    /// 异步加载
+    /// 异步加载数据表。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>一个任务表示异步操作。</returns>
     Task LoadAsync();
 
     /// <summary>
-    /// 获取数据表中对象的数量
+    /// 获取数据表中对象的数量。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>数据表中对象的数量。</returns>
     int Count { get; }
 }
 
 /// <summary>
-/// 数据表基础接口
+/// 泛型数据表基础接口。
 /// </summary>
+/// <typeparam name="T">数据表中对象的类型。</typeparam>
 public interface IDataTable<out T> : IDataTable
 {
     /// <summary>
-    /// 根据ID获取对象
+    /// 根据ID获取对象。
     /// </summary>
-    /// <param name="id">表唯一主键ID</param>
-    /// <returns></returns>
+    /// <param name="id">表唯一主键ID。</param>
+    /// <returns>指定ID的对象。</returns>
     T Get(int id);
 
     /// <summary>
-    /// 根据ID获取对象
+    /// 根据ID获取对象。
     /// </summary>
-    /// <param name="id">表唯一主键ID</param>
-    /// <returns></returns>
+    /// <param name="id">表唯一主键ID。</param>
+    /// <returns>指定ID的对象。</returns>
     T Get(string id);
 
     /// <summary>
-    /// 根据ID获取对象
+    /// 根据ID获取对象。
     /// </summary>
-    /// <param name="id">表唯一主键ID</param>
-    /// <returns></returns>
+    /// <param name="id">表唯一主键ID。</param>
+    /// <returns>指定ID的对象。</returns>
     T this[int id] { get; }
 
     /// <summary>
-    /// 根据ID获取对象
+    /// 根据ID获取对象。
     /// </summary>
-    /// <param name="id">表唯一主键ID</param>
-    /// <returns></returns>
+    /// <param name="id">表唯一主键ID。</param>
+    /// <returns>指定ID的对象。</returns>
     T this[string id] { get; }
 
     /// <summary>
-    /// 获取数据表中第一个对象
+    /// 获取数据表中第一个对象。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>数据表中的第一个对象。</returns>
     T FirstOrDefault { get; }
 
     /// <summary>
-    /// 获取数据表中最后一个对象
+    /// 获取数据表中最后一个对象。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>数据表中的最后一个对象。</returns>
     T LastOrDefault { get; }
 
     /// <summary>
-    /// 获取数据表中所有对象
+    /// 获取数据表中所有对象。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>数据表中的所有对象数组。</returns>
     T[] All { get; }
 
     /// <summary>
-    /// 获取数据表中所有对象
+    /// 获取数据表中所有对象。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>数据表中的所有对象数组。</returns>
     T[] ToArray();
 
     /// <summary>
-    /// 根据条件查找,第一个满足条件的对象
+    /// 根据条件查找，返回第一个满足条件的对象。
     /// </summary>
-    /// <param name="func">查询条件表达式</param>
-    /// <returns></returns>
+    /// <param name="func">查询条件表达式。</param>
+    /// <returns>第一个满足条件的对象。</returns>
     T Find(System.Func<T, bool> func);
 
     /// <summary>
-    /// 根据条件查找,获得所有满足条件的对象
+    /// 根据条件查找，返回所有满足条件的对象。
     /// </summary>
-    /// <param name="func">查询条件表达式</param>
-    /// <returns></returns>
+    /// <param name="func">查询条件表达式。</param>
+    /// <returns>所有满足条件的对象数组。</returns>
     T[] FindList(System.Func<T, bool> func);
 }
