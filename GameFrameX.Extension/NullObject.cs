38bc61e07cf08103cf18cecce328d8891ca47e3d
Blank <wangfj11@foxmail.com>
2024-12-05 17:59:50 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Extension/NullObject.cs
+++ GameFrameX.Extension/NullObject.cs
@@ -1,22 +1,22 @@
 ﻿namespace GameFrameX.Extension;
 
 /// <summary>
-/// 可空对象
+/// 表示可为空的对象。
 /// </summary>
-/// <typeparam name="T"></typeparam>
+/// <typeparam name="T">对象的类型。</typeparam>
 public readonly record struct NullObject<T> : IComparable, IComparable<T>, IEquatable<NullObject<T>>
 {
     /// <summary>
-    /// 
+    /// 初始化一个新的 <see cref="NullObject{T}"/> 实例。
     /// </summary>
-    /// <param name="item"></param>
+    /// <param name="item">对象的值。</param>
     public NullObject(T item)
     {
         Item = item;
     }
 
     /// <summary>
-    /// 
+    /// 获取一个表示空值的 <see cref="NullObject{T}"/> 实例。
     /// </summary>
     public static NullObject<T> Null
     {
@@ -24,43 +24,42 @@ public readonly record struct NullObject<T> : IComparable, IComparable<T>, IEqua
     }
 
     /// <summary>
-    /// 
+    /// 获取对象的值。
     /// </summary>
     public T Item { get; }
 
-
     /// <summary>
-    /// 隐式转换
+    /// 将 <see cref="NullObject{T}"/> 隐式转换为类型 <typeparamref name="T"/>。
     /// </summary>
-    /// <param name="nullObject"></param>
+    /// <param name="nullObject">要转换的 <see cref="NullObject{T}"/> 实例。</param>
     public static implicit operator T(NullObject<T> nullObject)
     {
         return nullObject.Item;
     }
 
     /// <summary>
-    /// 隐式转换
+    /// 将类型 <typeparamref name="T"/> 隐式转换为 <see cref="NullObject{T}"/>。
     /// </summary>
-    /// <param name="item"></param>
+    /// <param name="item">要转换的值。</param>
     public static implicit operator NullObject<T>(T item)
     {
         return new NullObject<T>(item);
     }
 
     /// <summary>
-    /// 
+    /// 返回对象的字符串表示形式。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>对象的字符串表示形式，如果对象为 null，则返回 "NULL"。</returns>
     public override string ToString()
     {
         return (Item != null) ? Item.ToString() : "NULL";
     }
 
     /// <summary>
-    /// 
+    /// 比较当前对象与另一个对象。
     /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
+    /// <param name="value">要比较的对象。</param>
+    /// <returns>一个整数，指示当前对象与 <paramref name="value"/> 的相对顺序。</returns>
     public int CompareTo(object value)
     {
         if (value is NullObject<T> nullObject)
@@ -77,10 +76,10 @@ public readonly record struct NullObject<T> : IComparable, IComparable<T>, IEqua
     }
 
     /// <summary>
-    /// 
+    /// 比较当前对象与同一类型的另一个对象。
     /// </summary>
-    /// <param name="other"></param>
-    /// <returns></returns>
+    /// <param name="other">要比较的对象。</param>
+    /// <returns>一个整数，指示当前对象与 <paramref name="other"/> 的相对顺序。</returns>
     public int CompareTo(T other)
     {
         if (other is IComparable c)
@@ -92,9 +91,9 @@ public readonly record struct NullObject<T> : IComparable, IComparable<T>, IEqua
     }
 
     /// <summary>
-    /// 
+    /// 返回当前对象的哈希代码。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>当前对象的哈希代码。</returns>
     public override int GetHashCode()
     {
         return EqualityComparer<T>.Default.GetHashCode(Item);
@@ -104,7 +103,7 @@ public readonly record struct NullObject<T> : IComparable, IComparable<T>, IEqua
     /// 指示当前对象是否等于同一类型的另一个对象。
     /// </summary>
     /// <param name="other">一个与此对象进行比较的对象。</param>
-    /// <returns>如果当前对象等于 <paramref name="other" /> 参数，则为 true；否则为 false。</returns>
+    /// <returns>如果当前对象等于 <paramref name="other"/> 参数，则为 true；否则为 false。</returns>
     public bool Equals(NullObject<T> other)
     {
         return EqualityComparer<T>.Default.Equals(Item, other.Item);
