2c7073bf1c7d8e1ee9eae4279280b2f814f3d150
Blank <wangfj11@foxmail.com>
2024-12-05 19:27:41 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Core.Config/IConfigManager.cs
+++ GameFrameX.Core.Config/IConfigManager.cs
@@ -8,35 +8,35 @@ public interface IConfigManager
     /// <summary>
     /// 获取全局配置项数量。
     /// </summary>
+    /// <returns>全局配置项的数量。</returns>
     int Count { get; }
 
     /// <summary>
     /// 检查是否存在指定全局配置项。
     /// </summary>
-    /// <param name="configName">要检查全局配置项的名称。</param>
-    /// <returns>指定的全局配置项是否存在。</returns>
+    /// <param name="configName">要检查的全局配置项的名称。</param>
+    /// <returns>如果存在指定的全局配置项，则返回 true；否则返回 false。</returns>
     bool HasConfig(string configName);
 
     /// <summary>
     /// 增加指定全局配置项。
     /// </summary>
-    /// <param name="configName">要增加全局配置项的名称。</param>
+    /// <param name="configName">要增加的全局配置项的名称。</param>
     /// <param name="configValue">全局配置项的值。</param>
-    /// <returns>是否增加全局配置项成功。</returns>
     void AddConfig(string configName, IDataTable configValue);
 
     /// <summary>
     /// 移除指定全局配置项。
     /// </summary>
-    /// <param name="configName">要移除全局配置项的名称。</param>
-    /// <returns>是否移除全局配置项成功。</returns>
+    /// <param name="configName">要移除的全局配置项的名称。</param>
+    /// <returns>如果成功移除指定的全局配置项，则返回 true；否则返回 false。</returns>
     bool RemoveConfig(string configName);
 
     /// <summary>
     /// 获取指定全局配置项。
     /// </summary>
-    /// <param name="configName">要获取全局配置项的名称。</param>
-    /// <returns>要获取全局配置项的全局配置项。</returns>
+    /// <param name="configName">要获取的全局配置项的名称。</param>
+    /// <returns>指定名称的全局配置项。</returns>
     IDataTable GetConfig(string configName);
 
     /// <summary>
