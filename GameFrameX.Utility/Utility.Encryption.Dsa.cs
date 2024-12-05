f1f62a3308a8e1ac14d77faee2700d4c7028f8bc
Blank <wangfj11@foxmail.com>
2024-12-05 11:36:21 +0800

    [增加]1. 补全代码的文档注释

--- GameFrameX.Utility/Utility.Encryption.Dsa.cs
+++ GameFrameX.Utility/Utility.Encryption.Dsa.cs
@@ -9,36 +9,36 @@ namespace GameFrameX.Utility;
 public static partial class Encryption
 {
     /// <summary>
-    /// DSA 加密解密
+    /// DSA 加密解密工具类
     /// </summary>
     public sealed class Dsa
     {
-        private readonly DSACryptoServiceProvider _dsa = null;
+        private readonly DSACryptoServiceProvider _dsa;
 
         /// <summary>
-        /// 
+        /// 使用现有的 DSACryptoServiceProvider 实例初始化 Dsa 类
         /// </summary>
-        /// <param name="dsa"></param>
+        /// <param name="dsa">DSACryptoServiceProvider 实例</param>
         public Dsa(DSACryptoServiceProvider dsa)
         {
             _dsa = dsa;
         }
 
         /// <summary>
-        /// 
+        /// 使用 XML 格式的密钥字符串初始化 Dsa 类
         /// </summary>
-        /// <param name="key"></param>
+        /// <param name="key">XML 格式的密钥字符串</param>
         public Dsa(string key)
         {
-            DSACryptoServiceProvider rsa = new DSACryptoServiceProvider();
-            rsa.FromXmlString(key);
-            this._dsa = rsa;
+            DSACryptoServiceProvider dsa = new DSACryptoServiceProvider();
+            dsa.FromXmlString(key);
+            _dsa = dsa;
         }
 
         /// <summary>
-        /// 
+        /// 生成新的 DSA 密钥对，并以 XML 字符串形式返回
         /// </summary>
-        /// <returns></returns>
+        /// <returns>包含私钥和公钥的字典</returns>
         public static Dictionary<string, string> Make()
         {
             Dictionary<string, string> dic = new Dictionary<string, string>();
@@ -49,11 +49,11 @@ public static partial class Encryption
         }
 
         /// <summary>
-        /// 签名
+        /// 使用私钥对数据进行签名
         /// </summary>
-        /// <param name="dataToSign"></param>
-        /// <param name="privateKey"></param>
-        /// <returns></returns>
+        /// <param name="dataToSign">要签名的数据字节数组</param>
+        /// <param name="privateKey">XML 格式的私钥字符串</param>
+        /// <returns>签名后的字节数组</returns>
         public static byte[] SignData(byte[] dataToSign, string privateKey)
         {
             try
@@ -69,23 +69,22 @@ public static partial class Encryption
         }
 
         /// <summary>
-        /// 签名
+        /// 使用私钥对字符串数据进行签名，并返回 Base64 编码的签名字符串
         /// </summary>
-        /// <param name="dataToSign"></param>
-        /// <param name="privateKey"></param>
-        /// <returns></returns>
+        /// <param name="dataToSign">要签名的字符串数据</param>
+        /// <param name="privateKey">XML 格式的私钥字符串</param>
+        /// <returns>Base64 编码的签名字符串</returns>
         public static string SignData(string dataToSign, string privateKey)
         {
             byte[] res = SignData(Encoding.UTF8.GetBytes(dataToSign), privateKey);
             return Convert.ToBase64String(res);
         }
 
-
         /// <summary>
-        /// 签名
+        /// 使用实例化的 DSACryptoServiceProvider 对数据进行签名
         /// </summary>
-        /// <param name="dataToSign"></param>
-        /// <returns></returns>
+        /// <param name="dataToSign">要签名的数据字节数组</param>
+        /// <returns>签名后的字节数组</returns>
         public byte[] SignData(byte[] dataToSign)
         {
             try
@@ -99,24 +98,23 @@ public static partial class Encryption
         }
 
         /// <summary>
-        /// 签名
+        /// 使用实例化的 DSACryptoServiceProvider 对字符串数据进行签名，并返回 Base64 编码的签名字符串
         /// </summary>
-        /// <param name="dataToSign"></param>
-        /// <returns></returns>
+        /// <param name="dataToSign">要签名的字符串数据</param>
+        /// <returns>Base64 编码的签名字符串</returns>
         public string SignData(string dataToSign)
         {
             byte[] res = SignData(Encoding.UTF8.GetBytes(dataToSign));
             return Convert.ToBase64String(res);
         }
 
-
         /// <summary>
-        /// 验证签名
+        /// 使用私钥验证数据的签名
         /// </summary>
-        /// <param name="dataToVerify"></param>
-        /// <param name="signedData"></param>
-        /// <param name="privateKey"></param>
-        /// <returns></returns>
+        /// <param name="dataToVerify">要验证的数据字节数组</param>
+        /// <param name="signedData">签名后的字节数组</param>
+        /// <param name="privateKey">XML 格式的私钥字符串</param>
+        /// <returns>如果签名有效，返回 true；否则返回 false</returns>
         public static bool VerifyData(byte[] dataToVerify, byte[] signedData, string privateKey)
         {
             try
@@ -132,24 +130,23 @@ public static partial class Encryption
         }
 
         /// <summary>
-        /// 验证签名
+        /// 使用私钥验证字符串数据的签名
         /// </summary>
-        /// <param name="dataToVerify"></param>
-        /// <param name="signedData"></param>
-        /// <param name="privateKey"></param>
-        /// <returns></returns>
+        /// <param name="dataToVerify">要验证的字符串数据</param>
+        /// <param name="signedData">Base64 编码的签名字符串</param>
+        /// <param name="privateKey">XML 格式的私钥字符串</param>
+        /// <returns>如果签名有效，返回 true；否则返回 false</returns>
         public static bool VerifyData(string dataToVerify, string signedData, string privateKey)
         {
-            return VerifyData(Encoding.UTF8.GetBytes(dataToVerify), Convert.FromBase64String(signedData),
-                privateKey);
+            return VerifyData(Encoding.UTF8.GetBytes(dataToVerify), Convert.FromBase64String(signedData), privateKey);
         }
 
         /// <summary>
-        /// 验证签名
+        /// 使用实例化的 DSACryptoServiceProvider 验证数据的签名
         /// </summary>
-        /// <param name="dataToVerify"></param>
-        /// <param name="signedData"></param>
-        /// <returns></returns>
+        /// <param name="dataToVerify">要验证的数据字节数组</param>
+        /// <param name="signedData">签名后的字节数组</param>
+        /// <returns>如果签名有效，返回 true；否则返回 false</returns>
         public bool VerifyData(byte[] dataToVerify, byte[] signedData)
         {
             try
@@ -163,11 +160,11 @@ public static partial class Encryption
         }
 
         /// <summary>
-        /// 验证签名
+        /// 使用实例化的 DSACryptoServiceProvider 验证字符串数据的签名
         /// </summary>
-        /// <param name="dataToVerify"></param>
-        /// <param name="signedData"></param>
-        /// <returns></returns>
+        /// <param name="dataToVerify">要验证的字符串数据</param>
+        /// <param name="signedData">Base64 编码的签名字符串</param>
+        /// <returns>如果签名有效，返回 true；否则返回 false</returns>
         public bool VerifyData(string dataToVerify, string signedData)
         {
             try
