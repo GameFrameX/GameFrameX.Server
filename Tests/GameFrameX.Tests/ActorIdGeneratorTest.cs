8842224efff49edf6d171109b5c7285961df33db
Blank <wangfj11@foxmail.com>
2024-12-11 18:05:05 +0800

    [修改]1. 修改Actor Type 的值导致的编译错误

--- Tests/GameFrameX.Tests/ActorIdGeneratorTest.cs
+++ Tests/GameFrameX.Tests/ActorIdGeneratorTest.cs
@@ -10,16 +10,4 @@ public class ActorIdGeneratorTest
     public void Setup()
     {
     }
-
-    [Test]
-    public void compress()
-    {
-        var actorId = ActorIdGenerator.GetActorId((ushort)ActorType.World, 1000);
-
-        var type = ActorIdGenerator.GetActorType(actorId);
-        var serverId = ActorIdGenerator.GetServerId(actorId);
-
-        Assert.That(type == (ushort)ActorType.World, Is.EqualTo(true));
-        Assert.That(serverId == 1000, Is.EqualTo(true));
-    }
 }
