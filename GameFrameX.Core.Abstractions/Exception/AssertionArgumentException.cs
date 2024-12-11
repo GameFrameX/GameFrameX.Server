bef924bd1fed5f4d97be6877b2233d7108f622f8
Blank <wangfj11@foxmail.com>
2024-12-11 17:45:34 +0800

    [增加]1. 增加断言异常的类型定义

---
+++ GameFrameX.Core.Abstractions/Exception/AssertionArgumentException.cs
@@ -0,0 +1,22 @@
+namespace GameFrameX.Core.Abstractions.Exception;
+
+/// <summary>
+/// 断言异常
+/// </summary>
+public abstract class AssertionArgumentException : System.Exception
+{
+    /// <summary>
+    /// 错误码
+    /// </summary>
+    public int ErrorCode { get; private set; }
+
+    /// <summary>
+    /// 构造
+    /// </summary>
+    /// <param name="errorCode">错误码</param>
+    /// <param name="message">错误信息</param>
+    protected AssertionArgumentException(int errorCode, string message) : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+}
