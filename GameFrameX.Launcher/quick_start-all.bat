4ecaef97ddc6b79bb16eb720d32305d702c4ac56
Blank <wangfj11@foxmail.com>
2024-12-21 15:57:36 +0800

    [删除]1. 删除废弃文件

--- GameFrameX.Launcher/quick_start-all.bat
+++
@@ -1,8 +0,0 @@
-@REM 发现中心
-call quick_start-DiscoveryCenter.bat
-
-@REM 网关服务器
-call quick_start-Gateway.bat
-
-@REM 对外服务器
-call quick_start-Router.bat
--- GameFrameX.Launcher/quick_start-discoveryCenter.bat
+++
@@ -1,2 +0,0 @@
-@REM 发现中心
-start dotnet GameFrameX.Launcher.dll --ServerType=DiscoveryCenter --ServerId=21000 --APMPort=21090 --HttpPort=21011 --InnerIp=127.0.0.1 --InnerPort=21001 --OuterIp=127.0.0.1 --OuterPort=21010 --IsDebug=true --IsDebugReceive=true --IsDebugSend=true
