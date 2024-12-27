64cac4e2ac62f0ed11a4a722d61e6c240fa6d396
Blank <wangfj11@foxmail.com>
2024-12-27 21:01:36 +0800

    [增加]1. 增加玩家的ActorID的设置

--- GameFrameX.Hotfix/Logic/Role/Login/ReqPlayerLoginHandler.cs
+++ GameFrameX.Hotfix/Logic/Role/Login/ReqPlayerLoginHandler.cs
@@ -6,6 +6,11 @@ namespace GameFrameX.Hotfix.Logic.Role.Login;
 [MessageMapping(typeof(ReqPlayerLogin))]
 internal class ReqPlayerLoginHandler : PlayerComponentHandler<PlayerComponentAgent>
 {
+    /// <summary>
+    /// 玩家登录初始化
+    /// 这个地方需要设置ActorID为玩家的ID,否则会报错，该代码只会执行一次。全局唯一。不能移除该代码，也请不要修改该函数ActorID
+    /// </summary>
+    /// <returns></returns>
     protected override Task InitActor()
     {
         if (ActorId <= 0 && Message is ReqPlayerLogin reqPlayerLogin)
