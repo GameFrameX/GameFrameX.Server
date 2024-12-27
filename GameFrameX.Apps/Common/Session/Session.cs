a48db7e4befb18ec4f6cc929e7022f9aa4f6a705
Blank <wangfj11@foxmail.com>
2024-12-27 21:01:12 +0800

    [删除]1. 删除无效的代码
    # Conflicts:
    #	GameFrameX.Apps/Common/Session/Session.cs
    #	GameFrameX.Apps/Common/Session/SessionManager.cs

--- GameFrameX.Apps/Common/Session/Session.cs
+++ GameFrameX.Apps/Common/Session/Session.cs
@@ -2,100 +2,84 @@
 using GameFrameX.NetWork.Messages;
 using GameFrameX.Setting;
 
-namespace GameFrameX.Apps.Common.Session
+namespace GameFrameX.Apps.Common.Session;
+
+public sealed class Session
 {
-    public sealed class Session
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    /// <param name="sessionId">连接会话ID</param>
+    /// <param name="netWorkChannel">网络渠道对象</param>
+    public Session(string sessionId, INetWorkChannel netWorkChannel)
     {
-        /// <summary>
-        /// 全局会话ID
-        /// </summary>
-        public string Id { get; }
+        WorkChannel = netWorkChannel;
+        Id = sessionId;
+        CreateTime = DateTime.Now;
+    }
 
-        public long ActorId
+    /// <summary>
+    /// 全局会话ID
+    /// </summary>
+    public string Id { get; }
+
+    /// <summary>
+    /// 角色ID
+    /// </summary>
+    public long RoleId { get; private set; }
+
+    /// <summary>
+    /// 连接时间
+    /// </summary>
+    public DateTime CreateTime { get; }
+
+    /// <summary>
+    /// 连接上下文
+    /// </summary>
+    public INetWorkChannel WorkChannel { get; }
+
+    /// <summary>
+    /// 连接标示，避免自己顶自己的号,客户端每次启动游戏生成一次/或者每个设备一个
+    /// </summary>
+    public string Sign { get; private set; }
+
+    /// <summary>
+    /// 设置角色ID
+    /// </summary>
+    /// <param name="roleId"></param>
+    public void SetRoleId(long roleId)
+    {
+        RoleId = roleId;
+    }
+
+    /// <summary>
+    /// 设置签名
+    /// </summary>
+    /// <param name="sign">签名</param>
+    public void SetSign(string sign)
+    {
+        Sign = sign;
+    }
+
+    /// <summary>
+    /// 发送消息
+    /// </summary>
+    /// <param name="messageObject">消息对象</param>
+    public void Write(MessageObject messageObject)
+    {
+        WorkChannel?.Write(messageObject);
+    }
+
+    /// <summary>
+    /// 发送消息
+    /// </summary>
+    /// <param name="messageObject">消息对象</param>
+    /// <param name="errorCode">消息错误码</param>
+    public async void WriteAsync(MessageObject messageObject, int errorCode = 0)
+    {
+        if (WorkChannel != null)
         {
-            get { return WorkChannel.GetData<long>(GlobalConst.ActorIdKey); }
-            private set { WorkChannel.SetData(GlobalConst.ActorIdKey, value); }
-        }
-
-        /// <summary>
-        /// 角色ID
-        /// </summary>
-        public long RoleId { get; private set; }
-
-        /// <summary>
-        /// 设置角色ID
-        /// </summary>
-        /// <param name="roleId"></param>
-        public void SetRoleId(long roleId)
-        {
-            RoleId = roleId;
-        }
-
-        /// <summary>
-        /// 设置ActorId
-        /// </summary>
-        /// <param name="actorId"></param>
-        public void SetActorId(long actorId)
-        {
-            ActorId = actorId;
-        }
-
-        /// <summary>
-        /// 初始化
-        /// </summary>
-        /// <param name="sessionId">连接会话ID</param>
-        /// <param name="netWorkChannel">网络渠道对象</param>
-        public Session(string sessionId, INetWorkChannel netWorkChannel)
-        {
-            WorkChannel = netWorkChannel;
-            Id = sessionId;
-            CreateTime = DateTime.Now;
-        }
-
-        /// <summary>
-        /// 连接时间
-        /// </summary>
-        public DateTime CreateTime { get; }
-
-        /// <summary>
-        /// 连接上下文
-        /// </summary>
-        public INetWorkChannel WorkChannel { get; }
-
-        /// <summary>
-        /// 连接标示，避免自己顶自己的号,客户端每次启动游戏生成一次/或者每个设备一个
-        /// </summary>
-        public string Sign { get; private set; }
-
-        /// <summary>
-        /// 设置签名
-        /// </summary>
-        /// <param name="sign">签名</param>
-        public void SetSign(string sign)
-        {
-            Sign = sign;
-        }
-
-        /// <summary>
-        /// 发送消息
-        /// </summary>
-        /// <param name="messageObject">消息对象</param>
-        public void Write(MessageObject messageObject)
-        {
-            WorkChannel?.Write(messageObject);
-        }
-
-        /// <summary>
-        /// 发送消息
-        /// </summary>
-        /// <param name="messageObject">消息对象</param>
-        /// <param name="errorCode">消息错误码</param>
-        public async void WriteAsync(MessageObject messageObject, int errorCode = 0)
-        {
-            if (WorkChannel != null)
-            {
-                await WorkChannel.WriteAsync(messageObject, errorCode);
-            }
+            await WorkChannel.WriteAsync(messageObject, errorCode);
         }
     }
 }
--- GameFrameX.Apps/Common/Session/SessionManager.cs
+++ GameFrameX.Apps/Common/Session/SessionManager.cs
@@ -4,181 +4,164 @@ using GameFrameX.Apps.Common.Event;
 using GameFrameX.NetWork.Abstractions;
 using GameFrameX.Setting;
 
-namespace GameFrameX.Apps.Common.Session
+namespace GameFrameX.Apps.Common.Session;
+
+/// <summary>
+/// 管理玩家session，一个玩家一个，下线之后移除，顶号之后释放之前的channel，替换channel
+/// </summary>
+public static class SessionManager
 {
+    private static readonly ConcurrentDictionary<string, Session> SessionMap = new();
+
     /// <summary>
-    /// 管理玩家session，一个玩家一个，下线之后移除，顶号之后释放之前的channel，替换channel
+    /// 玩家数量
     /// </summary>
-    public static class SessionManager
+    /// <returns></returns>
+    public static int Count()
     {
-        private static readonly ConcurrentDictionary<string, Apps.Common.Session.Session> SessionMap = new ConcurrentDictionary<string, Apps.Common.Session.Session>();
+        return SessionMap.Count;
+    }
 
-        /// <summary>
-        /// 玩家数量
-        /// </summary>
-        /// <returns></returns>
-        public static int Count()
-        {
-            return SessionMap.Count;
-        }
+    /// <summary>
+    /// 获取分页玩家列表
+    /// </summary>
+    /// <param name="pageSize"></param>
+    /// <param name="pageIndex"></param>
+    public static List<Session> GetPageList(int pageSize, int pageIndex)
+    {
+        var result = SessionMap.Values.OrderBy(m => m.CreateTime).Where(m => ActorManager.HasActor(m.RoleId)).Skip(pageIndex * pageSize).Take(pageSize).ToList();
+        return result;
+    }
 
-        /// <summary>
-        /// 获取分页玩家列表
-        /// </summary>
-        /// <param name="pageSize"></param>
-        /// <param name="pageIndex"></param>
-        public static List<Session> GetPageList(int pageSize, int pageIndex)
+    /// <summary>
+    /// 踢掉玩家
+    /// </summary>
+    /// <param name="roleId">链接ID</param>
+    public static void KickOffLineByUserId(long roleId)
+    {
+        var roleSession = Get(m => m.RoleId == roleId);
+        if (roleSession != null)
         {
-            var result = SessionMap.Values.OrderBy(m => m.CreateTime).Where(m => ActorManager.HasActor(m.RoleId)).Skip(pageIndex * pageSize).Take(pageSize).ToList();
-            return result;
-        }
-
-        /// <summary>
-        /// 踢掉玩家
-        /// </summary>
-        /// <param name="roleId">链接ID</param>
-        public static void KickOffLineByUserId(long roleId)
-        {
-            var roleSession = Get(m => m.RoleId == roleId);
-            if (roleSession != null)
+            if (SessionMap.TryRemove(roleSession.Id, out var value) && ActorManager.HasActor(roleSession.RoleId))
             {
-                if (SessionMap.TryRemove(roleSession.Id, out var value) && ActorManager.HasActor(roleSession.ActorId))
-                {
-                    EventDispatcher.Dispatch(roleSession.ActorId, (int)EventId.SessionRemove);
-                }
+                EventDispatcher.Dispatch(roleSession.RoleId, (int)EventId.SessionRemove);
             }
         }
+    }
 
-        /// <summary>
-        /// 根据角色ID获取会话对象,且会话对象必须已经存在才会返回
-        /// </summary>
-        /// <param name="roleId">角色ID</param>
-        /// <returns>会话对象</returns>
-        public static Session GetByRoleId(long roleId)
+    /// <summary>
+    /// 根据角色ID获取会话对象,且会话对象必须已经存在才会返回
+    /// </summary>
+    /// <param name="roleId">角色ID</param>
+    /// <returns>会话对象</returns>
+    public static Session GetByRoleId(long roleId)
+    {
+        var roleSession = Get(m => m.RoleId == roleId);
+        if (roleSession != null && ActorManager.HasActor(roleSession.RoleId))
         {
-            var roleSession = Get(m => m.RoleId == roleId);
-            if (roleSession != null && ActorManager.HasActor(roleSession.ActorId))
-            {
-                return roleSession;
-            }
-
             return roleSession;
         }
 
-        /// <summary>
-        /// 根据角色ID获取会话对象.且会话对象必须已经存在才会返回
-        /// </summary>
-        /// <param name="actorId"></param>
-        /// <returns>会话对象</returns>
-        public static Session GetByActorId(long actorId)
+        return roleSession;
+    }
+
+    /// <summary>
+    /// 获取连接会话
+    /// </summary>
+    /// <param name="sessionId">链接ID</param>
+    public static Session Get(string sessionId)
+    {
+        SessionMap.TryGetValue(sessionId, out var value);
+        return value;
+    }
+
+    /// <summary>
+    /// 根据查询条件获取会话对象
+    /// </summary>
+    /// <param name="predicate">查询条件</param>
+    /// <returns>会话对象</returns>
+    public static Session Get(Func<Session, bool> predicate)
+    {
+        return SessionMap.Values.FirstOrDefault(predicate);
+    }
+
+    /// <summary>
+    /// 根据查询条件获取会话对象列表
+    /// </summary>
+    /// <param name="predicate">查询条件</param>
+    /// <returns>会话对象列表</returns>
+    public static List<Session> GetList(Func<Session, bool> predicate)
+    {
+        return SessionMap.Values.Where(predicate).ToList();
+    }
+
+    /// <summary>
+    /// 移除玩家
+    /// </summary>
+    /// <param name="sessionId">链接ID</param>
+    public static Session Remove(string sessionId)
+    {
+        if (SessionMap.TryRemove(sessionId, out var value) && ActorManager.HasActor(value.RoleId))
         {
-            var roleSession = Get(m => m.ActorId == actorId);
-            if (roleSession != null && ActorManager.HasActor(roleSession.ActorId))
+            EventDispatcher.Dispatch(value.RoleId, (int)EventId.SessionRemove);
+        }
+
+        return value;
+    }
+
+    /// <summary>
+    /// 移除全部
+    /// </summary>
+    /// <returns></returns>
+    public static Task RemoveAll()
+    {
+        foreach (var session in SessionMap.Values)
+        {
+            if (ActorManager.HasActor(session.RoleId))
             {
-                return roleSession;
+                EventDispatcher.Dispatch(session.RoleId, (int)EventId.SessionRemove);
             }
-
-            return roleSession;
         }
 
-        /// <summary>
-        /// 获取连接会话
-        /// </summary>
-        /// <param name="sessionId">链接ID</param>
-        public static Session Get(string sessionId)
-        {
-            SessionMap.TryGetValue(sessionId, out var value);
-            return value;
-        }
+        SessionMap.Clear();
+        return Task.CompletedTask;
+    }
 
-        /// <summary>
-        /// 根据查询条件获取会话对象
-        /// </summary>
-        /// <param name="predicate">查询条件</param>
-        /// <returns>会话对象</returns>
-        public static Session Get(Func<Session, bool> predicate)
-        {
-            return SessionMap.Values.FirstOrDefault(predicate);
-        }
+    /// <summary>
+    /// 获取链接
+    /// </summary>
+    /// <param name="sessionId"></param>
+    /// <returns></returns>
+    public static INetWorkChannel GetChannel(string sessionId)
+    {
+        SessionMap.TryGetValue(sessionId, out var session);
+        return session?.WorkChannel;
+    }
 
-        /// <summary>
-        /// 根据查询条件获取会话对象列表
-        /// </summary>
-        /// <param name="predicate">查询条件</param>
-        /// <returns>会话对象列表</returns>
-        public static List<Session> GetList(Func<Session, bool> predicate)
+    /// <summary>
+    /// 添加新连接
+    /// </summary>
+    /// <param name="session"></param>
+    public static void Add(Session session)
+    {
+        if (SessionMap.TryGetValue(session.Id, out var oldSession) && oldSession.WorkChannel != session.WorkChannel)
         {
-            return SessionMap.Values.Where(predicate).ToList();
-        }
-
-        /// <summary>
-        /// 移除玩家
-        /// </summary>
-        /// <param name="sessionId">链接ID</param>
-        public static Session Remove(string sessionId)
-        {
-            if (SessionMap.TryRemove(sessionId, out var value) && ActorManager.HasActor(value.ActorId))
+            if (oldSession.Sign != session.Sign)
             {
-                EventDispatcher.Dispatch(value.ActorId, (int)EventId.SessionRemove);
-            }
-
-            return value;
-        }
-
-        /// <summary>
-        /// 移除全部
-        /// </summary>
-        /// <returns></returns>
-        public static Task RemoveAll()
-        {
-            foreach (var session in SessionMap.Values)
-            {
-                if (ActorManager.HasActor(session.ActorId))
+                var msg = new RespPrompt
                 {
-                    EventDispatcher.Dispatch(session.ActorId, (int)EventId.SessionRemove);
-                }
+                    Type = 5,
+                    Content = "你的账号已在其他设备上登陆",
+                };
+                oldSession.WriteAsync(msg);
             }
 
-            SessionMap.Clear();
-            return Task.CompletedTask;
+            // 新连接 or 顶号
+            oldSession.WorkChannel.ClearData();
+            oldSession.WorkChannel.Close();
         }
 
-        /// <summary>
-        /// 获取链接
-        /// </summary>
-        /// <param name="sessionId"></param>
-        /// <returns></returns>
-        public static INetWorkChannel GetChannel(string sessionId)
-        {
-            SessionMap.TryGetValue(sessionId, out var session);
-            return session?.WorkChannel;
-        }
-
-        /// <summary>
-        /// 添加新连接
-        /// </summary>
-        /// <param name="session"></param>
-        public static void Add(Session session)
-        {
-            if (SessionMap.TryGetValue(session.Id, out var oldSession) && oldSession.WorkChannel != session.WorkChannel)
-            {
-                if (oldSession.Sign != session.Sign)
-                {
-                    var msg = new RespPrompt
-                    {
-                        Type = 5,
-                        Content = "你的账号已在其他设备上登陆"
-                    };
-                    oldSession.WriteAsync(msg);
-                }
-
-                // 新连接 or 顶号
-                oldSession.WorkChannel.ClearData();
-                oldSession.WorkChannel.Close();
-            }
-
-            session.WorkChannel.SetData(GlobalConst.SessionIdKey, session.Id);
-            SessionMap[session.Id] = session;
-        }
+        session.WorkChannel.SetData(GlobalConst.SessionIdKey, session.Id);
+        SessionMap[session.Id] = session;
     }
 }
