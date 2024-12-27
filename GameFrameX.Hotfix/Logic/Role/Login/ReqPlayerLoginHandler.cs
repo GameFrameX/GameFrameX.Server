1984c97f0ad45aaa6c38a0a8de271b0c8ebb7cdc
Blank <wangfj11@foxmail.com>
2024-12-27 20:45:29 +0800

    #9  [修复]1. 修复ActorId 和PlayerID不一致的问题
    https://github.com/GameFrameX/GameFrameX.Server/issues/9

--- GameFrameX.Hotfix/Logic/Role/Login/ReqPlayerLoginHandler.cs
+++ GameFrameX.Hotfix/Logic/Role/Login/ReqPlayerLoginHandler.cs
@@ -6,6 +6,17 @@ namespace GameFrameX.Hotfix.Logic.Role.Login;
 [MessageMapping(typeof(ReqPlayerLogin))]
 internal class ReqPlayerLoginHandler : PlayerComponentHandler<PlayerComponentAgent>
 {
+    protected override Task InitActor()
+    {
+        if (ActorId <= 0 && Message is ReqPlayerLogin reqPlayerLogin)
+        {
+            NetWorkChannel.SetData(GlobalConst.ActorIdKey, reqPlayerLogin.Id);
+            ActorId = reqPlayerLogin.Id;
+        }
+
+        return base.InitActor();
+    }
+
     protected override async Task ActionAsync()
     {
         await ComponentAgent.OnPlayerLogin(NetWorkChannel, Message as ReqPlayerLogin);
