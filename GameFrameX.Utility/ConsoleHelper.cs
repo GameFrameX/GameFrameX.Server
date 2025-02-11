2869658c4d996d99caa287dd009a4becf73e9f62
Blank <wangfj11@foxmail.com>
2025-02-11 14:27:08 +0800

    [修改]1. 修改Logo 日志的打印

--- GameFrameX.Utility/ConsoleHelper.cs
+++ GameFrameX.Utility/ConsoleHelper.cs
@@ -10,15 +10,16 @@ public static class ConsoleHelper
     /// </summary>
     public static void ConsoleLogo()
     {
-        Console.WriteLine(@" _____                         ______                               __   __");
+        Console.WriteLine("                                                                           ");
+        Console.WriteLine(" _____                         ______                               __   __");
         Console.WriteLine(@"|  __ \                        |  ___|                              \ \ / /");
         Console.WriteLine(@"| |  \/  __ _  _ __ ___    ___ | |_    _ __   __ _  _ __ ___    ___  \ V / ");
         Console.WriteLine(@"| | __  / _` || '_ ` _ \  / _ \|  _|  | '__| / _` || '_ ` _ \  / _ \ /   \ ");
         Console.WriteLine(@"| |_\ \| (_| || | | | | ||  __/| |    | |   | (_| || | | | | ||  __// /^\ \");
         Console.WriteLine(@" \____/ \__,_||_| |_| |_| \___|\_|    |_|    \__,_||_| |_| |_| \___|\/   \/");
-        Console.WriteLine(@"                                                                           ");
-        Console.WriteLine(@"           项目主页:https://github.com/GameFrameX/GameFrameX                ");
-        Console.WriteLine(@"           在线文档:https://gameframex.doc.alianblank.com                   ");
-        Console.WriteLine(@"                                                                           ");
+        Console.WriteLine("                                                                           ");
+        Console.WriteLine("           项目主页:https://github.com/GameFrameX/GameFrameX                ");
+        Console.WriteLine("           在线文档:https://gameframex.doc.alianblank.com                   ");
+        Console.WriteLine("                                                                           ");
     }
 }
