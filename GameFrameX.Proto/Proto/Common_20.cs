dad4d0eef7b5e684e3d0e3d2c1b85c20039e604f
Blank <wangfj11@foxmail.com>
2025-02-11 10:52:23 +0800

    [修改]1. 修改错误码的实现

--- GameFrameX.Proto/Proto/Common_20.cs
+++ GameFrameX.Proto/Proto/Common_20.cs
@@ -51,6 +51,103 @@ namespace GameFrameX.Proto.Proto
     }
 
     /// <summary>
+    /// 操作错误代码
+    /// </summary>
+    [System.ComponentModel.Description("操作错误代码")]
+    public enum OperationStatusCode
+    {
+        /// <summary>
+        /// 成功
+        /// </summary>
+        [System.ComponentModel.Description("成功")]
+        Ok = 0,
+
+        /// <summary>
+        /// 配置表错误
+        /// </summary>
+        [System.ComponentModel.Description("配置表错误")]
+        ConfigErr = 1,
+
+        /// <summary>
+        /// 客户端传递参数错误
+        /// </summary>
+        [System.ComponentModel.Description("客户端传递参数错误")]
+        ParamErr = 2,
+
+        /// <summary>
+        /// 消耗不足
+        /// </summary>
+        [System.ComponentModel.Description("消耗不足")]
+        CostNotEnough = 3,
+
+        /// <summary>
+        /// 未开通服务
+        /// </summary>
+        [System.ComponentModel.Description("未开通服务")]
+        Forbidden = 4,
+
+        /// <summary>
+        /// 不存在
+        /// </summary>
+        [System.ComponentModel.Description("不存在")]
+        NotFound = 5,
+
+        /// <summary>
+        /// 已经存在
+        /// </summary>
+        [System.ComponentModel.Description("已经存在")]
+        HasExist = 6,
+
+        /// <summary>
+        /// 账号不存在或为空
+        /// </summary>
+        [System.ComponentModel.Description("账号不存在或为空")]
+        AccountCannotBeNull = 7,
+
+        /// <summary>
+        /// 无法执行数据库修改
+        /// </summary>
+        [System.ComponentModel.Description("无法执行数据库修改")]
+        Unprocessable = 8,
+
+        /// <summary>
+        /// 未知平台
+        /// </summary>
+        [System.ComponentModel.Description("未知平台")]
+        UnknownPlatform = 9,
+
+        /// <summary>
+        /// 正常通知
+        /// </summary>
+        [System.ComponentModel.Description("正常通知")]
+        Notice = 10,
+
+        /// <summary>
+        /// 功能未开启，主消息屏蔽
+        /// </summary>
+        [System.ComponentModel.Description("功能未开启，主消息屏蔽")]
+        FuncNotOpen = 11,
+
+        /// <summary>
+        /// 其他
+        /// </summary>
+        [System.ComponentModel.Description("其他")]
+        Other = 12,
+
+        /// <summary>
+        /// 内部服务错误
+        /// </summary>
+        [System.ComponentModel.Description("内部服务错误")]
+        InternalServerError = 13,
+
+        /// <summary>
+        /// 通知客户端服务器人数已达上限
+        /// </summary>
+        [System.ComponentModel.Description("通知客户端服务器人数已达上限")]
+        ServerFullyLoaded = 14,
+    }
+
+    /// <summary>
     /// 
     /// </summary>
     [ProtoContract]
