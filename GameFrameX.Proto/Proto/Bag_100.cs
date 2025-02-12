da5c2c1fd1ec2d1f67b5b3abdc9ce1cc465393b0
Blank <wangfj11@foxmail.com>
2025-02-12 16:59:24 +0800

    [修改]1. 修改背包协议的导出

--- GameFrameX.Proto/Proto/Bag_100.cs
+++ GameFrameX.Proto/Proto/Bag_100.cs
@@ -9,20 +9,23 @@ namespace GameFrameX.Proto.Proto
     /// <summary>
     /// 
     /// </summary>
+    [ProtoContract]
     [System.ComponentModel.Description("")]
-    public enum BagType
+    public sealed class BagItem
     {
         /// <summary>
-        /// 默认
+        /// 道具id
         /// </summary>
-        [System.ComponentModel.Description("默认")]
-        Default = 0,
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("道具id")]
+        public int ItemId { get; set; }
 
         /// <summary>
-        /// 宠物
+        /// 道具数量
         /// </summary>
-        [System.ComponentModel.Description("宠物")]
-        Pet = 1,
+        [ProtoMember(2)]
+        [System.ComponentModel.Description("道具数量")]
+        public long Count { get; set; }
     }
 
     /// <summary>
@@ -33,12 +36,6 @@ namespace GameFrameX.Proto.Proto
     [MessageTypeHandler(6553610)]
     public sealed class ReqBagInfo : MessageObject, IRequestMessage
     {
-        /// <summary>
-        /// 背包类型
-        /// </summary>
-        [ProtoMember(1)]
-        [System.ComponentModel.Description("背包类型")]
-        public BagType BagType { get; set; }
     }
 
     /// <summary>
@@ -66,11 +63,41 @@ namespace GameFrameX.Proto.Proto
     }
 
     /// <summary>
+    /// 
+    /// </summary>
+    [ProtoContract]
+    [System.ComponentModel.Description("")]
+    [MessageTypeHandler(6553612)]
+    public sealed class NotifyBagItem : MessageObject, INotifyMessage
+    {
+        /// <summary>
+        /// 道具id
+        /// </summary>
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("道具id")]
+        public int ItemId { get; set; }
+
+        /// <summary>
+        /// 最终道具数量
+        /// </summary>
+        [ProtoMember(2)]
+        [System.ComponentModel.Description("最终道具数量")]
+        public long Count { get; set; }
+
+        /// <summary>
+        /// 变化的值
+        /// </summary>
+        [ProtoMember(3)]
+        [System.ComponentModel.Description("变化的值")]
+        public long Value { get; set; }
+    }
+
+    /// <summary>
     /// 通知背包数据变化
     /// </summary>
     [ProtoContract]
     [System.ComponentModel.Description("通知背包数据变化")]
-    [MessageTypeHandler(6553612)]
+    [MessageTypeHandler(6553613)]
     public sealed class NotifyBagInfoChanged : MessageObject, INotifyMessage
     {
         /// <summary>
@@ -79,7 +106,7 @@ namespace GameFrameX.Proto.Proto
         [ProtoMember(1)]
         [System.ComponentModel.Description("变化的道具，key:道具id，value:数量")]
         [ProtoMap(DisableMap = true)]
-        public Dictionary<int, long> ItemDic { get; set; } = new Dictionary<int, long>();
+        public Dictionary<int, NotifyBagItem> ItemDic { get; set; } = new Dictionary<int, NotifyBagItem>();
     }
 
     /// <summary>
@@ -87,7 +114,7 @@ namespace GameFrameX.Proto.Proto
     /// </summary>
     [ProtoContract]
     [System.ComponentModel.Description("请求合成宠物")]
-    [MessageTypeHandler(6553613)]
+    [MessageTypeHandler(6553614)]
     public sealed class ReqComposePet : MessageObject, IRequestMessage
     {
         /// <summary>
@@ -103,7 +130,7 @@ namespace GameFrameX.Proto.Proto
     /// </summary>
     [ProtoContract]
     [System.ComponentModel.Description("返回合成宠物")]
-    [MessageTypeHandler(6553614)]
+    [MessageTypeHandler(6553615)]
     public sealed class RespComposePet : MessageObject, IResponseMessage
     {
         /// <summary>
@@ -126,7 +153,7 @@ namespace GameFrameX.Proto.Proto
     /// </summary>
     [ProtoContract]
     [System.ComponentModel.Description("请求使用道具")]
-    [MessageTypeHandler(6553615)]
+    [MessageTypeHandler(6553616)]
     public sealed class ReqUseItem : MessageObject, IRequestMessage
     {
         /// <summary>
@@ -135,6 +162,96 @@ namespace GameFrameX.Proto.Proto
         [ProtoMember(1)]
         [System.ComponentModel.Description("道具id")]
         public int ItemId { get; set; }
+
+        /// <summary>
+        /// 道具数量
+        /// </summary>
+        [ProtoMember(2)]
+        [System.ComponentModel.Description("道具数量")]
+        public long Count { get; set; }
+    }
+
+    /// <summary>
+    /// 请求使用道具
+    /// </summary>
+    [ProtoContract]
+    [System.ComponentModel.Description("请求使用道具")]
+    [MessageTypeHandler(6553617)]
+    public sealed class RespUseItem : MessageObject, IResponseMessage
+    {
+        /// <summary>
+        /// 道具id
+        /// </summary>
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("道具id")]
+        public int ItemId { get; set; }
+
+        /// <summary>
+        /// 道具数量
+        /// </summary>
+        [ProtoMember(2)]
+        [System.ComponentModel.Description("道具数量")]
+        public long Count { get; set; }
+
+        /// <summary>
+        /// 返回的错误码
+        /// </summary>
+        [ProtoMember(888)]
+        [System.ComponentModel.Description("返回的错误码")]
+        public int ErrorCode { get; set; }
+    }
+
+    /// <summary>
+    /// 丢弃物品请求
+    /// </summary>
+    [ProtoContract]
+    [System.ComponentModel.Description("丢弃物品请求")]
+    [MessageTypeHandler(6553618)]
+    public sealed class ReqDiscardItem : MessageObject, IRequestMessage
+    {
+        /// <summary>
+        /// 道具id
+        /// </summary>
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("道具id")]
+        public int ItemId { get; set; }
+
+        /// <summary>
+        /// 道具数量
+        /// </summary>
+        [ProtoMember(2)]
+        [System.ComponentModel.Description("道具数量")]
+        public long Count { get; set; }
+    }
+
+    /// <summary>
+    /// 丢弃物品返回
+    /// </summary>
+    [ProtoContract]
+    [System.ComponentModel.Description("丢弃物品返回")]
+    [MessageTypeHandler(6553619)]
+    public sealed class RespDiscardItem : MessageObject, IResponseMessage
+    {
+        /// <summary>
+        /// 道具id
+        /// </summary>
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("道具id")]
+        public int ItemId { get; set; }
+
+        /// <summary>
+        /// 道具数量
+        /// </summary>
+        [ProtoMember(2)]
+        [System.ComponentModel.Description("道具数量")]
+        public long Count { get; set; }
+
+        /// <summary>
+        /// 返回的错误码
+        /// </summary>
+        [ProtoMember(888)]
+        [System.ComponentModel.Description("返回的错误码")]
+        public int ErrorCode { get; set; }
     }
 
     /// <summary>
@@ -142,7 +259,7 @@ namespace GameFrameX.Proto.Proto
     /// </summary>
     [ProtoContract]
     [System.ComponentModel.Description("出售道具")]
-    [MessageTypeHandler(6553616)]
+    [MessageTypeHandler(6553620)]
     public sealed class ReqSellItem : MessageObject, IRequestMessage
     {
         /// <summary>
@@ -158,8 +275,90 @@ namespace GameFrameX.Proto.Proto
     /// </summary>
     [ProtoContract]
     [System.ComponentModel.Description("出售道具")]
-    [MessageTypeHandler(6553617)]
+    [MessageTypeHandler(6553621)]
     public sealed class RespItemChange : MessageObject, IResponseMessage
+    {
+        /// <summary>
+        /// 变化的道具
+        /// </summary>
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("变化的道具")]
+        [ProtoMap(DisableMap = true)]
+        public Dictionary<long, long> ItemDic { get; set; } = new Dictionary<long, long>();
+
+        /// <summary>
+        /// 返回的错误码
+        /// </summary>
+        [ProtoMember(888)]
+        [System.ComponentModel.Description("返回的错误码")]
+        public int ErrorCode { get; set; }
+    }
+
+    /// <summary>
+    /// 增加道具
+    /// </summary>
+    [ProtoContract]
+    [System.ComponentModel.Description("增加道具")]
+    [MessageTypeHandler(6553622)]
+    public sealed class ReqAddItem : MessageObject, IRequestMessage
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("")]
+        [ProtoMap(DisableMap = true)]
+        public Dictionary<int, long> ItemDic { get; set; } = new Dictionary<int, long>();
+    }
+
+    /// <summary>
+    /// 增加道具返回
+    /// </summary>
+    [ProtoContract]
+    [System.ComponentModel.Description("增加道具返回")]
+    [MessageTypeHandler(6553623)]
+    public sealed class RespAddItem : MessageObject, IResponseMessage
+    {
+        /// <summary>
+        /// 变化的道具
+        /// </summary>
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("变化的道具")]
+        [ProtoMap(DisableMap = true)]
+        public Dictionary<int, long> ItemDic { get; set; } = new Dictionary<int, long>();
+
+        /// <summary>
+        /// 返回的错误码
+        /// </summary>
+        [ProtoMember(888)]
+        [System.ComponentModel.Description("返回的错误码")]
+        public int ErrorCode { get; set; }
+    }
+
+    /// <summary>
+    /// 减少道具
+    /// </summary>
+    [ProtoContract]
+    [System.ComponentModel.Description("减少道具")]
+    [MessageTypeHandler(6553624)]
+    public sealed class ReqRemoveItem : MessageObject, IRequestMessage
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [ProtoMember(1)]
+        [System.ComponentModel.Description("")]
+        [ProtoMap(DisableMap = true)]
+        public Dictionary<int, long> ItemDic { get; set; } = new Dictionary<int, long>();
+    }
+
+    /// <summary>
+    /// 减少道具返回
+    /// </summary>
+    [ProtoContract]
+    [System.ComponentModel.Description("减少道具返回")]
+    [MessageTypeHandler(6553625)]
+    public sealed class RespRemoveItem : MessageObject, IResponseMessage
     {
         /// <summary>
         /// 变化的道具
