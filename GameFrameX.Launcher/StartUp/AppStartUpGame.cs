8107c7a80e86683c61f3e1bd3113ddbf60b6ac9e
Blank <wangfj11@foxmail.com>
2025-03-14 16:53:25 +0800

    [增加]1. 增加DB初始化失败的退出处理

--- GameFrameX.Launcher/StartUp/AppStartUpGame.cs
+++ GameFrameX.Launcher/StartUp/AppStartUpGame.cs
@@ -1,4 +1,6 @@
-﻿namespace GameFrameX.Launcher.StartUp;
+﻿using GameFrameX.DataBase.Abstractions;
+
+namespace GameFrameX.Launcher.StartUp;
 
 /// <summary>
 /// 游戏服务器
@@ -23,7 +25,12 @@ internal sealed class AppStartUpGame : AppStartUpBase
             LogHelper.DebugConsole("配置Actor限制逻辑结束...");
 
             LogHelper.DebugConsole("开始启动数据库服务...");
-            await GameDb.Init<MongoDbService>(Setting.DataBaseUrl, Setting.DataBaseName);
+            var initResult = await GameDb.Init<MongoDbService>(new DbOptions { ConnectionString = Setting.DataBaseUrl, Name = Setting.DataBaseName, });
+            if (initResult == false)
+            {
+                throw new InvalidOperationException("数据库服务启动失败");
+            }
+
             LogHelper.DebugConsole("启动数据库服务 结束...");
 
             LogHelper.DebugConsole("注册组件开始...");
