5721a917598f9a1ae252fbf7d64cb8f60ce7affc
Blank <wangfj11@foxmail.com>
2025-03-25 18:01:24 +0800

    [增加]1. 增加State的写入函数接口定义

--- GameFrameX.Core/Abstractions/IState.cs
+++ GameFrameX.Core/Abstractions/IState.cs
@@ -2,12 +2,26 @@
 
 /// <summary>
 /// 状态接口
+/// 用于管理和维护对象的状态信息
 /// </summary>
 public interface IState
 {
     /// <summary>
     /// 读取状态
     /// </summary>
-    /// <returns>一个表示异步操作的任务</returns>
+    /// <returns>一个表示异步操作的任务，该任务在状态读取完成时完成</returns>
+    /// <remarks>
+    /// 此方法用于异步读取对象的当前状态信息
+    /// </remarks>
     public Task ReadStateAsync();
+
+    /// <summary>
+    /// 更新状态
+    /// </summary>
+    /// <returns>一个表示异步操作的任务，该任务在状态更新完成时完成</returns>
+    /// <remarks>
+    /// 此方法用于异步更新对象的状态信息
+    /// 在状态发生变化时应调用此方法以保持状态的同步
+    /// </remarks>
+    public Task WriteStateAsync();
 }
