3713d57b68ca9eea41af6d71f1a0a48933b45394
Blank <wangfj11@foxmail.com>
2025-03-28 21:02:15 +0800

    [修改]1. 修改事件参数类型的命名

---
+++ GameFrameX.Core/Abstractions/Events/GameEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using GameFrameX.Utility;
+
+namespace GameFrameX.Core.Abstractions.Events;
+
+/// <summary>
+/// 事件参数
+/// </summary>
+public struct GameEventArgs
+{
+    /// <summary>
+    /// 默认事件
+    /// </summary>
+    public static readonly GameEventArgs Default = new GameEventArgs();
+
+    /// <summary>
+    /// 事件id
+    /// </summary>
+    public int EventId;
+
+    /// <summary>
+    /// 事件参数
+    /// </summary>
+    public Param Data;
+}
