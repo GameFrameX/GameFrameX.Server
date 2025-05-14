80fca08b1ea535f2337096fad410c7d340c25ce5
Blank <wangfj11@foxmail.com>
2025-05-14 14:52:19 +0800

    [修改]1. 修改压缩和解压缩算法为Deflate和Inflate

--- GameFrameX.Utility/CompressionHelper.cs
+++ GameFrameX.Utility/CompressionHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System.Buffers;
+using GameFrameX.Foundation.Logger;
 using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Zip.Compression;
 
 namespace GameFrameX.Utility;
 
@@ -9,10 +11,15 @@ namespace GameFrameX.Utility;
 public static class CompressionHelper
 {
     /// <summary>
-    /// 压缩数据。使用GZip算法将原始字节数组压缩成更小的字节数组。
+    /// 用于压缩和解压缩操作的缓冲区大小（以字节为单位）
+    /// </summary>
+    private const int BufferSize = 8192;
+
+    /// <summary>
+    /// 压缩数据。使用Deflate算法将原始字节数组压缩成更小的字节数组。
     /// </summary>
     /// <param name="bytes">要压缩的原始字节数组。不能为null。</param>
-    /// <returns>压缩后的字节数组。如果输入为空数组，则直接返回该空数组。</returns>
+    /// <returns>压缩后的字节数组。如果输入为空数组，则直接返回该空数组。如果压缩过程中发生异常，则返回原始数组。</returns>
     /// <exception cref="ArgumentNullException">当输入参数bytes为null时抛出。</exception>
     public static byte[] Compress(byte[] bytes)
     {
@@ -22,22 +29,41 @@ public static class CompressionHelper
             return bytes;
         }
 
-        using (var compressStream = new MemoryStream())
+        var compressor = new Deflater();
+        compressor.SetLevel(Deflater.BEST_COMPRESSION);
+
+        compressor.SetInput(bytes);
+        compressor.Finish();
+
+        using var compressorMemoryStream = new MemoryStream(bytes.Length);
+        var buffer = ArrayPool<byte>.Shared.Rent(BufferSize);
+        try
         {
-            using (var gZipOutputStream = new GZipOutputStream(compressStream))
+            while (!compressor.IsFinished)
             {
-                gZipOutputStream.Write(bytes, 0, bytes.Length);
-                var press = compressStream.ToArray();
-                return press;
+                var count = compressor.Deflate(buffer);
+                compressorMemoryStream.Write(buffer, 0, count);
             }
+
+            return compressorMemoryStream.ToArray();
         }
+        catch (Exception e)
+        {
+            LogHelper.Fatal(e);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+
+        return bytes;
     }
 
     /// <summary>
-    /// 解压数据。使用GZip算法将压缩的字节数组还原成原始字节数组。
+    /// 解压数据。使用Inflate算法将压缩的字节数组还原成原始字节数组。
     /// </summary>
     /// <param name="bytes">要解压的压缩字节数组。不能为null。</param>
-    /// <returns>解压后的原始字节数组。如果输入为空数组，则直接返回该空数组。</returns>
+    /// <returns>解压后的原始字节数组。如果输入为空数组，则直接返回该空数组。如果解压过程中发生异常，则返回原始数组。</returns>
     /// <exception cref="ArgumentNullException">当输入参数bytes为null时抛出。</exception>
     /// <exception cref="InvalidDataException">当压缩数据格式无效或已损坏时抛出。</exception>
     public static byte[] Decompress(byte[] bytes)
@@ -48,30 +74,29 @@ public static class CompressionHelper
             return bytes;
         }
 
-        using (var compressedStream = new MemoryStream(bytes))
+        var decompressor = new Inflater();
+        decompressor.SetInput(bytes, 0, bytes.Length);
+        using var decompressMemoryStream = new MemoryStream(bytes.Length);
+        var buffer = ArrayPool<byte>.Shared.Rent(BufferSize);
+        try
         {
-            using (var gZipInputStream = new GZipInputStream(compressedStream))
+            while (!decompressor.IsFinished)
             {
-                using (var decompressedStream = new MemoryStream())
-                {
-                    var buffer = ArrayPool<byte>.Shared.Rent(8192);
-                    try
-                    {
-                        int count;
-                        while ((count = gZipInputStream.Read(buffer, 0, buffer.Length)) != 0)
-                        {
-                            decompressedStream.Write(buffer, 0, count);
-                        }
-                    }
-                    finally
-                    {
-                        ArrayPool<byte>.Shared.Return(buffer);
-                    }
-
-                    var array = decompressedStream.ToArray();
-                    return array;
-                }
+                var count = decompressor.Inflate(buffer);
+                decompressMemoryStream.Write(buffer, 0, count);
             }
+
+            return decompressMemoryStream.ToArray();
         }
+        catch (Exception e)
+        {
+            LogHelper.Fatal(e);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer, true);
+        }
+
+        return bytes;
     }
 }
