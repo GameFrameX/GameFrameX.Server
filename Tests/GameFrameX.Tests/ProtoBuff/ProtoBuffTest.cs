5b10007ca746120e1f5cd713ac05749bd216866f
Blank <wangfj11@foxmail.com>
2025-05-21 15:31:08 +0800

    [增加]1. 增加序列化PB的单元测试

---
+++ Tests/GameFrameX.Tests/ProtoBuff/ProtoBuffTest.cs
@@ -0,0 +1,44 @@
+﻿using GameFrameX.ProtoBuf.Net;
+using ProtoBuf;
+
+namespace GameFrameX.Tests.ProtoBuff;
+
+public class ProtoBuffTest
+{
+    [ProtoContract]
+    public class PbTest
+    {
+        [ProtoMember(1, IsRequired = false)] public List<string> Test { get; set; } = new List<string>();
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestSerialize()
+    {
+        PbTest pbTest = new PbTest();
+        pbTest.Test.Add("1");
+        pbTest.Test.Add(null);
+        pbTest.Test.Add("3");
+        pbTest.Test.Add("4");
+        pbTest.Test.Add("5");
+        pbTest.Test.Add("6");
+        pbTest.Test.Add("7");
+        pbTest.Test.Add("8");
+        pbTest.Test.Add("9");
+
+        try
+        {
+            var xx = ProtoBufSerializerHelper.Serialize(pbTest);
+            Console.WriteLine(xx);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
