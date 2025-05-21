07c9037bf9f80096830382906e58fad71ac9c92d
Blank <wangfj11@foxmail.com>
2025-05-21 19:48:06 +0800

    [修改]1. 修改启动时间为UTC 时间

--- GameFrameX.Utility/Setting/AppSetting.cs
+++ GameFrameX.Utility/Setting/AppSetting.cs
@@ -29,7 +29,7 @@ public sealed class AppSetting
         IsDebugSendHeartBeat = true;
         IsDebugReceiveHeartBeat = true;
 #endif
-        LaunchTime = DateTime.Now;
+        LaunchTime = DateTime.UtcNow;
     }
 
     /// <summary>
