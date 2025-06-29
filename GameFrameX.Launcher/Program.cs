f655e840ac50271d83e0c9076955d8ad0a65499f
Blank <wangfj11@foxmail.com>
2025-06-29 15:59:29 +0800

    [删除]  删除数据打点的程序集和引用

--- GameFrameX.Launcher/Program.cs
+++ GameFrameX.Launcher/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using GameFrameX.GameAnalytics;
 using GameFrameX.NetWork.Abstractions;
 using GameFrameX.Proto.BuiltIn;
 
@@ -9,7 +8,6 @@ internal static class Program
 {
     private static async Task Main(string[] args)
     {
-        GameAnalyticsHelper.Init("ec33486a135ae80984dbfc66eede1e41", "379e705038d46691c5c567db947af1828f702861", Assembly.GetCallingAssembly().ImageRuntimeVersion);
         await GameApp.Entry(args, () =>
         {
             CacheStateTypeManager.Init();
