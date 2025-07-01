c06fc6ba431d30658658d1f69f875b226b057cde
Blank <wangfj11@foxmail.com>
2025-07-02 00:34:46 +0800

    [优化]  网络工具类的参数检查

--- GameFrameX.Utility/NetHelper.cs
+++ GameFrameX.Utility/NetHelper.cs
@@ -6,7 +6,21 @@ namespace GameFrameX.Utility;
 
 /// <summary>
 /// 网络帮助类
+/// 提供网络相关的实用工具方法，包括IP地址验证、端口可用性检查、本地IP获取等功能
 /// </summary>
+/// <remarks>
+/// 核心功能：
+/// - IP地址格式验证和解析
+/// - 端口可用性检测和管理
+/// - 本地网络接口信息获取
+/// - 网络连接状态查询
+/// 
+/// 适用场景：
+/// - 网络服务器端口分配
+/// - 客户端连接配置验证
+/// - 网络诊断和监控
+/// - 分布式系统节点发现
+/// </remarks>
 public static class NetHelper
 {
     /// <summary>
@@ -15,8 +29,11 @@ public static class NetHelper
     /// <param name="ipAddress">IP地址字符串</param>
     /// <param name="value">解析成功的IPAddress对象</param>
     /// <returns>如果IP地址合法，返回true；否则返回false</returns>
+    /// <exception cref="ArgumentNullException">当ipAddress为null时抛出此异常</exception>
     public static bool IsValidIpAddress(string ipAddress, out IPAddress value)
     {
+        ArgumentNullException.ThrowIfNull(ipAddress, nameof(ipAddress));
+
         return IPAddress.TryParse(ipAddress, out value);
     }
 
@@ -26,8 +43,17 @@ public static class NetHelper
     /// <param name="startPort">起始端口号，默认为667</param>
     /// <param name="maxPort">结束端口号，默认为65535</param>
     /// <returns>第一个可用的端口号，如果没有可用端口号则返回-1</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当startPort小于1或大于65535时抛出此异常</exception>
+    /// <exception cref="ArgumentOutOfRangeException">当maxPort小于1或大于65535时抛出此异常</exception>
+    /// <exception cref="ArgumentException">当startPort大于等于maxPort时抛出此异常</exception>
     public static int GetFirstAvailablePort(int startPort = 667, int maxPort = 65535)
     {
+        ArgumentOutOfRangeException.ThrowIfLessThan(startPort, 1, nameof(startPort));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(startPort, 65535, nameof(startPort));
+        ArgumentOutOfRangeException.ThrowIfLessThan(maxPort, 1, nameof(maxPort));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(maxPort, 65535, nameof(maxPort));
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(startPort, maxPort, nameof(startPort));
+
         for (var i = startPort; i < maxPort; i++)
         {
             if (PortIsAvailable(i))
@@ -81,8 +107,12 @@ public static class NetHelper
     /// </summary>
     /// <param name="port">要检查的端口号</param>
     /// <returns>如果端口未被使用，返回true；否则返回false</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当port小于1或大于65535时抛出此异常</exception>
     public static bool PortIsAvailable(int port)
     {
+        ArgumentOutOfRangeException.ThrowIfLessThan(port, 1, nameof(port));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(port, 65535, nameof(port));
+
         var isAvailable = true;
 
         var portUsed = PortIsUsed();
