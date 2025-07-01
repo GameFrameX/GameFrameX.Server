40031f289c831a4f89451d68c4284a84565d84b5
Blank <wangfj11@foxmail.com>
2025-07-02 00:39:13 +0800

    [增加]  网络工具类函数扩展

--- GameFrameX.Utility/NetHelper.cs
+++ GameFrameX.Utility/NetHelper.cs
@@ -179,4 +179,227 @@ public static class NetHelper
 
         return ipList;
     }
+
+    /// <summary>
+    /// 检测网络连通性
+    /// </summary>
+    /// <param name="host">目标主机地址</param>
+    /// <param name="timeout">超时时间（毫秒），默认5000毫秒</param>
+    /// <returns>如果网络连通，返回true；否则返回false</returns>
+    /// <exception cref="ArgumentException">当host为null或空字符串时抛出此异常</exception>
+    /// <exception cref="ArgumentOutOfRangeException">当timeout小于0时抛出此异常</exception>
+    public static bool IsNetworkReachable(string host, int timeout = 5000)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(host, nameof(host));
+        ArgumentOutOfRangeException.ThrowIfNegative(timeout, nameof(timeout));
+
+        try
+        {
+            using var ping = new Ping();
+            var reply = ping.Send(host, timeout);
+            return reply.Status == IPStatus.Success;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 获取本机MAC地址列表
+    /// </summary>
+    /// <returns>MAC地址字符串列表</returns>
+    public static List<string> GetMacAddresses()
+    {
+        var macAddresses = new List<string>();
+        try
+        {
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var network in networkInterfaces)
+            {
+                // 排除环回接口和非活动接口
+                if (network.NetworkInterfaceType == NetworkInterfaceType.Loopback || 
+                    network.OperationalStatus != OperationalStatus.Up)
+                {
+                    continue;
+                }
+
+                var macAddress = network.GetPhysicalAddress().ToString();
+                if (!string.IsNullOrEmpty(macAddress))
+                {
+                    // 格式化MAC地址为标准格式 (XX:XX:XX:XX:XX:XX)
+                    var formattedMac = string.Join(":", 
+                        Enumerable.Range(0, macAddress.Length / 2)
+                        .Select(i => macAddress.Substring(i * 2, 2)));
+                    macAddresses.Add(formattedMac);
+                }
+            }
+        }
+        catch
+        {
+            // 发生异常时返回空列表
+        }
+
+        return macAddresses;
+    }
+
+    /// <summary>
+    /// 验证IP地址是否在指定的子网内
+    /// </summary>
+    /// <param name="ipAddress">要验证的IP地址</param>
+    /// <param name="networkAddress">网络地址</param>
+    /// <param name="subnetMask">子网掩码</param>
+    /// <returns>如果IP地址在子网内，返回true；否则返回false</returns>
+    /// <exception cref="ArgumentException">当IP地址格式无效时抛出此异常</exception>
+    public static bool IsIpInSubnet(string ipAddress, string networkAddress, string subnetMask)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(ipAddress, nameof(ipAddress));
+        ArgumentException.ThrowIfNullOrEmpty(networkAddress, nameof(networkAddress));
+        ArgumentException.ThrowIfNullOrEmpty(subnetMask, nameof(subnetMask));
+
+        if (!IPAddress.TryParse(ipAddress, out var ip) ||
+            !IPAddress.TryParse(networkAddress, out var network) ||
+            !IPAddress.TryParse(subnetMask, out var mask))
+        {
+            throw new ArgumentException("无效的IP地址格式");
+        }
+
+        var ipBytes = ip.GetAddressBytes();
+        var networkBytes = network.GetAddressBytes();
+        var maskBytes = mask.GetAddressBytes();
+
+        if (ipBytes.Length != networkBytes.Length || ipBytes.Length != maskBytes.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < ipBytes.Length; i++)
+        {
+            if ((ipBytes[i] & maskBytes[i]) != (networkBytes[i] & maskBytes[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// 获取指定范围内的可用端口列表
+    /// </summary>
+    /// <param name="startPort">起始端口号</param>
+    /// <param name="endPort">结束端口号</param>
+    /// <param name="maxCount">最大返回数量，默认为10</param>
+    /// <returns>可用端口号列表</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当端口范围无效时抛出此异常</exception>
+    public static List<int> GetAvailablePorts(int startPort, int endPort, int maxCount = 10)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(startPort, 1, nameof(startPort));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(startPort, 65535, nameof(startPort));
+        ArgumentOutOfRangeException.ThrowIfLessThan(endPort, 1, nameof(endPort));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(endPort, 65535, nameof(endPort));
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(startPort, endPort, nameof(startPort));
+        ArgumentOutOfRangeException.ThrowIfLessThan(maxCount, 1, nameof(maxCount));
+
+        var availablePorts = new List<int>();
+        var usedPorts = PortIsUsed().ToHashSet();
+
+        for (int port = startPort; port < endPort && availablePorts.Count < maxCount; port++)
+        {
+            if (!usedPorts.Contains(port))
+            {
+                availablePorts.Add(port);
+            }
+        }
+
+        return availablePorts;
+    }
+
+    /// <summary>
+    /// 获取本机的公网IP地址
+    /// </summary>
+    /// <param name="timeout">超时时间（毫秒），默认10000毫秒</param>
+    /// <returns>公网IP地址字符串，获取失败返回null</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当timeout小于0时抛出此异常</exception>
+    public static async Task<string?> GetPublicIpAddressAsync(int timeout = 10000)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(timeout, nameof(timeout));
+
+        var services = new[]
+        {
+            "https://api.ipify.org",
+            "https://icanhazip.com",
+            "https://ipinfo.io/ip"
+        };
+
+        using var httpClient = new HttpClient { Timeout = TimeSpan.FromMilliseconds(timeout) };
+        
+        foreach (var service in services)
+        {
+            try
+            {
+                var response = await httpClient.GetStringAsync(service);
+                var ip = response.Trim();
+                if (IsValidIpAddress(ip, out _))
+                {
+                    return ip;
+                }
+            }
+            catch
+            {
+                // 尝试下一个服务
+                continue;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// 判断IP地址是否为私有地址
+    /// </summary>
+    /// <param name="ipAddress">IP地址字符串</param>
+    /// <returns>如果是私有地址，返回true；否则返回false</returns>
+    /// <exception cref="ArgumentException">当IP地址格式无效时抛出此异常</exception>
+    public static bool IsPrivateIpAddress(string ipAddress)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(ipAddress, nameof(ipAddress));
+
+        if (!IPAddress.TryParse(ipAddress, out var ip))
+        {
+            throw new ArgumentException("无效的IP地址格式", nameof(ipAddress));
+        }
+
+        var bytes = ip.GetAddressBytes();
+        
+        // IPv4私有地址范围
+        if (ip.AddressFamily == AddressFamily.InterNetwork)
+        {
+            // 10.0.0.0/8
+            if (bytes[0] == 10)
+            {
+                return true;
+            }
+
+            // 172.16.0.0/12
+            if (bytes[0] == 172 && bytes[1] >= 16 && bytes[1] <= 31)
+            {
+                return true;
+            }
+
+            // 192.168.0.0/16
+            if (bytes[0] == 192 && bytes[1] == 168)
+            {
+                return true;
+            }
+
+            // 127.0.0.0/8 (环回地址)
+            if (bytes[0] == 127)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
