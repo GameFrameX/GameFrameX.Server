8c5208a66d8c2552f46c0de7343cbe8f98972b66
Blank <wangfj11@foxmail.com>
2025-07-12 11:43:05 +0800

    [增加]  增加随机数的函数

--- GameFrameX.Utility/RandomHelper.cs
+++ GameFrameX.Utility/RandomHelper.cs
@@ -1,4 +1,4 @@
-﻿using GameFrameX.Foundation.Extensions;
+using GameFrameX.Foundation.Extensions;
 using Tedd.RandomUtils;
 
 namespace GameFrameX.Utility;
@@ -152,6 +152,56 @@ public static class RandomHelper
     public static double NextDouble()
     {
         return RandomShared.NextDouble();
+    }
+
+    /// <summary>
+    /// 返回一个小于所指定最大值的非负随机数。
+    /// </summary>
+    /// <param name="maxValue">要生成的随机数的上界（随机数不能取该上界值）。maxValue 必须大于等于零。</param>
+    /// <returns>大于等于 0.0 且小于 maxValue 的双精度浮点数。</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当 maxValue 小于 0 时抛出此异常</exception>
+    public static double NextDouble(double maxValue)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(maxValue, nameof(maxValue));
+        return RandomShared.NextDouble() * maxValue;
+    }
+
+    /// <summary>
+    /// 返回一个指定范围内的随机数。
+    /// </summary>
+    /// <param name="minValue">返回的随机数的下界（随机数可取该下界值）。</param>
+    /// <param name="maxValue">返回的随机数的上界（随机数不能取该上界值）。maxValue 必须大于等于 minValue。</param>
+    /// <returns>一个大于等于 minValue 且小于 maxValue 的双精度浮点数。</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当 minValue 大于 maxValue 时抛出此异常</exception>
+    public static double NextDouble(double minValue, double maxValue)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(minValue, maxValue, nameof(minValue));
+        return RandomShared.NextDouble() * (maxValue - minValue) + minValue;
+    }
+
+    /// <summary>
+    /// 返回一个小于所指定最大值的非负随机数。
+    /// </summary>
+    /// <param name="maxValue">要生成的随机数的上界（随机数不能取该上界值）。maxValue 必须大于等于零。</param>
+    /// <returns>大于等于零且小于 maxValue 的 64 位带符号整数。</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当 maxValue 小于 0 时抛出此异常</exception>
+    public static long NextLong(long maxValue)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(maxValue, nameof(maxValue));
+        return RandomShared.NextInt64(maxValue);
+    }
+
+    /// <summary>
+    /// 返回一个指定范围内的随机数。
+    /// </summary>
+    /// <param name="minValue">返回的随机数的下界（随机数可取该下界值）。</param>
+    /// <param name="maxValue">返回的随机数的上界（随机数不能取该上界值）。maxValue 必须大于等于 minValue。</param>
+    /// <returns>一个大于等于 minValue 且小于 maxValue 的 64 位带符号整数。</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当 minValue 大于 maxValue 时抛出此异常</exception>
+    public static long NextLong(long minValue, long maxValue)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(minValue, maxValue, nameof(minValue));
+        return RandomShared.NextInt64(minValue, maxValue);
     }
 
     /// <summary>
