0967f9bb66910a55dd41d7c06a256b25205aa969
Blank <wangfj11@foxmail.com>
2025-10-17 14:47:15 +0800

    style(ConsoleHelper): 调整控制台logo显示格式和内容
    更新控制台logo的显示格式，增加空行分隔，并添加Gitee仓库链接和官方文档的英文标识

--- GameFrameX.Utility/ConsoleHelper.cs
+++ GameFrameX.Utility/ConsoleHelper.cs
@@ -41,16 +41,18 @@ public static class ConsoleHelper
     /// </summary>
     public static void ConsoleLogo()
     {
-        Console.WriteLine("                                                                           ");
-        Console.WriteLine(" _____                         ______                               __   __");
-        Console.WriteLine(@"|  __ \                        |  ___|                              \ \ / /");
-        Console.WriteLine(@"| |  \/  __ _  _ __ ___    ___ | |_    _ __   __ _  _ __ ___    ___  \ V / ");
-        Console.WriteLine(@"| | __  / _` || '_ ` _ \  / _ \|  _|  | '__| / _` || '_ ` _ \  / _ \ /   \ ");
-        Console.WriteLine(@"| |_\ \| (_| || | | | | ||  __/| |    | |   | (_| || | | | | ||  __// /^\ \");
-        Console.WriteLine(@" \____/ \__,_||_| |_| |_| \___|\_|    |_|    \__,_||_| |_| |_| \___|\/   \/");
-        Console.WriteLine("                                                                           ");
-        Console.WriteLine("           项目主页:https://github.com/GameFrameX/GameFrameX                ");
-        Console.WriteLine("           在线文档:https://gameframex.doc.alianblank.com                   ");
-        Console.WriteLine("                                                                           ");
+        Console.WriteLine();
+        Console.WriteLine(@"        _____                         ______                               __   __     ");
+        Console.WriteLine(@"       |  __ \                        |  ___|                              \ \ / /     ");
+        Console.WriteLine(@"       | |  \/  __ _  _ __ ___    ___ | |_    _ __   __ _  _ __ ___    ___  \ V /      ");
+        Console.WriteLine(@"       | | __  / _` || '_ ` _ \  / _ \|  _|  | '__| / _` || '_ ` _ \  / _ \ /   \      ");
+        Console.WriteLine(@"       | |_\ \| (_| || | | | | ||  __/| |    | |   | (_| || | | | | ||  __// /^\ \     ");
+        Console.WriteLine(@"        \____/ \__,_||_| |_| |_| \___|\_|    |_|    \__,_||_| |_| |_| \___|\/   \/     ");
+        Console.WriteLine();
+        Console.WriteLine(@"        GitHub Repository[项目主页]:https://github.com/GameFrameX/GameFrameX            ");
+        Console.WriteLine(@"        Gitee Repository[项目主页]:https://gitee.com/GameFrameX/GameFrameX              ");
+        Console.WriteLine(@"        Official Documentation[官方文档]:https://gameframex.doc.alianblank.com          ");
+        Console.WriteLine();
+        Console.WriteLine();
     }
 }
