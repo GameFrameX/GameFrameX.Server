5c6323ef8adf476379f4bac66bc5a57608ee9625
Blank <wangfj11@foxmail.com>
2025-11-07 21:16:24 +0800

    feat(社交服务): 添加社交服务启动类及发现中心注册功能
    实现社交服务的基础启动类AppStartUpSocial，包含数据库初始化、消息处理等核心功能
    新增AppStartUpSocialByDiscoveryCenter类用于处理服务注册到发现中心的逻辑
    添加心跳检测和消息处理机制，支持内外网消息区分处理

---
+++ GameFrameX.Launcher/StartUp/Social/AppStartUpSocial.cs
@@ -0,0 +1,90 @@
+﻿using GameFrameX.Core.Components;
+using GameFrameX.DataBase.Abstractions;
+using GameFrameX.NetWork.Abstractions;
+using GameFrameX.NetWork.HTTP;
+using GameFrameX.NetWork.Message;
+
+namespace GameFrameX.Launcher.StartUp.Social;
+
+/// <summary>
+/// 游戏服务器
+/// </summary>
+[StartUpTag(GlobalConst.SocialServiceName)]
+internal sealed partial class AppStartUpSocial : AppStartUpBase
+{
+    protected override bool IsRegisterToDiscoveryCenter { get; set; } = true;
+
+    public override async Task StartAsync()
+    {
+        try
+        {
+            var aopHandlerTypes = AssemblyHelper.GetRuntimeImplementTypeNamesInstance<IHttpAopHandler>();
+            aopHandlerTypes.Sort((handlerX, handlerY) => handlerX.Priority.CompareTo(handlerY.Priority));
+            LogHelper.Debug("开始启动数据库服务...");
+            LogHelper.Debug("开始配置Actor限制逻辑...");
+            ActorLimit.Init(ActorLimit.RuleType.None);
+            LogHelper.Debug("配置Actor限制逻辑结束...");
+            var initResult = await GameDb.Init<MongoDbService>(new DbOptions { ConnectionString = Setting.DataBaseUrl, Name = Setting.DataBaseName, });
+            if (initResult == false)
+            {
+                throw new InvalidOperationException("数据库服务启动失败");
+            }
+
+            await ComponentRegister.Init(typeof(AppsHandler).Assembly);
+            HotfixManager.LoadHotfix(Setting);
+            await StartServerAsync<DefaultMessageDecoderHandler, DefaultMessageEncoderHandler>(new DefaultMessageCompressHandler(), new DefaultMessageDecompressHandler(), HotfixManager.GetListHttpHandler(), HotfixManager.GetHttpHandler, aopHandlerTypes);
+
+            await AppExitToken;
+        }
+        catch (Exception e)
+        {
+            LogHelper.Info($"服务器{ServerType}执行异常，e:{e}");
+            LogHelper.Fatal(e);
+        }
+
+        await StopAsync();
+    }
+
+    protected override async ValueTask PackageHandler(IAppSession session, IMessage message)
+    {
+        await base.PackageHandler(session, message);
+        if (message is INetworkMessagePackage messageObject)
+        {
+            if (Setting.IsDebug && Setting.IsDebugReceive)
+            {
+                // 当需要打印心跳，或当前非心跳消息时才输出日志
+                // if (Setting.IsDebugReceiveHeartBeat || messageObject.Header.OperationType != (byte)MessageOperationType.HeartBeat)
+                // {
+                //    
+                //     LogHelper.Debug($"---收到[{from} To {ServerType}]  {message.ToFormatMessageString()}");
+                // }
+            }
+
+            var handler = HotfixManager.GetTcpHandler(messageObject.Header.MessageId);
+            await InvokeMessageHandler(handler, messageObject.DeserializeMessageObject(), new DefaultNetWorkChannel(session, Setting));
+        }
+    }
+
+    protected override void Init()
+    {
+        if (Setting == null)
+        {
+            Setting = new AppSetting
+            {
+                ServerType = GlobalConst.SocialServiceName,
+                ServerId = GlobalConst.SocialServiceServerId,
+                InnerPort = 29400,
+                HttpIsDevelopment = true,
+                IsDebug = true,
+                IsDebugSend = true,
+                IsDebugReceive = true,
+                IsDebugReceiveHeartBeat = false,
+                IsDebugSendHeartBeat = false,
+                DataBaseUrl = "mongodb://gameframex:7bmEw5HS0otl_KNpnsGeMOq@175.178.65.215:27017/?authSource=admin",
+                DataBaseName = "gameframex"
+            };
+        }
+
+        base.Init();
+    }
+}
